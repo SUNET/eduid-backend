9ed949ca6871f1e76d16bc77bca762664e4ccdd9
Fredrik Thulin <fredrik@thulin.net>
2013-06-03 15:42:23 +0200

    mend error message

--- eduid_am/tasks.py
+++ eduid_am/tasks.py
@@ -105,7 +105,7 @@ def update_attributes(app_name, user_id):
     try:
         attributes = attribute_fetcher(plugin_db, _id)
     except UserDoesNotExist as error:
-        logger.error('The user %s does not exist in the database for app %s' % error)
+        logger.error('The user %s does not exist in the database for app : %s' % (_id, app_name, error))
         return
 
     logger.debug('Attributes fetched from app %s for user %s: %s'
