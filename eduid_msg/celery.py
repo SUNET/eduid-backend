a26c8f6b0d1c48c578811894ebb383d87e4c67c1
Stefan Wold <swold@sunet.se>
2014-01-16 10:21:38 +0100

    Force amqp backend
    Since version 3.1.x it seems our configuration update is ignored
    for backend type.

--- eduid_msg/celery.py
+++ eduid_msg/celery.py
@@ -8,7 +8,7 @@ from celery.signals import celeryd_init
 
 from eduid_msg.config import read_configuration
 
-celery = Celery('eduid_msg.celery', include=['eduid_msg.tasks'])
+celery = Celery('eduid_msg.celery', include=['eduid_msg.tasks'], backend='amqp')
 
 # This signal is only emited when run as a worker
 @celeryd_init.connect
