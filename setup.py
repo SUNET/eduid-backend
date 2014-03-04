b22092d3792e1e447094fff36d71e431352954ca
Fredrik Thulin <fredrik@thulin.net>
2014-03-04 09:05:47 +0100

    Semantic versioning, now 0.3.0.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = 'eduID Attribute Manager'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.2'
+version = '0.3.0'
 
 install_requires = [
     'python-dateutil==2.1',
