af7469022ce89e16cc6be26bf4539943d8a6001a
Enrique Pérez <eperez@yaco.es>
2014-03-25 15:06:13 +0100

    bring over some common code fron am & dashboard, EDUID-155

--- eduid_am/config.py
+++ eduid_am/config.py
@@ -29,6 +29,49 @@ def get_config_file():
     if os.path.exists(global_file):
         return global_file
 
+def read_setting_from_env(settings, key, default=None):
+    env_variable = key.upper()
+    if env_variable in os.environ:
+        return os.environ[env_variable]
+    else:
+        return settings.get(key, default)
+
+
+def read_mapping(settings, prop, available_keys=None, default=None, required=True):
+    raw = read_setting_from_env(settings, prop, '')
+
+    if raw.strip() == '':
+        return default
+
+    rows = raw.strip('\n ').split('\n')
+
+    mapping = {}
+
+    for row in rows:
+        splitted_row = row.split('=')
+        key = splitted_row[0].strip()
+        if len(splitted_row) > 1:
+            value = splitted_row[1].strip()
+        else:
+            value = ''
+        if available_keys is None or key in available_keys:
+            mapping[key] = value
+
+    if available_keys is not None:
+        if (len(mapping.keys()) != len(available_keys) and
+                not 'testing' in settings):
+            return None
+
+    return mapping
+
+
+def read_list(settings, prop, default=[]):
+    raw = read_setting_from_env(settings, prop, None)
+    if raw is None or raw.strip() == '':
+        return default
+
+    return [e for e in raw.split('\n') if e is not None and e.strip() != '']
+
 
 def read_configuration():
     """
