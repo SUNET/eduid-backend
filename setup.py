e678778af00a88f404ecd0bad765258a5f79370e
Stefan Wold <swold@sunet.se>
2014-04-02 15:50:23 +0200

    New dev iteration (0.4.5-dev)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = 'eduID Attribute Manager'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.4.4'
+version = '0.4.5-dev'
 
 install_requires = [
     'python-dateutil==2.1',
