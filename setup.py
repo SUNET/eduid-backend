4ceed6d359755c1553fe31a0fac3f29c00c1d27e
Stefan Wold <swold@sunet.se>
2014-04-24 14:55:00 +0200

    Bump am and version

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ README = 'eduID Message Manager'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.7.9'
+version = '0.7.10'
 
 install_requires = [
-    'eduid_am>=0.4.5',
+    'eduid_am>=0.4.6',
     'python-dateutil == 2.1',
     'pymongo == 2.6.3',
     'celery == 3.1.9',
