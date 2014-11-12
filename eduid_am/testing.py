75e3c599296e90a34412a04f8dbfd2c070d76c01
Enrique Pérez <eperez@yaco.es>
2014-11-12 16:14:23 +0100

    terminated as datetime/None in the test fixtures

--- eduid_am/testing.py
+++ eduid_am/testing.py
@@ -31,7 +31,7 @@ MOCKED_USER_STANDARD = {
     'preferredLanguage': 'en',
     'eduPersonPrincipalName': 'hubba-bubba',
     'modified_ts': datetime.strptime("2013-09-02T10:23:25", "%Y-%m-%dT%H:%M:%S"),
-    'terminated': False,
+    'terminated': None,
     'eduPersonEntitlement': [
         'urn:mace:eduid.se:role:admin',
         'urn:mace:eduid.se:role:student',
