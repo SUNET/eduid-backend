a9e1705ad5a867bfc5ef5b913ba8922b17257e2c
Fredrik Thulin <fredrik@thulin.net>
2014-11-20 12:19:57 +0100

    prepare next development cycle

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = 'eduID Attribute Manager'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.5.0'
+version = '0.5.1-dev'
 
 install_requires = [
     'python-dateutil==2.1',
