b872c3d5898b32905287bdc491b84f010f06e708
Johan Lundberg <lundberg@nordu.net>
2014-11-27 13:57:54 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = 'eduID Attribute Manager'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.5.1-dev'
+version = '0.5.1'
 
 install_requires = [
     'python-dateutil==2.1',
