28ebe55cf01484f5d3528997c78d1c1128323879
Fredrik Thulin <fredrik@thulin.net>
2015-03-09 18:11:01 +0100

    don't use env EDUID_IDPROOFING_MOBILE_CONFIG as a base directory

--- eduid_lookup_mobile/config.py
+++ eduid_lookup_mobile/config.py
@@ -7,26 +7,23 @@ DEFAULT_CONFIG_FILE_NAME = 'eduid_lookup_mobile.ini'
 DEFAULT_PASSWORD_FILE_NAME = 'eduid_lookup_mobile.pw'
 
 
-def get_config_file(conf_file_name):
+def get_config_file(file_name):
     """Get the configuration file looking for it in several places.
 
     The lookup order is:
-    1. A file named acording to the value of the EDUID_IDPROOFING_MOBILE_CONFIG env variable
-    2. A file named eduid_lookup_mobile.ini In the current working directory
-    3. A file named .eduid_lookup_mobile.ini in the user's home directory
-    4. A file named eduid_lookup_mobile.ini in the system configuration directory
+    1. In the current working directory
+    2. in the user's home directory
+    3. in the system configuration directory /etc
     """
-    file_name = os.path.join(os.environ.get('EDUID_IDPROOFING_MOBILE_CONFIG', '/'), conf_file_name)
-
     if os.path.exists(file_name):
         return file_name
 
     user_file = os.path.expanduser(
-        os.path.join('~', '.', conf_file_name))
+        os.path.join('~', '.', file_name))
     if os.path.exists(user_file):
         return user_file
 
-    global_file = os.path.join('/etc', conf_file_name)
+    global_file = os.path.join('/etc', file_name)
     if os.path.exists(global_file):
         return global_file
 
@@ -34,24 +31,16 @@ def get_config_file(conf_file_name):
 def read_configuration():
     """Read the settings from environment or .ini file and return them as a dict"""
     settings = {}
+    # Add config and passwords optionally from second config file
+    cfg_fn = os.environ.get('EDUID_IDPROOFING_MOBILE_CONFIG', DEFAULT_CONFIG_FILE_NAME)
+    for fn in [cfg_fn, DEFAULT_PASSWORD_FILE_NAME]:
+        config_file = get_config_file(fn)
+        if config_file is not None:
+            config = ConfigParser.RawConfigParser()
+            config.read(config_file)
 
-    config = ConfigParser.RawConfigParser()
-
-    # Add config
-    config_file = get_config_file(DEFAULT_CONFIG_FILE_NAME)
-    if config_file is not None:
-        config.read(config_file)
-
-        if config.has_section('main'):
-            settings = dict([(s.upper(), v) for s, v in config.items('main')])
-
-    # Add passwords
-    config_file = get_config_file(DEFAULT_PASSWORD_FILE_NAME)
-    if config_file is not None:
-        config.read(config_file)
-
-        if config.has_section('main'):
-            for s, v in config.items('main'):
-                settings[s.upper()] = v
+            if config.has_section('main'):
+                for s, v in config.items('main'):
+                    settings[s.upper()] = v
 
     return settings
