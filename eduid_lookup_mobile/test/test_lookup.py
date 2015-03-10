262380f184eaaf8f611ca2152672f9c5555c8435
Mathias Hedström <mathias.hedstrom.mh@gmail.com>
2015-03-10 16:55:34 +0100

    renamed test_verifiers to test_lookup. Only test find_NIN_by_mobile

--- eduid_lookup_mobile/test/test_lookup.py
+++ eduid_lookup_mobile/test/test_lookup.py
@@ -5,8 +5,7 @@ from eduid_lookup_mobile.tasks import logger
 
 class TestVerifiers(TestCase):
 
-    def test_verify_mobile(self):
-
+    def test_find_NIN_by_mobile(self):
         mobile_verifier = MobileLookupClient(logger)
 
         self.assertTrue(mobile_verifier.find_NIN_by_mobile('+46700011222') == '200202025678')
@@ -14,6 +13,10 @@ class TestVerifiers(TestCase):
         self.assertTrue(mobile_verifier.find_NIN_by_mobile('+46700011777') == '197512125432')
         self.assertTrue(mobile_verifier.find_NIN_by_mobile('+46700011999') is None)
 
-        self.assertTrue(mobile_verifier.find_mobiles_by_NIN('200202025678') == ['+46700011222'])
-        self.assertTrue(mobile_verifier.find_mobiles_by_NIN('197512125432') == ['+46700011333', '+46700011777'])
-        self.assertTrue(mobile_verifier.find_mobiles_by_NIN('197512125430') == [])
+    # def test_find_mobiles_by_NIN(self):
+    #
+    #     mobile_verifier = MobileLookupClient(logger)
+    #
+    #     self.assertTrue(mobile_verifier.find_mobiles_by_NIN('200202025678') == ['+46700011222'])
+    #     self.assertTrue(mobile_verifier.find_mobiles_by_NIN('197512125432') == ['+46700011333', '+46700011777'])
+    #     self.assertTrue(mobile_verifier.find_mobiles_by_NIN('197512125430') == [])
