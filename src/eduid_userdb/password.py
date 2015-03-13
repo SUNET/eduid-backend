02d057b492728149b73cc6c1f4adcb8c4e8dd4bb
Johan Lundberg <lundberg@nordu.net>
2015-03-13 13:25:02 +0100

    Initial commit of password and it's tests.

---
+++ src/eduid_userdb/password.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2015 NORDUnet A/S
+# All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or
+#   without modification, are permitted provided that the following
+#   conditions are met:
+#
+#     1. Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#     2. Redistributions in binary form must reproduce the above
+#        copyright notice, this list of conditions and the following
+#        disclaimer in the documentation and/or other materials provided
+#        with the distribution.
+#     3. Neither the name of the NORDUnet nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author : Johan Lundberg <lundberg@nordu.net>
+#
+__author__ = 'lundberg'
+
+from bson.objectid import ObjectId
+import copy
+from eduid_userdb.element import Element, ElementList, DuplicatePrimaryElementViolation
+from eduid_userdb.exceptions import UserHasUnknownData, UserDBValueError
+
+
+class Password(Element):
+
+    def __init__(self, data, raise_on_unknown=True):
+        data_in = data
+        data = copy.copy(data_in)  # to not modify callers data
+        Element.__init__(self, data)
+        self.id = data.pop('id')
+        self.salt = data.pop('salt')
+        self.created_by = data.pop('source', None)  # XXX We should rename source in db
+
+        leftovers = data.keys()
+        if leftovers:
+            if raise_on_unknown:
+                raise UserHasUnknownData('Password {!r} unknown data: {!r}'.format(
+                    self.id, leftovers,
+                ))
+            # Just keep everything that is left as-is
+            self._data.update(data)
+
+    @property
+    def key(self):
+        """
+        Return the element that is used as key.
+        """
+        return self.id
+
+    @property
+    def id(self):
+        """
+        This is a reference to the ObjectId in the authentication private database.
+
+        :return: Unique ID of password.
+        :rtype: bson.ObjectId
+        """
+        return self._data['id']
+
+    @id.setter
+    def id(self, value):
+        """
+        :param value: Unique ID of password.
+        :type value: bson.ObjectId
+        """
+        if not isinstance(value, ObjectId):
+            raise UserDBValueError("Invalid 'id': {!r}".format(value))
+        self._data['id'] = value
+
+    @property
+    def salt(self):
+        """
+        This is a reference to the ObjectId in the authentication private database.
+
+        :return: Password salt.
+        :rtype: str
+        """
+        return self._data['salt']
+
+    @salt.setter
+    def salt(self, value):
+        """
+        :param value: Password salt.
+        :type value: str
+        """
+        if not isinstance(value, basestring):
+            raise UserDBValueError("Invalid 'salt': {!r}".format(value))
+        self._data['salt'] = value
+
+    def to_dict(self, old_userdb_format=False):
+        if not old_userdb_format:
+            return self._data
+        old = copy.copy(self._data)
+        # XXX created_by -> source
+        old['source'] = old.pop('created_by')
+        return old
+
+
+class PasswordList(ElementList):
+    """
+    Hold a list of Password instances.
+
+    Provide methods to add, update and remove elements from the list while
+    maintaining some governing principles, such as ensuring there no duplicates in the list.
+
+    :param passwords: List of passwords
+    :type passwords: [dict | Passwords]
+    """
+
+    def __init__(self, passwords, raise_on_unknown=True):
+        elements = []
+        for this in passwords:
+            if isinstance(this, Password):
+                password = this
+            else:
+                password = password_from_dict(this, raise_on_unknown)
+            elements.append(password)
+
+        ElementList.__init__(self, elements)
+
+    def add(self, element):
+        if self.find(element.key):
+            raise DuplicatePrimaryElementViolation("password {!s} already in list".format(element.key))
+        super(PasswordList, self).add(element)
+
+
+def password_from_dict(data, raise_on_unknown=True):
+    """
+    Create a Password instance from a dict.
+
+    :param data: Password parameters from database
+
+    :type data: dict
+    :rtype: Password
+    """
+    return Password(data, raise_on_unknown)
---
+++ src/eduid_userdb/tests/test_PasswordList.py
@@ -0,0 +1,96 @@
+from unittest import TestCase
+
+from bson.objectid import ObjectId
+import eduid_userdb.exceptions
+import eduid_userdb.element
+from eduid_userdb.password import Password, PasswordList
+
+__author__ = 'lundberg'
+
+#{'passwords': {
+#    'id': password_id,
+#    'salt': salt,
+#    'source': 'signup',
+#    'created_ts': datetime.datetime.utcnow(),
+#}}
+
+
+class TestPasswordList(TestCase):
+
+    def setUp(self):
+        self.empty = PasswordList([])
+
+        self._one_dict = \
+            [{'id': ObjectId('55002741d00690878ae9b600'),
+              'salt': 'firstPasswordElement',
+              'source': 'test',
+              }]
+        self.one = PasswordList(self._one_dict)
+
+        self._two_dict = self._one_dict + \
+            [{'id': ObjectId('55002741d00690878ae9b601'),
+              'salt': 'secondPasswordElement',
+              'source': 'test',
+              }]
+        self.two = PasswordList(self._two_dict)
+
+        self._three_dict = self._two_dict + \
+            [{'id': ObjectId('55002741d00690878ae9b602'),
+              'salt': 'thirdPasswordElement',
+              'source': 'test',
+              },
+             ]
+        self.three = PasswordList(self._three_dict)
+
+    def test_to_list(self):
+        self.assertEqual([], self.empty.to_list(), list)
+        self.assertIsInstance(self.one.to_list(), list)
+
+        self.assertEqual(1, len(self.one.to_list()))
+
+    def test_to_list_of_dicts(self):
+        self.assertEqual([], self.empty.to_list_of_dicts(), list)
+
+        self.assertEqual(self._one_dict, self.one.to_list_of_dicts(old=True))
+
+    def test_find(self):
+        match = self.one.find(ObjectId('55002741d00690878ae9b600'))
+        self.assertIsInstance(match, Password)
+        self.assertEqual(match.id, ObjectId('55002741d00690878ae9b600'))
+        self.assertEqual(match.salt, 'firstPasswordElement')
+        self.assertEqual(match.created_by, 'test')
+
+    def test_add(self):
+        second = self.two.find(ObjectId('55002741d00690878ae9b601'))
+        self.one.add(second)
+        self.assertEqual(self.one.to_list_of_dicts(), self.two.to_list_of_dicts())
+
+    def test_add_duplicate(self):
+        dup = self.two.find(ObjectId('55002741d00690878ae9b601'))
+        with self.assertRaises(eduid_userdb.element.DuplicatePrimaryElementViolation):
+            self.two.add(dup)
+
+    def test_add_password(self):
+        third = self.three.find(ObjectId('55002741d00690878ae9b602'))
+        this = PasswordList(self._two_dict + [third])
+        self.assertEqual(this.to_list_of_dicts(), self.three.to_list_of_dicts())
+
+    def test_remove(self):
+        now_two = self.three.remove(ObjectId('55002741d00690878ae9b602'))
+        self.assertEqual(self.two.to_list_of_dicts(), now_two.to_list_of_dicts())
+
+    def test_remove_unknown(self):
+        with self.assertRaises(eduid_userdb.exceptions.UserDBValueError):
+            self.one.remove('foo@no-such-address.example.org')
+
+    def test_key(self):
+        """
+        Test that the 'key' property (used by PasswordList) works for the Password.
+        """
+        password = self.one.find(ObjectId('55002741d00690878ae9b600'))
+        self.assertEqual(password.key, password.id)
+
+    def test_setting_invalid_mail(self):
+        this = self.one.find(ObjectId('55002741d00690878ae9b600'))
+        with self.assertRaises(eduid_userdb.exceptions.UserDBValueError):
+            this.id = None
