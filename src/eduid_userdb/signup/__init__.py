d87ca917c8962cee4902e0ab7bfd5d12c6983dea
Fredrik Thulin <fredrik@thulin.net>
2015-04-16 14:04:09 +0200

    Move signup's userdb here, after discussions about where to put it.

---
+++ src/eduid_userdb/signup/__init__.py
@@ -0,0 +1,16 @@
+"""
+The eduID Signup application database interface package.
+
+Copyright (c) 2015 SUNET. All rights reserved.
+See the file LICENSE.txt for full license statement.
+"""
+
+__version__ = '0.0.1'
+__copyright__ = 'SUNET'
+__organization__ = 'SUNET'
+__license__ = 'BSD'
+__authors__ = ['Fredrik Thulin']
+
+# convenience imports (order matters)
+from eduid_userdb.signup.user import SignupUser
+from eduid_userdb.signup.userdb import SignupUserDB
