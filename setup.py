4a63f58e6a7f370a742521914dfb723da18acd0d
Fredrik Thulin <fredrik@thulin.net>
2015-04-22 15:26:21 +0200

    next development version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = 'eduID Attribute Manager'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.5.3'
+version = '0.5.3-dev'
 
 install_requires = [
     'python-dateutil>=2.1',
