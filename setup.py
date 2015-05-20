5209af58fc7574e0e6e4ee811de1a58a7a30215f
Johan Lundberg <lundberg@nordu.net>
2015-05-20 17:03:05 +0200

    Bumbed version from 0.0.4b to 0.0.4.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = 'eduID Lookup Mobile'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.0.4b'
+version = '0.0.4'
 
 install_requires = [
     'eduid-userdb >= 0.0.2',
