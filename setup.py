ffeefc15e701791101344cffcf794405b94aa3da
Fredrik Thulin <fredrik@thulin.net>
2015-05-25 17:55:37 +0200

    version 0.8.9

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = 'eduID Message Manager'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.8.8b0'
+version = '0.8.9'
 
 install_requires = [
     'eduid-am>=0.5.3',
