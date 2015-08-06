dcbd257e376db55ddf002b5e5858054e31aed19a
John Van de Meulebrouck Brendgard <john@nordu.net>
2015-08-06 14:39:33 +0200

    Added test for the fix to set primary email address

--- eduid_am/tests/test_user.py
+++ eduid_am/tests/test_user.py
@@ -19,3 +19,31 @@ class TestUser(MongoTestCase):
         self.assertEqual(user.get_display_name(), M['displayName'])
         user_dict = user.get_doc()
         self.assertEqual(user_dict['mail'], M['mail'])
+
+    def test_verify_mail_and_set_as_primary(self):
+        user = self.udb.get_user(M['mail'])
+
+        # Save the original information so that
+        # we can restore it after this test.
+        old_mail_aliases = user.get_mail_aliases()
+        old_mail = user.get_mail()
+
+        # Remove the existing aliases and add one unverified
+        user.set_mail_aliases([])
+        user.set_mail_aliases(
+            [{
+            'email': 'testmail@example.com',
+            'verified': False,
+            }]
+        )
+
+        # Verify the only existing mail alias and since it
+        # is the only existing mail address, set it as primary.
+        user.add_verified_email('testmail@example.com')
+
+        self.assertEqual(user.get_mail_aliases(), [{'verified': True, 'email': 'testmail@example.com'}])
+        self.assertEqual(user.get_mail(), 'testmail@example.com')
+
+        # Restore the old mail settings for other tests
+        user.set_mail_aliases(old_mail_aliases)
+        user.set_mail(old_mail)
