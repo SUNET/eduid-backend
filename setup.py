9cc2e3660e5f6afc4c0904cfd97abd3d074934c8
Fredrik Thulin <fredrik@thulin.net>
2015-08-19 14:38:46 +0200

    version 0.6.0b0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = 'eduID Attribute Manager'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.6.0-dev'
+version = '0.6.0b0'
 
 install_requires = [
     'eduid_userdb >= 0.0.0, < 0.1.0',
