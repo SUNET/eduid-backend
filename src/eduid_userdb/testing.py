244860903fcce03e4719656577478dab5493308f
Fredrik Thulin <fredrik@thulin.net>
2015-08-19 14:36:42 +0200

    reset testing framework to what was in commit 28e2112e15 (working)

--- src/eduid_userdb/testing.py
+++ src/eduid_userdb/testing.py
@@ -49,23 +49,154 @@ from copy import deepcopy
 from bson import ObjectId
 
 from eduid_userdb import UserDB, User
-from eduid_userdb.data_samples import NEW_BASIC_USER_EXAMPLE
 
 import logging
 logger = logging.getLogger(__name__)
 
 
-def get_two_test_users():
-    first_user = deepcopy(NEW_BASIC_USER_EXAMPLE)
-    second_user = deepcopy(NEW_BASIC_USER_EXAMPLE)
-    second_user['_id'] = ObjectId('901234567890123456789012')
-    second_user['mail'] = 'johnsmith@example.org'
-    second_user['eduPersonPrincipalName'] = 'babba-labba'
-    second_user['mailAliases'][0]['email'] = 'johnsmith@example.org'
-    second_user['mailAliases'][1]['email'] = 'johnsmith2@example.org'
-    second_user['nins'][0]['number'] = '196701101234'
-    second_user['phone'] = []
-    return first_user, second_user
+MONGO_URI_AM_TEST = 'mongodb://localhost:27017/eduid_userdb_test'
+MONGO_URI_TEST = 'mongodb://localhost:27017/eduid_dashboard_test'
+
+#eduid_userdb.db.DEFAULT_MONGODB_URI = MONGO_URI_AM_TEST
+#eduid_userdb.db.DEFAULT_MONGODB_NAME = 'eduid_userdb_test'
+
+
+MOCKED_USER_STANDARD = {
+    '_id': ObjectId('012345678901234567890123'),
+    'givenName': 'John',
+    'sn': 'Smith',
+    'displayName': 'John Smith',
+    'norEduPersonNIN': [{'number': '197801011234',
+                         'verified': True,
+                         'primary': True,
+                         }],
+    #'photo': 'https://pointing.to/your/photo',
+    'preferredLanguage': 'en',
+    'eduPersonPrincipalName': 'hubba-bubba',
+    #'modified_ts': datetime.strptime("2013-09-02T10:23:25", "%Y-%m-%dT%H:%M:%S"),
+    #'terminated': None,
+    'eduPersonEntitlement': [
+        'urn:mace:eduid.se:role:admin',
+        'urn:mace:eduid.se:role:student',
+    ],
+    #'maxReachedLoa': 3,
+    'mobile': [{
+        'mobile': '+34609609609',
+        'primary': True,
+        'verified': True
+    }, {
+        'mobile': '+34 6096096096',
+        'verified': False
+    }],
+    'mail': 'johnsmith@example.com',
+    'mailAliases': [{
+        'email': 'johnsmith@example.com',
+        'verified': True,
+    }, {
+        'email': 'johnsmith2@example.com',
+        'verified': True,
+    }, {
+        'email': 'johnsmith3@example.com',
+        'verified': False,
+    }],
+    'passwords': [{
+        'id': ObjectId('112345678901234567890123'),
+        'salt': '$NDNv1H1$9c810d852430b62a9a7c6159d5d64c41c3831846f81b6799b54e1e8922f11545$32$32$',
+    }],
+    #'postalAddress': [{
+    #    'type': 'home',
+    #    'country': 'SE',
+    #    'address': "Long street, 48",
+    #    'postalCode': "123456",
+    #    'locality': "Stockholm",
+    #    'verified': True,
+    #}, {
+    #    'type': 'work',
+    #    'country': 'ES',
+    #    'address': "Calle Ancha, 49",
+    #    'postalCode': "123456",
+    #    'locality': "Punta Umbria",
+    #    'verified': False,
+    #}],
+}
+
+INITIAL_VERIFICATIONS = [{
+    '_id': ObjectId('234567890123456789012301'),
+    'code': '9d392c',
+    'model_name': 'mobile',
+    'obj_id': '+34 6096096096',
+    'user_oid': ObjectId("012345678901234567890123"),
+    'timestamp': datetime.utcnow(),
+    'verified': False,
+}, {
+    '_id': ObjectId(),
+    'code': '123123',
+    'model_name': 'norEduPersonNIN',
+    'obj_id': '210987654321',
+    'user_oid': ObjectId("012345678901234567890123"),
+    'timestamp': datetime.utcnow(),
+    'verified': False,
+}, {
+    '_id': ObjectId(),
+    'code': '123124',
+    'model_name': 'norEduPersonNIN',
+    'obj_id': '197801011234',
+    'user_oid': ObjectId("012345678901234567890123"),
+    'timestamp': datetime.utcnow(),
+    'verified': True,
+}, {
+    '_id': ObjectId(),
+    'code': '123124',
+    'model_name': 'norEduPersonNIN',
+    'obj_id': '123456789050',
+    'user_oid': ObjectId("012345678901234567890123"),
+    'timestamp': datetime.utcnow(),
+    'verified': False,
+}]
+
+
+class MockedUserDB(UserDB):
+    """
+    Some mock users used in different tests.
+
+    Need to do deepcopy everywhere to better isolate tests from each other - since this is
+    an in-memory database, one test might manipulate the returned data and could otherwise
+    potentially affect other tests using the same data.
+    """
+
+    test_users = {
+        'johnsmith@example.com': deepcopy(MOCKED_USER_STANDARD),
+        'johnsmith@example.org': deepcopy(MOCKED_USER_STANDARD),
+    }
+    test_users['johnsmith@example.org']['mail'] = 'johnsmith@example.org'
+    test_users['johnsmith@example.org']['mailAliases'][0]['email'] = 'johnsmith@example.org'
+    test_users['johnsmith@example.org']['mailAliases'][1]['email'] = 'johnsmith2@example.org'
+    test_users['johnsmith@example.org']['_id'] = ObjectId('901234567890123456789012')
+    test_users['johnsmith@example.org']['norEduPersonNIN'] = []
+    test_users['johnsmith@example.org']['mobile'] = []
+    test_users['johnsmith@example.org']['eduPersonPrincipalName'] = 'babba-labba'
+
+    def __init__(self, users=[]):
+        import pprint
+        for user in users:
+            mail = user.get('mail', '')
+            if mail in self.test_users:
+                logger.debug("Updating MockedUser {!r} with:\n{!s}".format(mail, pprint.pformat(user)))
+                self.test_users[mail].update(user)
+                logger.debug("New MockedUser {!r}:\n{!s}".format(mail, pprint.pformat(self.test_users[mail])))
+
+    def get_user(self, userid):
+        if userid not in self.test_users:
+            raise self.UserDoesNotExist
+        return User(deepcopy(self.test_users.get(userid)))
+
+    def all_users(self):
+        for user in self.test_users.values():
+            yield User(deepcopy(user))
+
+    def all_userdocs(self):
+        for user in self.test_users.values():
+            yield deepcopy(user)
 
 
 class MongoTemporaryInstance(object):
@@ -146,17 +277,49 @@ class MongoTestCase(unittest.TestCase):
     A test can access the connection using the attribute `conn`.
     A test can access the port using the attribute `port`
     """
+    fixtures = []
 
-    def setUp(self):
+    MockedUserDB = MockedUserDB
+
+    user = User(data=MOCKED_USER_STANDARD)
+    mock_users_patches = []
+
+    def setUp(self, celery, get_attribute_manager, userdb_use_old_format=False):
         """
         Test case initialization.
 
+        To not get a circular dependency between eduid-userdb and eduid-am, celery
+        and get_attribute_manager needs to be imported in the place where this
+        module is called.
+
+        Usage:
+
+            from eduid_am.celery import celery, get_attribute_manager
+
+            class MyTest(MongoTestCase):
+
+                def setUp(self):
+                    super(MyTest, self).setUp(celery, get_attribute_manager)
+                    ...
+
+        :param celery: module
+        :param get_attribute_manager: callable
         :return:
         """
         super(MongoTestCase, self).setUp()
         self.tmp_db = MongoTemporaryInstance.get_instance()
         self.conn = self.tmp_db.conn
         self.port = self.tmp_db.port
+        self.am_settings = {
+            'BROKER_TRANSPORT': 'memory',
+            'BROKER_URL': 'memory://',
+            'CELERY_EAGER_PROPAGATES_EXCEPTIONS': True,
+            'CELERY_ALWAYS_EAGER': True,
+            'CELERY_RESULT_BACKEND': "cache",
+            'CELERY_CACHE_BACKEND': 'memory',
+            'MONGO_URI': self.tmp_db.get_uri(),
+            'MONGO_DBNAME': 'eduid_userdb',
+        }
 
         mongo_settings = {
             'mongo_replicaset': None,
@@ -167,9 +330,35 @@ class MongoTestCase(unittest.TestCase):
             self.settings = mongo_settings
         else:
             self.settings.update(mongo_settings)
+        celery.conf.update(self.am_settings)
+
+        self.am = get_attribute_manager(celery)
 
         for db_name in self.conn.database_names():
             self.conn.drop_database(db_name)
+
+        # Be sure to tell AttributeManager.get_userdb() about the temporary
+        # mongodb instance.
+        self.am.default_db_uri = self.tmp_db.get_uri()
+        self.amdb = self.am.get_userdb('default')
+
+        self.initial_verifications = (getattr(self, 'initial_verifications', None)
+                                      or INITIAL_VERIFICATIONS)
+        self.amdb._drop_whole_collection()
+
+        # Set up test users in the MongoDB. Read the users from MockedUserDB, which might
+        # be overridden by subclasses.
+        _foo_userdb = self.MockedUserDB(self.mock_users_patches)
+        for userdoc in _foo_userdb.all_userdocs():
+            this = deepcopy(userdoc)  # deep-copy to not have side effects between tests
+            user = User(data=this)
+            self.amdb.save(user, check_sync=False, old_format=userdb_use_old_format)
+
+    def tearDown(self):
+        super(MongoTestCase, self).tearDown()
+        for userdoc in self.amdb._get_all_userdocs():
+            assert User(userdoc)
+        self.amdb._drop_whole_collection()
 
     def mongodb_uri(self, dbname=None):
         return self.tmp_db.get_uri(dbname=dbname)
