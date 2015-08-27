6dda7f1264c8263ac824ea739161b6825aac9a32
Fredrik Thulin <fredrik@thulin.net>
2015-08-27 21:46:41 +0200

    Format string and logging improvements.

--- eduid_am/tasks.py
+++ eduid_am/tasks.py
@@ -15,6 +15,10 @@ logger = get_task_logger(__name__)
 
 
 class PluginsRegistry(object):
+    """
+    In-memory information about existing Attribute Manager plugins,
+    and the result of their initialization function (context).
+    """
 
     def __init__(self, am_conf):
         self.context = dict()
@@ -22,17 +26,17 @@ class PluginsRegistry(object):
 
         for entry_point in iter_entry_points('eduid_am.plugin_init'):
             if entry_point.name in self.context:
-                logger.warn("Duplicate plugin_init entry point: {}".format(entry_point.name))
+                logger.warn('Duplicate plugin_init entry point: {!r}'.format(entry_point.name))
             else:
-                logger.debug("Calling plugin_init entry point: {}".format(entry_point.name))
+                logger.debug('Calling plugin_init entry point for {!r}'.format(entry_point.name))
                 plugin_init = entry_point.load()
                 self.context[entry_point.name] = plugin_init(am_conf)
 
         for entry_point in iter_entry_points('eduid_am.attribute_fetcher'):
             if entry_point.name in self.attribute_fetcher:
-                logger.warn("Duplicate entry point: %s" % entry_point.name)
+                logger.warn('Duplicate attribute_fetcher entry point: {!r}'.format(entry_point.name))
             else:
-                logger.debug("Registering entry point: %s" % entry_point.name)
+                logger.debug('Registering attribute_fetcher entry point for {!r}'.format(entry_point.name))
                 self.attribute_fetcher[entry_point.name] = entry_point.load()
 
 
@@ -42,9 +46,7 @@ class AttributeManager(Task):
 
     abstract = True  # This means Celery won't register this as another task
 
-    def __init__(self, unknown_arg=None):
-        logger.warn("Initializing AttributeManager with unknown_arg {!r}".format(unknown_arg))
-
+    def __init__(self):
         self.default_db_uri = self.app.conf.get('MONGO_URI')
 
         # self.userdb is the UserDB to which AM will write the updated users
@@ -85,43 +87,46 @@ def update_attributes_keep_result(app_name, obj_id):
 
 
 def _update_attributes(app_name, obj_id):
-    logger.debug("Update attributes called for {!r} by {!r}".format(obj_id, app_name))
+    logger.debug('Update attributes called for {!r} by {!r}'.format(obj_id, app_name))
     try:
         return _update_attributes_safe(app_name, obj_id)
     except Exception:
-        logger.error("Got exception processing {!r}[{!r}]".format(app_name, obj_id), exc_info = True)
+        logger.error('Got exception processing {!r}[{!r}]'.format(app_name, obj_id), exc_info = True)
         raise
 
 
 def _update_attributes_safe(app_name, user_id):
     self = update_attributes
-    logger.debug("update %s[%s]" % (app_name, user_id))
+    logger.debug('update {!s}[{!s}]'.format(app_name, user_id))
 
     try:
         attribute_fetcher = self.registry.attribute_fetcher[app_name]
     except KeyError:
-        logger.error('Plugin for %s is not installed'.format(app_name))
+        logger.error('Plugin for {!s} is not installed'.format(app_name))
         return
+
+    logger.debug("Attribute fetcher for {!s}: {!r}".format(app_name, attribute_fetcher))
 
     try:
         _id = bson.ObjectId(user_id)
     except bson.errors.InvalidId:
-        logger.error('Invalid user_id %s from app %s' % (user_id, app_name))
+        logger.error('Invalid user_id {!s} from app {!s}'.format(user_id, app_name))
         raise ValueError('Bad user_id')
 
     try:
         _context = self.registry.context[app_name]
     except KeyError:
-        logger.error('Plugin for %s is not initialized'.format(app_name))
+        logger.error('Plugin for {!s} is not initialized'.format(app_name))
         return
+
+    logger.debug("Context for {!s}: {!r}".format(app_name, _context))
 
     try:
         attributes = attribute_fetcher(_context, _id)
     except UserDoesNotExist as error:
-        logger.error('The user %s does not exist in the database for plugin %s: %s' % (
+        logger.error('The user {!s} does not exist in the database for plugin {!s}: {!s}'.format(
             _id, app_name, error))
         return
 
-    logger.debug('Attributes fetched from app %s for user %s: %s'
-                 % (app_name, user_id, attributes))
+    logger.debug('Attributes fetched from app {!s} for user {!s}: {!s}'.format(app_name, user_id, attributes))
     self.userdb.update_user(_id, attributes)
