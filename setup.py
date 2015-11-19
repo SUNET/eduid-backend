5065fb112ac1fb403e402e82cd8ae8dea3c8dc03
Johan Lundberg <lundberg@nordu.net>
2015-11-19 10:57:40 +0100

    Bumped version 0.6.1b0 -> 0.6.1

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ README = 'eduID Attribute Manager'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.6.1b0'
+version = '0.6.1'
 
 install_requires = [
-    'eduid_userdb >= 0.0.4b4',
+    'eduid_userdb >= 0.0.4',
     'python-dateutil>=2.1',
     'celery>=3.1.17, < 3.2.0',
     'simplejson>=3.6.5',
