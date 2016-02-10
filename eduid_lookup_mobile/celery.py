7885cdda5459483109df27108349cfb1f04a2831
Johan Lundberg <lundberg@nordu.net>
2016-02-10 17:10:45 +0100

    Load celery settings in one place only

--- eduid_lookup_mobile/celery.py
+++ eduid_lookup_mobile/celery.py
@@ -4,12 +4,11 @@ from celery import Celery
 from celery.signals import celeryd_init
 from eduid_lookup_mobile import config
 
-app = Celery('eduid_lookup_mobile.celery', include=['eduid_lookup_mobile.tasks'], backend='amqp')
+app = Celery('eduid_lookup_mobile.celery', include=['eduid_lookup_mobile.tasks'])
 app.conf.update(config.read_configuration())
 
 
 # This signal is only emited when run as a worker
 @celeryd_init.connect
 def setup_celeryd(sender, conf, **kwargs):
-    settings = config.read_configuration()
-    conf.update(settings)
+    pass  # If we need to do anything on connect later
