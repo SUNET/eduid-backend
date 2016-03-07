3aedc1027c586d081c3f0faebd3d9ad57fb403e8
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-03-07 12:07:18 +0100

    initial package metadata

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import sys
+
+version = '0.1.0'
+
+requires = [
+    'Flask==0.8',
+    'Flask-RESTful==0.3.5',
+]
+
+
+test_requires = [
+    'WebTest==2.0.18',
+    'mock==1.0.1',
+]
+
+testing_extras = test_requires + [
+    'nose==1.2.1',
+    'coverage==3.6',
+    'nosexcover==1.0.8',
+]
+
+setup(
+    name='eduid_api.authn',
+    version='0.1.0',
+    license='bsd',
+    url='https://www.github.com/eduID/',
+    author='NORDUnet A/S',
+    author_email='',
+    description='authentication service for eduID',
+    classifiers=[
+        'Framework :: Flask',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['eduid_api'],
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=requires,
+    tests_require=test_requires,
+    extras_require={
+        'testing': testing_extras,
+    },
+)
