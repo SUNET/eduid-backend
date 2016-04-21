2f6c601d7d7b136d559537a9175b68a5f9dc9ff3
Johan Lundberg <lundberg@nordu.net>
2016-04-21 13:15:02 +0200

    Fixes for OP compatability and more logging

--- src/eduid_webapp/oidc_proofing/app.py
+++ src/eduid_webapp/oidc_proofing/app.py
@@ -3,8 +3,9 @@
 from __future__ import absolute_import
 
 from flask import Flask
+from requests.exceptions import ConnectionError
 from oic.oic import Client
-from oic.oic.message import ProviderConfigurationResponse, RegistrationRequest
+from oic.oic.message import RegistrationRequest
 from oic.utils.authn.client import CLIENT_AUTHN_METHOD
 
 from eduid_common.api.app import eduid_init_app
@@ -15,10 +16,15 @@ __author__ = 'lundberg'
 
 
 def init_oidc_client(app):
-    provider_config = ProviderConfigurationResponse(**app.config['PROVIDER_CONFIGURATION_INFO'])
     oidc_client = Client(client_authn_method=CLIENT_AUTHN_METHOD)
     oidc_client.store_registration_info(RegistrationRequest(**app.config['CLIENT_REGISTRATION_INFO']))
-    oidc_client.handle_provider_config(provider_config, provider_config['issuer'])
+    provider = app.config['PROVIDER_CONFIGURATION_INFO']['issuer']
+    try:
+        oidc_client.provider_config(provider)
+    except ConnectionError as e:
+        app.logger.critical('No connection to provider {!s}. Can not start without provider configuration.'.format(
+            provider))
+        raise e
     return oidc_client
 
 
@@ -33,9 +39,10 @@ def oidc_proofing_init_app(name, config):
     and finally all needed blueprints will be registered with it.
 
     :param name: The name of the instance, it will affect the configuration loaded.
-    :type name: str
     :param config: any additional configuration settings. Specially useful
                    in test cases
+
+    :type name: str
     :type config: dict
 
     :return: the flask app
--- src/eduid_webapp/oidc_proofing/views.py
+++ src/eduid_webapp/oidc_proofing/views.py
@@ -21,46 +21,54 @@ OIDC code very inspired by https://github.com/its-dirg/Flask-pyoidc
 oidc_proofing_views = Blueprint('oidc_proofing', __name__, url_prefix='')
 
 
-@oidc_proofing_views.route('/authorization_response')
+@oidc_proofing_views.route('/authorization-response')
 def authorization_response():
     # parse authentication response
+    current_app.logger.debug('Authorization response received')
     query_string = request.query_string.decode('utf-8')
     authn_resp = current_app.oidc_client.parse_response(AuthorizationResponse, info=query_string,
                                                         sformat='urlencoded')
-
     user_oidc_state = authn_resp['state']
     proofing_state = current_app.proofing_statedb.get_state_by_oidc_state(user_oidc_state)
     if not proofing_state:
-        current_app.logger.error('The \'state\' parameter ({!s}) does not match a user state.'.format(user_oidc_state))
-        raise ValueError('The \'state\' parameter ({!s}) does not match a user state.'.format(user_oidc_state))
+        msg = 'The \'state\' parameter ({!s}) does not match a user state.'.format(user_oidc_state)
+        current_app.logger.error(msg)
+        raise ApiException(payload={'error': msg})
 
+    current_app.logger.debug('Proofing state {!s} for user {!s} found'.format(proofing_state.state,
+                                                                              proofing_state.eppn))
     # do token request
     args = {
         'code': authn_resp['code'],
-        'redirect_uri': current_app.oidc_client.registration_response['redirect_uris'][0]
+        'redirect_uri': url_for('oidc_proofing.authorization_response', _external=True)
     }
+    current_app.logger.debug('Trying to do token request: {!s}'.format(args))
     token_resp = current_app.oidc_client.do_access_token_request(scope='openid', state=authn_resp['state'],
                                                                  request_args=args,
                                                                  authn_method='client_secret_basic')
+    current_app.logger.debug('token response received: {!s}'.format(token_resp))
     id_token = token_resp['id_token']
     if id_token['nonce'] != proofing_state.nonce:
-        raise ValueError('The \'nonce\' parameter does not match.')
-    access_token = token_resp['access_token']
+        current_app.logger.error('The \'nonce\' parameter does not match for user {!s}.'.format(proofing_state.eppn))
+        raise ApiException(payload={'error': 'The \'nonce\' parameter does not match match.'})
 
+    access_token = token_resp['access_token']
+    current_app.logger.debug('Trying to do userinfo request: {!s}')
     # do userinfo request
     userinfo = current_app.oidc_client.do_user_info_request(method=current_app.config['USERINFO_ENDPOINT_METHOD'],
                                                             state=authn_resp['state'])
+    current_app.logger.debug('userinfo received: {!s}'.format(userinfo))
     if userinfo['sub'] != id_token['sub']:
-        raise ValueError('The \'sub\' of userinfo does not match \'sub\' of ID Token.')
+        current_app.logger.error('The \'sub\' of userinfo does not match \'sub\' of ID Token for user {!s}.'.format(
+            proofing_state.eppn))
+        raise ApiException(payload={'The \'sub\' of userinfo does not match \'sub\' of ID Token'})
 
     # TODO: Using id_token, access_token and userinfo create ProofingData and
     # TODO: hand that over to the Proofing Consumer service
-    current_app.logger.debug(authn_resp)
-    current_app.logger.debug(token_resp)
-    current_app.logger.debug(userinfo)
 
     # Remove users proofing state
     current_app.proofing_statedb.remove_state(proofing_state)
+    return make_response('OK', 200)
 
 
 @oidc_proofing_views.route('/get-state')
@@ -68,8 +76,10 @@ def get_state():
     # TODO: Authenticate user
     # TODO: Look up user in central db
     eppn = request.args.get('eppn')
+    current_app.logger.debug('Getting state for user with eppn {!s}.'.format(eppn))
     proofing_state = current_app.proofing_statedb.get_state_by_eppn(eppn, raise_on_missing=False)
     if not proofing_state:
+        current_app.logger.debug('No proofing state found, initializing new proofing flow.'.format(eppn))
         state = get_unique_hash()
         nonce = get_unique_hash()
         proofing_state = OidcProofingState({'eduPersonPrincipalName': eppn, 'state': state, 'nonce': nonce})
@@ -77,6 +87,7 @@ def get_state():
         args = {
             'client_id': current_app.oidc_client.client_id,
             'response_type': 'code id_token token',
+            'response_mode': 'query',
             'scope': ['openid'],
             'redirect_uri': url_for('oidc_proofing.authorization_response', _external=True),
             'state': state,
@@ -85,15 +96,22 @@ def get_state():
         current_app.logger.debug('AuthorizationRequest args:')
         current_app.logger.debug(args)
         try:
-            current_app.oidc_client.do_authorization_request(request_args=args, method='POST')
+            response = requests.post(current_app.oidc_client.authorization_endpoint, data=args)
         except requests.exceptions.ConnectionError as e:
             msg = 'No connection to authorization endpoint: {!s}'.format(e)
             current_app.logger.error(msg)
             raise ApiException(payload={'error': msg})
         # If do_authorization_request went well save user state
-        current_app.proofing_statedb.save(proofing_state)
-
+        if response.status_code == 200:
+            current_app.logger.debug('Authentication request delivered to provider {!s}'.format(
+                current_app.config['PROVIDER_CONFIGURATION_INFO']['issuer']))
+            current_app.proofing_statedb.save(proofing_state)
+            current_app.logger.debug('Proofing state {!s} for user {!s} saved'.format(proofing_state.state, eppn))
+        else:
+            payload = {'error': response.reason, 'message': response.content}
+            raise ApiException(status_code=response.status_code, payload=payload)
     # Return nonce and nonce as qr code
+    current_app.logger.debug('Returning nonce for user {!s}'.format(eppn))
     buf = StringIO()
     qrcode.make(proofing_state.nonce).save(buf)
     qr_b64 = buf.getvalue().encode('base64')
@@ -101,3 +119,4 @@ def get_state():
     image_tag = '<img src="data:image/png;base64, {!s}"/>'.format(qr_b64)
     body = '<html><body><p>{!s}</p><p>{!s}</p></body></html>'.format(proofing_state.nonce, image_tag)
     return make_response(body)
+
