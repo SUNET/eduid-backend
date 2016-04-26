c596d93510a9966995df83b27ed2a0bce16fcbb4
John Van de Meulebrouck Brendgard <john@nordu.net>
2016-04-26 10:20:57 +0200

    Bumped to version 0.2.1b2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = 'eduID User Database interface module'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.2.1b1'
+version = '0.2.1b2'
 
 install_requires = [
     'pymongo >= 2.8.0, < 3.0',
