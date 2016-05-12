f8ee1c6a5774ed278495dc9a334b8cde1fa7e9c1
Johan Lundberg <lundberg@nordu.net>
2016-05-12 15:14:44 +0200

    Added exception handling init code and better ApiException info

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-version = '0.2.1b4'
+version = '0.2.1b5'
 
 requires = [
     'setuptools >= 2.2',
--- src/eduid_common/api/exceptions.py
+++ src/eduid_common/api/exceptions.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from flask import jsonify
+
 __author__ = 'lundberg'
 
 
@@ -12,6 +14,18 @@ class ApiException(Exception):
         if status_code is not None:
             self.status_code = status_code
         self.payload = payload
+
+    def __repr__(self):
+        return u'ApiException(message={!s}, status_code={!s}, payload={!r})'.format(self.message, self.status_code,
+                                                                                    self.payload)
+
+    def __unicode__(self):
+        return self.__str__()
+
+    def __str__(self):
+        if self.payload:
+            return u'{!s} {!s} {!r}'.format(self.status_code, self.message, self.payload)
+        return u'{!s} {!s}'.format(self.status_code, self.message)
 
     def to_dict(self):
         rv = dict(self.payload or ())
@@ -27,3 +41,27 @@ class BadConfiguration(Exception):
 
     def __str__(self):
         return self.value
+
+
+def init_exception_handlers(app):
+
+    # Init error handler for raised exceptions
+    @app.errorhandler(ApiException)
+    def handle_flask_exception(error):
+        app.logger.error('ApiException {!s}'.format(error))
+        response = jsonify(error.to_dict())
+        response.status_code = error.status_code
+        return response
+
+    # Init exception handler for input validation when webargs is used
+    try:
+        from webargs.flaskparser import parser as webargs_flaskparser
+    except ImportError:
+        pass
+    else:
+        @webargs_flaskparser.error_handler
+        def handle_webargs_exception(error):
+            app.logger.error('ApiException {!s}'.format(error))
+            raise ApiException('Unprocessable Entity', error.status_code, error.messages)
+
+    return app
