1d99eba4606ec88b2c706b66b6e69459f5a2f44c
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-05-24 08:28:40 +0200

    add authn deps

--- docker/setup.sh
+++ docker/setup.sh
@@ -15,7 +15,11 @@ apt-get -y install \
     libjpeg8-dev \
     zlib1g-dev \
     libfreetype6-dev \
-    libssl-dev
+    libssl-dev \
+    libxml2-dev \
+    libxslt1-dev \
+    xmlsec1 \
+    libxml2-utils
 
 apt-get clean
 rm -rf /var/lib/apt/lists/*
