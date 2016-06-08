8aab7c2034147bb4d61bba25ccbb985a1ff24e1c
Johan Lundberg <lundberg@nordu.net>
2016-06-08 13:43:47 +0200

    Moved common config to project common.py and move config values to dev.yaml

--- src/eduid_webapp/authn/settings/common.py
+++ src/eduid_webapp/authn/settings/common.py
@@ -1,19 +1,14 @@
 # -*- coding: utf-8 -*-
 __author__ = 'lundberg'
 
-SECRET_KEY = 'supersecretkey'
-MONGO_URI = 'mongodb://eduid_authn:eduid_authn_pw@mongodb.eduid_dev'
-TESTING = False
-SESSION_COOKIE_PATH = '/'
-SESSION_COOKIE_HTTPONLY = False
-SESSION_COOKIE_SECURE = False
-PERMANENT_SESSION_LIFETIME = 3600
-LOGGER_NAME = 'eduid_webapp.authn'
-SERVER_NAME = 'authn.eduid.docker:8080'
-REDIS_PORT = 6379
-REDIS_DB = 0
+SERVER_NAME = ''
+SECRET_KEY = ''
+MONGO_URI = 'mongodb://'
+REDIS_HOST = ''
+
+
 required_loa = {
     'personal': 'http://www.swamid.se/policy/assurance/al1',
     'helpdesk': 'http://www.swamid.se/policy/assurance/al2',
     'admin': 'http://www.swamid.se/policy/assurance/al3',
-}
+}  # Should be changed to uppercase
