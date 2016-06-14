b1ec5088d70fd7ee9b15731e6af3c1bdfb2b0d17
Johan Lundberg <lundberg@nordu.net>
2016-06-14 12:55:32 +0200

    Initiating root logger to capture all log messages

--- src/eduid_common/api/logging.py
+++ src/eduid_common/api/logging.py
@@ -9,10 +9,14 @@ from eduid_common.api.exceptions import BadConfiguration
 __author__ = 'lundberg'
 
 
-def rotating(app):
+def rotating(app, root_logger):
     """
     :param app: Flask app
+    :param root_logger: Catch all logger object
+
     :type app: flask.app.Flask
+    :type root_logger: logging.Logger
+
     :return: Flask app with rotating log handler
     :rtype: flask.app.Flask
 
@@ -26,7 +30,6 @@ def rotating(app):
     """
     app.config.setdefault('LOG_FILE', None)
     app.config.setdefault('LOG_FORMAT', '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    app.config.setdefault('LOG_LEVEL', 'INFO')
     app.config.setdefault('LOG_MAX_BYTES', 1000000)  # 1 MB
     app.config.setdefault('LOG_BACKUP_COUNT', 10)  # 10 x 1 MB
 
@@ -37,7 +40,7 @@ def rotating(app):
             handler.setLevel(app.config['LOG_LEVEL'])
             formatter = logging.Formatter(app.config['LOG_FORMAT'])
             handler.setFormatter(formatter)
-            app.logger.addHandler(handler)
+            root_logger.addHandler(handler)
             app.logger.info('Rotating log handler initiated')
         except AttributeError as e:
             raise BadConfiguration(e.message)
@@ -51,12 +54,15 @@ def init_logging(app):
     :return: Flask app with log handlers
     :rtype: flask.app.Flask
     """
+    app.config.setdefault('LOG_LEVEL', 'INFO')
     app.config.setdefault('LOG_TYPE', ['rotating'])
+    root_logger = logging.getLogger()
+    root_logger.setLevel(app.config['LOG_LEVEL'])
 
     for log_type in app.config['LOG_TYPE']:
         init_handler = globals().get(log_type)
         if init_handler:
-            app = init_handler(app)
+            app = init_handler(app, root_logger)
     return app
 
 
