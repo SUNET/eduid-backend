ec860d68d74101154d3dbb5f454aaa2580a20c21
Johan Lundberg <lundberg@nordu.net>
2016-06-21 17:05:06 +0200

    Do not try to import dump_cookie
    Rewrote get_session_cookie to a context manager name.

--- src/eduid_common/api/testing.py
+++ src/eduid_common/api/testing.py
@@ -35,11 +35,11 @@ import time
 import atexit
 import random
 import subprocess
+from contextlib import contextmanager
 from copy import deepcopy
 
 import redis
 import etcd
-from werkzeug.http import dump_cookie
 
 from eduid_userdb import User
 from eduid_userdb.data_samples import NEW_USER_EXAMPLE
@@ -125,16 +125,13 @@ class EduidAPITestCase(unittest.TestCase):
         """
         return config
 
-    def get_session_cookie(self, eppn):
-        with self.client.session_transaction() as sess:
+    @contextmanager
+    def session_cookie(self, client, eppn, server_name='localhost'):
+        with client.session_transaction() as sess:
             sess['user_eppn'] = eppn
             sess.persist()
-        return dump_cookie(self.app.config.get('SESSION_COOKIE_NAME'), sess._session.token,
-                           max_age=float(self.app.config.get('PERMANENT_SESSION_LIFETIME')),
-                           path=self.app.config.get('SESSION_COOKIE_PATH'),
-                           domain=self.app.config.get('SESSION_COOKIE_DOMAIN'),
-                           secure=self.app.config.get('SESSION_COOKIE_SECURE'),
-                           httponly=self.app.config.get('SESSION_COOKIE_HTTPONLY'))
+        self.client.set_cookie(server_name, key=self.app.config.get('SESSION_COOKIE_NAME'), value=sess._session.token)
+        yield client
 
 
 class RedisTemporaryInstance(object):
