92bd42820cae91438109c9259a5c19b7e13fb01a
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-08-11 12:08:43 +0200

    new js config microservice

---
+++ src/eduid_webapp/jsconfig/views.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 NORDUnet A/S
+# All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or
+#   without modification, are permitted provided that the following
+#   conditions are met:
+#
+#     1. Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#     2. Redistributions in binary form must reproduce the above
+#        copyright notice, this list of conditions and the following
+#        disclaimer in the documentation and/or other materials provided
+#        with the distribution.
+#     3. Neither the name of the NORDUnet nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+from __future__ import absolute_import
+
+from flask import json
+from flask import Blueprint, current_app, request, abort
+
+from eduid_userdb.exceptions import UserOutOfSync
+from eduid_common.api.decorators import require_dashboard_user
+from eduid_common.api.utils import save_dashboard_user
+from eduid_webapp.personal_data.schemas import PersonalDataSchema
+
+jsconfig_views = Blueprint('jsconfig', __name__, url_prefix='')
+
+
+@jsconfig_views.route('/get-config', methods=['GET'])
+def get_config():
+    data = {
+        'available_languages': current_app.config.get('AVAILABLE_LANGUAGES'),
+        }
+    return json.jsonify({
+            'type': 'GET_CONFIG_SUCCESS',
+            'payload': data,
+            })
