cacd5add638ee48d419d0b4d63e5bf5b8b613b1f
Johan Lundberg <lundberg@nordu.net>
2016-08-24 13:34:59 +0200

    Added test for pdf creation

--- src/eduid_webapp/letter_proofing/tests/test_pdf.py
+++ src/eduid_webapp/letter_proofing/tests/test_pdf.py
@@ -4,6 +4,10 @@ from __future__ import absolute_import
 
 import unittest
 from collections import OrderedDict
+from datetime import datetime
+from StringIO import StringIO
+from eduid_common.api.testing import EduidAPITestCase
+from eduid_webapp.letter_proofing.app import init_letter_proofing_app
 from eduid_webapp.letter_proofing import pdf
 
 # We need to add Navet responses that we fail to handle
@@ -81,3 +85,42 @@ class FormatAddressTest(unittest.TestCase):
 
         for response in failing_navet_responses:
             self.assertRaises(pdf.FormatException, pdf.format_address, response)
+
+
+class CreatePDFTest(EduidAPITestCase):
+
+    def load_app(self, config):
+        """
+        Called from the parent class, so we can provide the appropriate flask
+        app for this test case.
+        """
+        return init_letter_proofing_app('testing', config)
+
+    def update_config(self, config):
+        config.update({
+            'LETTER_WAIT_TIME_HOURS': 336,
+            'CELERY_CONFIG': {
+                'BROKER_URL': 'amqp://dummy',
+                'CELERY_RESULT_BACKEND': 'amqp',
+                'CELERY_TASK_SERIALIZER': 'json'
+            },
+        })
+        return config
+
+    def test_create_pdf(self):
+
+        recipient = OrderedDict([
+                (u'Name', OrderedDict([
+                    (u'GivenNameMarking', u'20'), (u'GivenName', u'Testaren Test'),
+                    (u'MiddleName', u'Tester'), (u'Surname', u'Testsson')])),
+                (u'OfficialAddress', OrderedDict([(u'Address2', u'\xd6RGATAN 79 LGH 10'),
+                                                  (u'Address1', u'LGH 4321'),
+                                                  (u'CareOf', u'TESTAREN & TESTSSON'),
+                                                  (u'PostalCode', u'12345'),
+                                                  (u'City', u'LANDET')]))
+            ])
+
+        with self.app.app_context():
+            pdf_document = pdf.create_pdf(recipient, verification_code='bogus code',
+                                          created_timestamp=datetime.utcnow(), primary_mail_address='test@example.org')
+            self.assertIsInstance(pdf_document, StringIO)
