c674346f9142e23a9bf384b971cf9e835419cf96
Johan Lundberg <lundberg@nordu.net>
2016-08-26 15:46:24 +0200

    Added proofing user and proofing service user dbs

--- src/eduid_userdb/proofing/__init__.py
+++ src/eduid_userdb/proofing/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .proofing_state import LetterProofingState, OidcProofingState
-from .proofingdb import LetterProofingStateDB, OidcProofingStateDB
+from eduid_userdb.proofing.user import ProofingUser
+from eduid_userdb.proofing.proofing_state import LetterProofingState, OidcProofingState
+from eduid_userdb.proofing.proofingdb import LetterProofingStateDB, OidcProofingStateDB
+from eduid_userdb.proofing.proofingdb import OidcProofingUserDB, LetterProofingUserDB
