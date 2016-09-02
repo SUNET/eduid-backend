78072262aad298df749a6fdb052b955493a03163
Johan Lundberg <lundberg@nordu.net>
2016-09-02 14:19:52 +0200

    Removed acceptable_user_types

--- src/eduid_common/api/am.py
+++ src/eduid_common/api/am.py
@@ -9,51 +9,47 @@ from eduid_am.tasks import update_attributes_keep_result
 __author__ = 'lundberg'
 
 
-def init_relay(app, application_name, acceptable_user_types):
+def init_relay(app, application_name):
     """
     :param app: Flask app
     :type app:
     :param application_name: Name to help am find the entry point for the am plugin
     :type application_name: str|unicode
-    :param acceptable_user_types: User types to try to sync
-    :type acceptable_user_types: tuple
     :return: Flask app
     :rtype:
     """
     config = app.config['CELERY_CONFIG']
     config['BROKER_URL'] = app.config['AM_BROKER_URL']
     eduid_am.celery.celery.conf.update(config)
-    app.am_relay = AmRelay(relay_for=application_name, acceptable_user_types=acceptable_user_types)
+    app.am_relay = AmRelay(relay_for=application_name)
     return app
 
 
 class AmRelay(object):
 
-    def __init__(self, relay_for, acceptable_user_types):
+    def __init__(self, relay_for):
         """
         :param relay_for: Name of application to relay for
         :type relay_for: str|unicode
-        :param acceptable_user_types: User types to try and sync
-        :type acceptable_user_types: tuple
         """
         self.relay_for = relay_for
-        self.acceptable_user_types = acceptable_user_types
 
-    def request_sync(self, user):
+    def request_user_sync(self, user):
         """
         Use Celery to ask eduid-am worker to propagate changes from our
         private UserDB into the central UserDB.
 
         :param user: User object
-        :type user: any(self.acceptable_user_types)
+        :type user: eduid_userdb.User
 
         :return:
         """
         # XXX: Do we need to check for acceptable_user_types?
-        if isinstance(user, self.acceptable_user_types):
+        try:
             user_id = str(user.user_id)
-        else:
-            raise ValueError('Can only propagate changes for {!s}.'.format(self.acceptable_user_types))
+        except (AttributeError, ValueError) as e:
+            current_app.logger.error('Bad user_id in sync request: {!s}'.format(e))
+            raise ValueError('Missing user_id. Can only propagate changes for eduid_userdb.User users.')
 
         current_app.logger.debug("Asking Attribute Manager to sync user {!s}".format(user))
         try:
