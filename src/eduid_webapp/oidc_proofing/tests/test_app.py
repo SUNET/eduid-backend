a04db319451493a74ff5195d97cf970795f2ccb7
Johan Lundberg <lundberg@nordu.net>
2016-10-20 12:21:15 +0200

    Boiler plate for oidc_proofing tests

--- src/eduid_webapp/oidc_proofing/tests/test_app.py
+++ src/eduid_webapp/oidc_proofing/tests/test_app.py
@@ -9,6 +9,7 @@ from datetime import datetime
 from collections import OrderedDict
 from mock import patch
 from bson import ObjectId
+from requests import Response
 
 from eduid_userdb.data_samples import NEW_USER_EXAMPLE
 from eduid_userdb.user import User
@@ -37,6 +38,48 @@ class AppTests(EduidAPITestCase):
 
         self.client = self.app.test_client()
 
+        self.oidc_provider_config = {
+            'authorization_endpoint': 'https://example.com/op/authentication',
+            'claims_parameter_supported': True,
+            'grant_types_supported': [
+                'authorization_code',
+                'implicit'
+            ],
+            'id_token_signing_alg_values_supported': [
+                'RS256'
+            ],
+            'issuer': 'https://example.com/op/',
+            'jwks_uri': 'https://example.com/op/jwks',
+            'response_modes_supported': [
+                'query',
+                'fragment'
+            ],
+            'response_types_supported': [
+                'code',
+                'code id_token',
+                'code token',
+                'code id_token token'
+            ],
+            'scopes_supported': [
+                'openid'
+            ],
+            'subject_types_supported': [
+                'pairwise'
+            ],
+            'token_endpoint': 'https://example.com/op/token',
+            'token_endpoint_auth_methods_supported': [
+                'client_secret_basic'
+            ],
+            'userinfo_endpoint': 'https://example.com/op/userinfo'
+        }
+
+        class MockResponse(object):
+            def __init__(self, status_code, text):
+                self.status_code = status_code
+                self.text = text
+
+        self.oidc_provider_config_response = MockResponse(200, json.dumps(self.oidc_provider_config))
+
         # Replace user with one without previous proofings
         userdata = deepcopy(NEW_USER_EXAMPLE)
         del userdata['nins']
@@ -59,6 +102,13 @@ class AppTests(EduidAPITestCase):
                 'CELERY_RESULT_BACKEND': 'amqp',
                 'CELERY_TASK_SERIALIZER': 'json'
             },
+            'PROVIDER_CONFIGURATION_INFO': {
+                'issuer': 'https://example.com'
+            },
+            'CLIENT_REGISTRATION_INFO': {
+                'client_id': 'test_client',
+                'client_secret': 'secret'
+            }
         })
         return config
 
@@ -68,9 +118,11 @@ class AppTests(EduidAPITestCase):
             self.app.proofing_statedb._drop_whole_collection()
             self.app.central_userdb._drop_whole_collection()
 
-    def test_authenticate(self):
-        response = self.client.get('/proofing')
-        self.assertEqual(response.status_code, 302)  # Redirect to token service
-        with self.session_cookie(self.client, self.test_user_eppn) as client:
-            response = client.get('/proofing')
-        self.assertEqual(response.status_code, 200)  # Authenticated request
+    #@patch('eduid_webapp.oidc_proofing.app.Client.http_request')
+    #def test_authenticate(self, mock_response):
+    #    mock_response.return_value = self.oidc_provider_config_response
+    #    response = self.client.get('/proofing')
+    #    self.assertEqual(response.status_code, 302)  # Redirect to token service
+    #    with self.session_cookie(self.client, self.test_user_eppn) as client:
+    #        response = client.get('/proofing')
+    #    self.assertEqual(response.status_code, 200)  # Authenticated request
