0161aed6177c53b3273e3ec6e5d74297153454d8
Johan Lundberg <lundberg@nordu.net>
2016-10-20 12:24:04 +0200

    Use example users from data_samples

--- src/eduid_userdb/tests/test_support_models.py
+++ src/eduid_userdb/tests/test_support_models.py
@@ -1,4 +1,5 @@
-from eduid_userdb.testing import MOCKED_USER_STANDARD
+from eduid_userdb.data_samples import NEW_USER_EXAMPLE, NEW_SIGNUP_USER_EXAMPLE, NEW_DASHBOARD_USER_EXAMPLE
+from eduid_userdb.data_samples import OLD_USER_EXAMPLE, OLD_VERIFICATIONS_EXAMPLE
 from eduid_userdb.support import models
 
 from unittest import TestCase
@@ -6,24 +7,30 @@ from unittest import TestCase
 
 class TestSupportUsers(TestCase):
 
+    def test_old_support_user(self):
+        user = models.SupportUser(OLD_USER_EXAMPLE)
+        self.assertNotIn('_id', user)
+        self.assertNotIn('letter_proofing_data', user)
+        for password in user['passwords']:
+            self.assertNotIn('salt', password)
+
     def test_support_user(self):
-        user = models.SupportUser(MOCKED_USER_STANDARD)
+        user = models.SupportUser(NEW_USER_EXAMPLE)
         self.assertNotIn('_id', user)
         self.assertNotIn('letter_proofing_data', user)
         for password in user['passwords']:
             self.assertNotIn('salt', password)
 
     def test_support_dashboard_user(self):
-        user = models.SupportDashboardUser(MOCKED_USER_STANDARD)
+        user = models.SupportDashboardUser(NEW_DASHBOARD_USER_EXAMPLE)
         self.assertNotIn('_id', user)
         self.assertNotIn('letter_proofing_data', user)
         for password in user['passwords']:
             self.assertNotIn('salt', password)
 
     def test_support_signup_user(self):
-        user = models.SupportDashboardUser(MOCKED_USER_STANDARD)
+        user = models.SupportSignupUser(NEW_SIGNUP_USER_EXAMPLE)
         self.assertNotIn('_id', user)
         self.assertNotIn('letter_proofing_data', user)
         for password in user['passwords']:
             self.assertNotIn('salt', password)
-
