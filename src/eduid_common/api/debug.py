29d7288663da467164b38d149176eaefa48059f6
Johan Lundberg <lundberg@nordu.net>
2016-11-22 16:44:09 +0100

    Log endpoints and config when using app debug

--- src/eduid_common/api/debug.py
+++ src/eduid_common/api/debug.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import pprint
+import urllib
+from flask import url_for
 
 __author__ = 'lundberg'
 
@@ -20,6 +23,30 @@ class LoggingMiddleware(object):
         return self._app(environ, log_response)
 
 
+def log_endpoints(app):
+    output = []
+    with app.app_context():
+        for rule in app.url_map.iter_rules():
+
+            options = {}
+            for arg in rule.arguments:
+                options[arg] = "[{0}]".format(arg)
+
+            methods = ','.join(rule.methods)
+            url = url_for(rule.endpoint, **options)
+            line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+            output.append(line)
+
+        for line in sorted(output):
+            pprint.pprint(('ENDPOINT', line), stream=sys.stderr)
+
+
+def dump_config(app):
+    pprint.pprint(('CONFIGURATION', app.config), stream=sys.stderr)
+
+
 def init_app_debug(app):
     app.wsgi_app = LoggingMiddleware(app.wsgi_app)
+    log_endpoints(app)
+    dump_config(app)
     return app
