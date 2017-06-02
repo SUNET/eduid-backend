35be28a50719937e9729e48333df0b1834bc76f7
Fredrik Thulin <fredrik@thulin.net>
2017-06-02 12:50:15 +0200

    version 0.2.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = 'eduID User Database interface module'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.2.6b4'
+version = '0.2.6'
 
 install_requires = [
     'pymongo >= 2.8.0, < 3.0',
