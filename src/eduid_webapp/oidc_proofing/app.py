ca92040a03945d5ea8d0b04a1456292a76f94742
Johan Lundberg <lundberg@sunet.se>
2017-06-21 14:00:10 +0200

    Added retry for fetching configuration from OP
    Reviewed by John

--- src/eduid_webapp/oidc_proofing/app.py
+++ src/eduid_webapp/oidc_proofing/app.py
@@ -2,6 +2,8 @@
 
 from __future__ import absolute_import
 
+from time import sleep
+from sys import exit
 from requests.exceptions import ConnectionError
 from oic.oic import Client
 from oic.oic.message import RegistrationRequest
@@ -20,12 +22,20 @@ def init_oidc_client(app):
     oidc_client = Client(client_authn_method=CLIENT_AUTHN_METHOD)
     oidc_client.store_registration_info(RegistrationRequest(**app.config['CLIENT_REGISTRATION_INFO']))
     provider = app.config['PROVIDER_CONFIGURATION_INFO']['issuer']
+
     try:
         oidc_client.provider_config(provider)
-    except ConnectionError as e:
-        app.logger.critical('No connection to provider {!s}. Can not start without provider configuration.'.format(
-            provider))
-        raise e
+    except ConnectionError:
+        app.logger.warning('No connection to provider {!s}. Can not start without provider configuration.'
+                           ' Retrying...'.format(provider))
+        # Retry after 20 seconds
+        sleep(20)  # Hack until we use new frontends
+        try:
+            oidc_client.provider_config(provider)
+        except ConnectionError:
+            app.logger.critical('No connection to provider {!s}. Can not start without provider configuration.'
+                                ' Exiting.'.format(provider))
+            exit(1)
     return oidc_client
 
 
@@ -59,12 +69,12 @@ def init_oidc_proofing_app(name, config):
     # Register view path that should not be authorized
     app = no_authn_views(app, ['/authorization-response'])
 
+    # Initialize the oidc_client after views to be able to set correct redirect_uris
+    app.oidc_client = init_oidc_client(app)
+
     # Init celery
     app = msg.init_relay(app)
     app = am.init_relay(app, 'eduid_oidc_proofing')
-
-    # Initialize the oidc_client after views to be able to set correct redirect_uris
-    app.oidc_client = init_oidc_client(app)
 
     # Initialize db
     app.proofing_statedb = OidcProofingStateDB(app.config['MONGO_URI'])
