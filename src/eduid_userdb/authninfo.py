5f205d539557f6056861fa204917b256082af3b7
Johan Lundberg <lundberg@sunet.se>
2017-09-29 17:36:16 +0200

    Do not exclude credentials that never has been used

--- src/eduid_userdb/authninfo.py
+++ src/eduid_userdb/authninfo.py
@@ -15,8 +15,7 @@ __author__ = 'eperez'
 
 class AuthnInfoDB(BaseDB):
 
-    def __init__(self, db_uri,
-            db_name = 'eduid_idp_authninfo', collection = 'authn_info'):
+    def __init__(self, db_uri, db_name='eduid_idp_authninfo', collection='authn_info'):
         super(AuthnInfoDB, self).__init__(db_uri, db_name, collection)
 
     def get_authn_info(self, user):
@@ -28,19 +27,19 @@ class AuthnInfoDB(BaseDB):
         """
         authninfo = []
         for credential in user.credentials.to_list():
+            created_ts = credential.created_ts.isoformat()
+            success_ts = None
+            data_type = 'security.unknown_credential_type'
+            if isinstance(credential, Password):
+                data_type = 'security.password_credential_type'
+            elif isinstance(credential, U2F):
+                data_type = 'security.u2f_credential_type'
+
             auth_entry = self._coll.find_one(credential.key)
             logger.debug("get_authn_info {!s}: cred id: {!r} auth entry: {!r}".format(
                 user, credential.key, auth_entry))
             if auth_entry:
-                created_dt = credential['created_ts']
-                success_dt = auth_entry['success_ts']
-                data_type = 'security.unknown_credential_type'
-                if isinstance(credential, Password):
-                    data_type = 'security.password_credential_type'
-                elif isinstance(credential, U2F):
-                    data_type = 'security.u2f_credential_type'
-                data = {'credential_type': data_type,
-                        'created_ts': created_dt.isoformat(),
-                        'success_ts': success_dt.isoformat()}
-                authninfo.append(data)
+                success_ts = auth_entry['success_ts'].isoformat()
+
+            authninfo.append({'credential_type': data_type, 'created_ts': created_ts, 'success_ts': success_ts})
         return authninfo
