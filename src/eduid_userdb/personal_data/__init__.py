38e622843667b1681c2361afc502538b4881221d
Johan Lundberg <lundberg@sunet.se>
2017-10-02 18:08:02 +0200

    Added private db classes for personal_data and security microservices

---
+++ src/eduid_userdb/personal_data/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from eduid_userdb.personal_data.user import PersonalDataUser
+from eduid_userdb.personal_data.db import PersonalDataUserDB
+
+__author__ = 'lundberg'
---
+++ src/eduid_userdb/security/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from eduid_userdb.security.user import SecurityUser
+from eduid_userdb.security.db import SecurityUserDB
+
+__author__ = 'lundberg'
