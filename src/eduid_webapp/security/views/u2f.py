c7d0f53b906c2aba150cd6cdf18f22e6131bcace
Johan Lundberg <lundberg@sunet.se>
2017-10-03 17:43:57 +0200

    Initial sign and verify views

--- src/eduid_webapp/security/views/u2f.py
+++ src/eduid_webapp/security/views/u2f.py
@@ -64,30 +64,55 @@ def bind(user, version, registration_data, client_data):
 
 @u2f_views.route('/sign', methods=['GET'])
 @MarshalWith(SignWithU2FTokenResponseSchema)
-@require_dashboard_user
+@require_user
 def sign(user):
-    pass
+    user_u2f_tokens = user.credentials.filter(U2F).to_list()
+    if not user_u2f_tokens:
+        current_app.logger.error('Found no U2F token for user.')
+        return {'_error': True, 'message': 'security.u2f.no_token_found'}
+    challenge = begin_authentication(current_app.config['UF2_APP_ID'], user_u2f_tokens)
+    session['_u2f_challenge_'] = challenge.json
+    return challenge.data_for_client
 
 
 @u2f_views.route('/verify', methods=['POST'])
 @UnmarshalWith(VerifyWithU2FTokenRequestSchema)
 @MarshalWith(VerifyWithU2FTokenResponseSchema)
-@require_dashboard_user
-def verify(user):
-    pass
+@require_user
+def verify(user, key_handle, signature_data, client_data):
+    challenge = session.pop('_u2f_challenge_')
+    if not challenge:
+        current_app.logger.error('Found no U2F challenge data in session.')
+        return {'_error': True, 'message': 'security.u2f.missing_challenge_data'}
+    data = {
+        'keyHandle': key_handle,
+        'signatureData': signature_data,
+        'clientData': client_data
+    }
+    device, c, t = complete_authentication(challenge, data, [current_app.config['SERVER_NAME']])
+    return {'keyHandle': device['keyHandle'], 'touch': t, 'counter': c}
 
 
 @u2f_views.route('/modify', methods=['POST'])
 @UnmarshalWith(ModifyU2FTokenRequestSchema)
 @MarshalWith(SecurityResponseSchema)
-@require_dashboard_user
-def modify(user):
-    pass
+@require_user
+def modify(user, key_handle, description):
+    token_to_modify = user.credentials.filter(U2F).find(key_handle)
+    if not token_to_modify:
+        current_app.logger.error('Did not find requested U2F token for user.')
+        return {'_error': True, 'message': 'security.u2f.missing_u2f_token'}
+    if len(description) > current_app.config['U2F_MAX_DESCRIPTION_LENGTH']:
+        current_app.logger.error('User tried to set a U2F token description longer than {}.'.format(
+            current_app.config['U2F_MAX_DESCRIPTION_LENGTH']))
+        return {'_error': True, 'message': 'security.u2f.missing_u2f_token'}
+    token_to_modify.description = description
+    save_and_sync_user(user)
 
 
 @u2f_views.route('/remove', methods=['POST'])
 @UnmarshalWith(RemoveU2FTokenRequestSchema)
 @MarshalWith(SecurityResponseSchema)
-@require_dashboard_user
+@require_user
 def remove(user):
     pass
