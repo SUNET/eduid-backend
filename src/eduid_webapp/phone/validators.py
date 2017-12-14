2ef04f6d2ce6f44b3911beb8bc2e6004e345f351
Johan Lundberg <lundberg@sunet.se>
2017-12-14 10:39:32 +0100

    Update validators.py

--- src/eduid_webapp/phone/validators.py
+++ src/eduid_webapp/phone/validators.py
@@ -54,7 +54,7 @@ def validate_phone(number):
 
 def validate_format_phone(number):
 
-    if not re.match(r"^\+[1-9]\d{8,15}$", number):
+    if not re.match(r"^\+[1-9]\d{8,20}$", number):
         raise ValidationError("phone.phone_format")
 
 
