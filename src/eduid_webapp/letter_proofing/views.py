0efcda4d9792d863af24af6a845df29d4b41b9f6
Enrique Pérez Arnaud <enrique@cazalla.net>
2018-01-23 17:54:17 +0100

    add missing message

--- src/eduid_webapp/letter_proofing/views.py
+++ src/eduid_webapp/letter_proofing/views.py
@@ -29,7 +29,7 @@ def get_state(user):
     if proofing_state:
         current_app.logger.info('Found proofing state for user {}'.format(user))
         return check_state(proofing_state)
-    return {'_status': 'error', 'message': 'letter.no_state_found'}
+    return {'message': 'letter.no_state_found'}
 
 
 @letter_proofing_views.route('/proofing', methods=['POST'])
