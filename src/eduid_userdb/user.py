10eccb0bb748bf13581a04b3dee234be2b99a2b0
Fredrik Thulin <fredrik@thulin.net>
2018-01-30 17:10:52 +0100

    oops, removed raise_on_missing=False by mistake

--- src/eduid_userdb/user.py
+++ src/eduid_userdb/user.py
@@ -580,7 +580,7 @@ class User(object):
         :rtype: cls
         """
         user_dict = user.to_dict()
-        private_user = private_userdb.get_user_by_eppn(user.eppn)
+        private_user = private_userdb.get_user_by_eppn(user.eppn, raise_on_missing=False)
         if private_user is not None:
             user_dict['modified_ts'] = private_user.modified_ts
         return cls(data = user_dict)
