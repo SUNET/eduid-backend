a8f1a30ab6cdbbf13a8bbba552de9e6d3fba0f92
Johan Lundberg <lundberg@sunet.se>
2018-02-01 16:05:35 +0100

    Nitpicking and more logging of user termination

--- src/eduid_webapp/security/views/security.py
+++ src/eduid_webapp/security/views/security.py
@@ -61,11 +61,11 @@ from eduid_webapp.security.helpers import compile_credential_list
 security_views = Blueprint('security', __name__, url_prefix='', template_folder='templates')
 
 
-def error(err):
+def error_message(message):
     return {
         '_status': 'error',
-        'message': str(err)
-        }
+        'message': str(message)
+    }
 
 
 @security_views.route('/credentials', methods=['GET'])
@@ -125,26 +125,26 @@ def change_password(user, old_password, new_password):
     security_user = SecurityUser.from_user(user, current_app.private_userdb)
     authn_ts = session.get('reauthn-for-chpass', None)
     if authn_ts is None:
-        return error('chpass.no_reauthn')
+        return error_message('chpass.no_reauthn')
 
     now = datetime.utcnow()
     delta = now - datetime.fromtimestamp(authn_ts)
     timeout = current_app.config.get('CHPASS_TIMEOUT', 600)
     if int(delta.total_seconds()) > timeout:
-        return error('chpass.stale_reauthn')
+        return error_message('chpass.stale_reauthn')
 
     vccs_url = current_app.config.get('VCCS_URL')
     added = add_credentials(vccs_url, old_password, new_password, security_user, source='security')
 
     if not added:
         current_app.logger.debug('Problem verifying the old credentials for {!r}'.format(user))
-        return error('chpass.unable-to-verify-old-password')
+        return error_message('chpass.unable-to-verify-old-password')
 
     security_user.terminated = False
     try:
         save_and_sync_user(security_user)
     except UserOutOfSync:
-        return error('user-out-of-sync')
+        return error_message('user-out-of-sync')
 
     del session['reauthn-for-chpass']
 
@@ -212,7 +212,7 @@ def account_terminated(user):
     delta = now - datetime.fromtimestamp(authn_ts)
 
     if int(delta.total_seconds()) > 600:
-        return error('security.stale_authn_info')
+        return error_message('security.stale_authn_info')
 
     del session['reauthn-for-termination']
 
@@ -230,17 +230,20 @@ def account_terminated(user):
     try:
         save_and_sync_user(security_user)
     except UserOutOfSync:
-        return error('user-out-of-sync')
+        return error_message('user-out-of-sync')
 
     current_app.stats.count(name='security_account_terminated', value=1)
-    current_app.logger.info('Terminated account for user {!r}'.format(security_user))
+    current_app.logger.info('Terminated user account')
 
     # email the user
     send_termination_mail(security_user)
 
     session.invalidate()
+    current_app.logger.info('Invalidated session for user')
 
     site_url = current_app.config.get("EDUID_SITE_URL")
+    current_app.logger.info('Redirection user to user {}'.format(site_url))
+    # TODO: Add a account termination completed view to redirect to
     return redirect(site_url)
 
 
