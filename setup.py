232c4bb920e1b424344709ed988a9ff5364a83f1
Johan Lundberg <lundberg@sunet.se>
2018-03-01 14:25:34 +0100

    mongo_uri does no longer contain db name

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = 'eduID Lookup Mobile'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.0.6b4'
+version = '0.0.6b5'
 
 install_requires = [
     'eduid-userdb >= 0.0.2',
