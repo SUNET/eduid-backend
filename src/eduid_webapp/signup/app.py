b6a8a6182477d4fc27c8207a1ca45d7540050cbb
Enrique Pérez Arnaud <enrique@cazalla.net>
2018-04-09 11:25:03 +0200

    initial signup app skeleton

---
+++ src/eduid_webapp/signup/app.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 NORDUnet A/S
+# All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or
+#   without modification, are permitted provided that the following
+#   conditions are met:
+#
+#     1. Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#     2. Redistributions in binary form must reproduce the above
+#        copyright notice, this list of conditions and the following
+#        disclaimer in the documentation and/or other materials provided
+#        with the distribution.
+#     3. Neither the name of the NORDUnet nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+from __future__ import absolute_import
+
+from flask import Flask
+
+from eduid_common.api.app import eduid_init_app
+from eduid_common.api import mail_relay
+from eduid_common.api import am
+
+
+def signup_init_app(name, config):
+    """
+    Create an instance of an eduid signup app.
+
+    First, it will load the configuration from signup.settings.common
+    then any settings given in the `config` param.
+
+    Then, the app instance will be updated with common stuff by `eduid_init_app`,
+    all needed blueprints will be registered with it,
+    and finally the app is configured with the necessary db connections.
+
+    :param name: The name of the instance, it will affect the configuration loaded.
+    :type name: str
+    :param config: any additional configuration settings. Specially useful
+                   in test cases
+    :type config: dict
+
+    :return: the flask app
+    :rtype: flask.Flask
+    """
+
+    app = eduid_init_app(name, config, app_class=Flask)
+    app.config.update(config)
+
+    from eduid_webapp.signup.views import signup_views
+    app.register_blueprint(signup_views)
+
+    app = am.init_relay(app, 'eduid_signup')
+    app = mail_relay.init_relay(app)
+
+    app.logger.info('Init {} app...'.format(name))
+
+    return app
---
+++ src/eduid_webapp/signup/tests/test_app.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 NORDUnet A/S
+# All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or
+#   without modification, are permitted provided that the following
+#   conditions are met:
+#
+#     1. Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#     2. Redistributions in binary form must reproduce the above
+#        copyright notice, this list of conditions and the following
+#        disclaimer in the documentation and/or other materials provided
+#        with the distribution.
+#     3. Neither the name of the NORDUnet nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import json
+from mock import patch
+from flask import Flask
+
+from eduid_common.api.testing import EduidAPITestCase
+from eduid_webapp.signup.app import signup_init_app
+
+
+class SignupTests(EduidAPITestCase):
+
+    def load_app(self, config):
+        """
+        Called from the parent class, so we can provide the appropiate flask
+        app for this test case.
+        """
+        return signup_init_app('emails', config)
+
+    def update_config(self, config):
+        config.update({
+            'AVAILABLE_LANGUAGES': {'en': 'English','sv': 'Svenska'},
+            'MSG_BROKER_URL': 'amqp://dummy',
+            'AM_BROKER_URL': 'amqp://dummy',
+            'DASHBOARD_URL': '/profile/',
+            'CELERY_CONFIG': {
+                'CELERY_RESULT_BACKEND': 'amqp',
+                'CELERY_TASK_SERIALIZER': 'json',
+                'MONGO_URI': config['MONGO_URI'],
+            },
+        })
+        return config
+
+    def init_data(self):
+        self.app.private_userdb.save(self.app.private_userdb.UserClass(data=self.test_user.to_dict()), check_sync=False)
