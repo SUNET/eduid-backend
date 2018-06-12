74a7bafd371d0a7eb26653ec2d994e68d23b40c9
Johan Lundberg <lundberg@sunet.se>
2018-06-12 16:58:27 +0200

    Use user objects to look up aux data

---
+++ src/eduid_webapp/support/helpers.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from flask import current_app
+
+__author__ = 'lundberg'
+
+
+def get_credentials_aux_data(user):
+    """
+    :param user: User object
+    :type user: eduid_userdb.user.User
+    :return: Augmented credentials list
+    :rtype: list
+    """
+    credentials = []
+    for credential in user.credentials.to_list():
+        credential_info = current_app.support_authn_db.get_credential_info(credential.credential_id)
+        if credential_info:
+            credential_dict = credential.to_dict()
+            # Add success_ts
+            credential_dict['success_ts'] = credential_info['success_ts']
+            credentials.append(credential_dict)
+    return credentials
