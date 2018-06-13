b279598826b8d88c7a634234665730cdba9b75c8
Fredrik Thulin <fredrik@thulin.net>
2018-06-13 15:09:42 +0200

    add current proofing method ids

--- src/eduid_userdb/credentials/__init__.py
+++ src/eduid_userdb/credentials/__init__.py
@@ -4,3 +4,7 @@ from eduid_userdb.credentials.base import Credential
 from eduid_userdb.credentials.password import Password, password_from_dict
 from eduid_userdb.credentials.u2f import U2F, u2f_from_dict
 from eduid_userdb.credentials.list import CredentialList
+
+# well-known proofing methods
+METHOD_SWAMID_AL2_MFA    = 'SWAMID_AL2_MFA'
+METHOD_SWAMID_AL2_MFA_HI = 'SWAMID_AL2_MFA_HI'
