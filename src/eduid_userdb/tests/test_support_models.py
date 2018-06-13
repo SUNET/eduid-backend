18a3a74411e94da1d82b1ea7e88418374d1f528b
Johan Lundberg <lundberg@sunet.se>
2018-06-13 10:59:56 +0200

    Fix tests

--- src/eduid_userdb/tests/test_support_models.py
+++ src/eduid_userdb/tests/test_support_models.py
@@ -3,6 +3,8 @@ from eduid_userdb.data_samples import (NEW_USER_EXAMPLE,
                                       NEW_COMPLETED_SIGNUP_USER_EXAMPLE,
                                       NEW_DASHBOARD_USER_EXAMPLE)
 from eduid_userdb.data_samples import OLD_USER_EXAMPLE, OLD_VERIFICATIONS_EXAMPLE
+from eduid_userdb import User
+from eduid_userdb.signup import SignupUser
 from eduid_userdb.support import models
 
 from unittest import TestCase
@@ -11,35 +13,28 @@ from unittest import TestCase
 class TestSupportUsers(TestCase):
 
     def test_old_support_user(self):
-        user = models.SupportUser(OLD_USER_EXAMPLE)
+        user = models.SupportUserFilter(User(data=OLD_USER_EXAMPLE).to_dict())
         self.assertNotIn('_id', user)
         self.assertNotIn('letter_proofing_data', user)
         for password in user['passwords']:
             self.assertNotIn('salt', password)
 
     def test_support_user(self):
-        user = models.SupportUser(NEW_USER_EXAMPLE)
-        self.assertNotIn('_id', user)
-        self.assertNotIn('letter_proofing_data', user)
-        for password in user['passwords']:
-            self.assertNotIn('salt', password)
-
-    def test_support_dashboard_user(self):
-        user = models.SupportDashboardUser(NEW_DASHBOARD_USER_EXAMPLE)
+        user = models.SupportUserFilter(User(data=NEW_USER_EXAMPLE).to_dict())
         self.assertNotIn('_id', user)
         self.assertNotIn('letter_proofing_data', user)
         for password in user['passwords']:
             self.assertNotIn('salt', password)
 
     def test_support_signup_user(self):
-        user = models.SupportSignupUser(NEW_SIGNUP_USER_EXAMPLE)
+        user = models.SupportSignupUserFilter(SignupUser(data=NEW_SIGNUP_USER_EXAMPLE).to_dict())
         self.assertNotIn('_id', user)
         self.assertNotIn('letter_proofing_data', user)
         for password in user['passwords']:
             self.assertNotIn('salt', password)
 
     def test_support_completed_signup_user(self):
-        user = models.SupportSignupUser(NEW_COMPLETED_SIGNUP_USER_EXAMPLE)
+        user = models.SupportSignupUserFilter(SignupUser(data=NEW_COMPLETED_SIGNUP_USER_EXAMPLE).to_dict())
         self.assertNotIn('_id', user)
         self.assertNotIn('letter_proofing_data', user)
 
