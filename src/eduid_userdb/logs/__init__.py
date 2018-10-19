9113212cc9ad3b8df754c02bea197bf403f30c2e
Johan Lundberg <lundberg@sunet.se>
2018-10-19 17:04:28 +0200

    Added proofing elements for eduid-eidas

--- src/eduid_userdb/logs/__init__.py
+++ src/eduid_userdb/logs/__init__.py
@@ -2,7 +2,8 @@
 from __future__ import absolute_import
 
 from .element import MailAddressProofing, PhoneNumberProofing, TeleAdressProofing, TeleAdressProofingRelation
-from .element import LetterProofing, SeLegProofing, SeLegProofingFrejaEid, OrcidProofing
+from .element import LetterProofing, SeLegProofing, SeLegProofingFrejaEid, OrcidProofing, SwedenConnectProofing
+from .element import MFATokenProofing
 from .db import ProofingLog
 
 __author__ = 'lundberg'
