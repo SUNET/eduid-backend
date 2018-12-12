5ee51d37d8d3570c22dde9461724b552dcb08a96
Johan Lundberg <lundberg@sunet.se>
2018-12-12 11:21:43 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = 'eduID Message Manager'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.10.3b10'
+version = '0.10.3'
 
 install_requires = [
     'six>=1.11.0',
