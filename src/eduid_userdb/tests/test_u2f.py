d949edd51c68f9a15123d15f64d48e0ceeb99711
Fredrik Thulin <fredrik@thulin.net>
2018-12-21 12:01:39 +0100

    fix test cases

--- src/eduid_userdb/tests/test_u2f.py
+++ src/eduid_userdb/tests/test_u2f.py
@@ -36,8 +36,10 @@ _three_dict = {
     'public_key': 'foo',
 }
 
-def _keyid(kh):
-    return 'sha256:' + sha256(kh.encode('utf-8')).hexdigest()
+def _keyid(key):
+    return 'sha256:' + sha256(key['keyhandle'].encode('utf-8') +
+                              key['public_key'].encode('utf-8')
+                              ).hexdigest()
 
 
 class TestU2F(TestCase):
@@ -52,11 +54,13 @@ class TestU2F(TestCase):
         """
         Test that the 'key' property (used by CredentialList) works for the credential.
         """
-        this = self.one.find(_keyid('firstU2FElement'))
-        self.assertEqual(this.key, _keyid(this.keyhandle))
+        this = self.one.find(_keyid(_one_dict))
+        self.assertEqual(this.key, _keyid({'keyhandle': this.keyhandle,
+                                           'public_key': this.public_key,
+                                           }))
 
     def test_setting_invalid_keyhandle(self):
-        this = self.one.find(_keyid('firstU2FElement'))
+        this = self.one.find(_keyid(_one_dict))
         with self.assertRaises(eduid_userdb.exceptions.UserDBValueError):
             this.keyhandle = None
 
@@ -83,14 +87,14 @@ class TestU2F(TestCase):
         self.assertEqual(out['foo'], one['foo'])
 
     def test_created_by(self):
-        this = self.three.find(_keyid('thirdU2FElement'))
+        this = self.three.find(_keyid(_three_dict))
         this.created_by = 'unit test'
         self.assertEqual(this.created_by, 'unit test')
         with self.assertRaises(eduid_userdb.exceptions.UserDBValueError):
             this.created_by = False
 
     def test_modify_created_by(self):
-        this = self.three.find(_keyid('thirdU2FElement'))
+        this = self.three.find(_keyid(_three_dict))
         with self.assertRaises(eduid_userdb.exceptions.UserDBValueError):
             this.created_by = 1
         this.created_by = 'unit test'
@@ -100,14 +104,14 @@ class TestU2F(TestCase):
             this.created_by = 'test unit'
 
     def test_created_ts(self):
-        this = self.three.find(_keyid('thirdU2FElement'))
+        this = self.three.find(_keyid(_three_dict))
         this.created_ts = True
         self.assertIsInstance(this.created_ts, datetime.datetime)
         with self.assertRaises(eduid_userdb.exceptions.UserDBValueError):
             this.created_ts = False
 
     def test_modify_created_ts(self):
-        this = self.three.find(_keyid('thirdU2FElement'))
+        this = self.three.find(_keyid(_three_dict))
         this.created_ts = datetime.datetime.utcnow()
         with self.assertRaises(eduid_userdb.exceptions.UserDBValueError):
             this.created_ts = None
@@ -115,7 +119,7 @@ class TestU2F(TestCase):
             this.created_ts = True
 
     def test_proofing_method(self):
-        this = self.three.find(_keyid('thirdU2FElement'))
+        this = self.three.find(_keyid(_three_dict))
         this.proofing_method = 'TEST'
         self.assertEqual(this.proofing_method, 'TEST')
         this.proofing_method = 'TEST2'
@@ -127,7 +131,7 @@ class TestU2F(TestCase):
 
 
     def test_proofing_version(self):
-        this = self.three.find(_keyid('thirdU2FElement'))
+        this = self.three.find(_keyid(_three_dict))
         this.proofing_version = 'TEST'
         self.assertEqual(this.proofing_version, 'TEST')
         this.proofing_version = 'TEST2'
