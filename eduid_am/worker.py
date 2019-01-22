2d02df77a44595b92fc458594b040ece2643feb8
Fredrik Thulin <fredrik@thulin.net>
2019-01-22 14:36:38 +0100

    fix tests

--- eduid_am/worker.py
+++ eduid_am/worker.py
@@ -6,21 +6,22 @@ from eduid_common.rpc.celery import init_celery
 
 import eduid_am.common as common
 
+worker_config = {}
 
-worker_config = get_worker_config('am')
-celery = init_celery('eduid_am', config=worker_config['CELERY'], include=['eduid_am.tasks'])
+if common.celery is None:
+    worker_config = get_worker_config('am')
+    celery = init_celery('eduid_am', config=worker_config['CELERY'], include=['eduid_am.tasks'])
 
-# When Celery starts the worker, it expects there to be a 'celery' in the module it loads,
-# but our tasks expect to find the Celery instance in common.celery - so copy it there
-common.celery = celery
+    # When Celery starts the worker, it expects there to be a 'celery' in the module it loads,
+    # but our tasks expect to find the Celery instance in common.celery - so copy it there
+    common.celery = celery
 
 
-def setup_indexes(db_name, collection):
+def setup_indexes(db_uri, db_name, collection):
     """
     Ensure that indexes in eduid_am.attributes collection are correctly setup.
     To update an index add a new item in indexes and remove the previous version.
     """
-    global worker_config
     indexes = {
         # 'index-name': {'key': [('key', 1)], 'param1': True, 'param2': False}
         # http://docs.mongodb.org/manual/reference/method/db.collection.ensureIndex/
@@ -30,10 +31,11 @@ def setup_indexes(db_name, collection):
         'mobile-index-v1': {'key': [('mobile.mobile', 1), ('mobile.verified', 1)]},
         'mailAliases-index-v1': {'key': [('mailAliases.email', 1), ('mailAliases.verified', 1)]}
     }
-    userdb = UserDB(worker_config.get('MONGO_URI'), db_name=db_name, collection=collection)
+    userdb = UserDB(db_uri, db_name=db_name, collection=collection)
     userdb.setup_indexes(indexes)
 
 
-setup_indexes('eduid_am', 'attributes')
+if 'MONGO_URI' in worker_config:
+    setup_indexes(worker_config['MONGO_URI'], 'eduid_am', 'attributes')
 
 
