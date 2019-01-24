00f6369a50ae2210b3d48247cc72722d14ea902d
Fredrik Thulin <fredrik@thulin.net>
2019-01-24 11:31:41 +0100

    re-implement using a dict

--- eduid_lookup_mobile/development/nin_mobile_db.py
+++ eduid_lookup_mobile/development/nin_mobile_db.py
@@ -1,21 +1,18 @@
 __author__ = 'mathiashedstrom'
 
-_nins = ['200202025678', '197512125432', '195010106543', '190001019876']
-_mobiles = [['+46700011222'], ['+46700011333', '+46700011777'], ['+46700011444'], ['+46700011555', '+46700011666']]
+_db = {'200202025678': ['+46700011222'],
+       '197512125432': ['+46700011333', '+46700011777'],
+       '195010106543': ['+46700011444'],
+       '190001019876': ['+46700011555', '+46700011666']
+       }
+
 
 def get_mobile(nin):
-    if nin not in _nins:
-        return []
-
-    return _mobiles[_nins.index(nin)]
+    return _db.get(nin, [])
 
 
 def get_nin(mobile):
-
-    index = 0
-    for mobile_list in _mobiles:
-        if mobile in mobile_list:
-            return _nins[index]
-        index += 1
-
+    for nin, numbers in _db.items():
+        if mobile in numbers:
+            return nin
     return None
