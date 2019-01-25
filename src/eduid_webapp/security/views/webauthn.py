a65ef205607b6454bad5b419115595b4053360ba
Enrique Pérez Arnaud <enrique@cazalla.net>
2019-01-25 14:59:50 +0100

    webauthn registration - WIP

--- src/eduid_webapp/security/views/webauthn.py
+++ src/eduid_webapp/security/views/webauthn.py
@@ -3,6 +3,7 @@
 from __future__ import print_function, absolute_import, unicode_literals
 
 import json
+import base64
 from flask import Blueprint, session, Response
 from flask import current_app
 
@@ -20,7 +21,7 @@ from eduid_userdb.security import SecurityUser
 from eduid_common.api.decorators import require_user, MarshalWith, UnmarshalWith
 from eduid_common.api.utils import save_and_sync_user
 from eduid_webapp.security.helpers import credentials_to_registered_keys, compile_credential_list
-from eduid_webapp.security.schemas import WebauthnOptionsResponseSchema
+from eduid_webapp.security.schemas import WebauthnOptionsResponseSchema, WebauthnRegistrationRequestSchema
 from eduid_webapp.security.schemas import SecurityResponseSchema
 
 
@@ -41,7 +42,7 @@ def get_webauthn_server():
 
 class Credential:
     def __init__(self, id):
-        self.credential_id = id
+        self.credential_id = id.encode('ascii')
 
 def make_credentials(creds):
     return [Credential(cred.key) for cred in creds]
@@ -63,7 +64,7 @@ def registration_begin(user):
     creds = make_credentials(user_webauthn_tokens.to_list())
     server = get_webauthn_server()
     registration_data, state = server.register_begin({
-        'id': str(user.user_id),
+        'id': str(user.user_id).encode('ascii'),
         'name': user.surname,
         'displayName': user.display_name,
         'icon': ''
@@ -81,14 +82,21 @@ def registration_begin(user):
 
 @webauthn_views.route('/register/complete', methods=['POST'])
 @MarshalWith(SecurityResponseSchema)
+@UnmarshalWith(WebauthnRegistrationRequestSchema)
 @require_user
-def registration_complete(user, attestation, client_data_json):
+def registration_complete(user, description, attestation, client_data_json):
     security_user = SecurityUser.from_user(user, current_app.private_userdb)
-    server = get_webauthn_server(current_app.config['WEBAUTHN_RP_ID'])
-    att_obj = AttestationObject(attestation)
-    client_data = ClientData(client_data_json)
+    server = get_webauthn_server()
+    att_obj = AttestationObject(base64.b64decode(attestation))
+    client_data = ClientData(base64.b64decode(client_data_json))
     state = session['_webauthn_state_']
     auth_data = server.register_complete(state, client_data, att_obj)
+
+    key_handle = auth_data.credential_data.credential_id
+    public_key = auth_data.credential_data.public_key
+    app_id = current_app.config['WEBAUTHN_RP_ID']
+
+    raise Exception('SssssssssSSSSssSSSSSSSSSSSssSSSss    ' + str(auth_data) + '   gfgfgfgfgfg   ' + str(client_data) + '      dkduelpsidldiokfv     ' + str(state) + '       mnmnmnmnmnm     ' + str(att_obj))
     security_user.credentials.add(auth_data.credential_data)
     save_and_sync_user(security_user)
     current_app.stats.count(name='webauthn_register_complete')
