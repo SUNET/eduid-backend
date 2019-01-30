9ca45faa31f464bf6dce17f19a1d4f6670bdeacd
Johan Lundberg <lundberg@sunet.se>
2019-01-30 14:38:13 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = 'eduID User Database interface module'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.4.6b0'
+version = '0.4.6b1'
 
 install_requires = [
     'pymongo >= 3.6',
