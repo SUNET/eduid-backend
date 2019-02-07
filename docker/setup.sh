c0b65260d87860fa41cf1cf04938cda0586e1930
Johan Lundberg <lundberg@sunet.se>
2019-02-07 17:52:53 +0100

    Set locale

--- docker/setup.sh
+++ docker/setup.sh
@@ -7,6 +7,13 @@ apt-get update
 apt-get -y install \
     libxml2-dev \
     libxslt-dev \
-    zlib1g-dev
+    zlib1g-dev \
+    locales  # tmp test
 apt-get clean
 rm -rf /var/lib/apt/lists/*
+
+# tmp test
+locale-gen en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US:en
+
