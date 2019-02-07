c0a97b4f69355fd341f1e45a14868a1c81eea1a3
Fredrik Thulin <fredrik@thulin.net>
2019-02-07 15:05:13 +0100

    fix tests

--- eduid_lookup_mobile/test/test_decorators.py
+++ eduid_lookup_mobile/test/test_decorators.py
@@ -10,7 +10,7 @@ class TestTransactionAudit(LookupMobileMongoTestCase):
     def setUp(self):
         super(TestTransactionAudit, self).setUp()
         # need to set self.mongo_uri and db for the TransactionAudit decorator
-        self.mongo_uri = self.tmp_db.uri
+        self.conf = {'MONGO_URI': self.tmp_db.uri}
         self.db = self.tmp_db.conn['eduid_lookup_mobile']
 
         self.transaction_audit = True
