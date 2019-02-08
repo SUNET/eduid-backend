330e673c20a79d2fcaef0f9aa1377902deebc52e
Johan Lundberg <lundberg@sunet.se>
2019-02-08 12:36:44 +0100

    Another test to set locale

--- docker/setup.sh
+++ docker/setup.sh
@@ -7,10 +7,7 @@ apt-get update
 apt-get -y install \
     libxml2-dev \
     libxslt-dev \
-    zlib1g-dev \
-    locales  # tmp test
+    zlib1g-dev
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
-# tmp test
-locale-gen en_US.UTF-8
