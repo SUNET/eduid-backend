21954e250f2e8d148bffe012a74085d23682094f
Johan Lundberg <lundberg@sunet.se>
2019-06-17 14:23:20 +0200

    Updated the test NINs to pass the Luhn check

--- eduid_lookup_mobile/test/test_lookup.py
+++ eduid_lookup_mobile/test/test_lookup.py
@@ -11,15 +11,15 @@ class TestVerifiers(LookupMobileMongoTestCase):
         from eduid_lookup_mobile.tasks import logger
         mobile_verifier = MobileLookupClient(logger, self.lookup_mobile_settings)
 
-        self.assertEqual(mobile_verifier.find_NIN_by_mobile('+46700011222'), '200202025678')
-        self.assertEqual(mobile_verifier.find_NIN_by_mobile('+46700011333'), '197512125432')
-        self.assertEqual(mobile_verifier.find_NIN_by_mobile('+46700011777'), '197512125432')
+        self.assertEqual(mobile_verifier.find_NIN_by_mobile('+46700011222'), '200202027140')
+        self.assertEqual(mobile_verifier.find_NIN_by_mobile('+46700011333'), '197512126371')
+        self.assertEqual(mobile_verifier.find_NIN_by_mobile('+46700011777'), '197512126371')
         self.assertEqual(mobile_verifier.find_NIN_by_mobile('+46700011999'), None)
 
     def test_find_mobiles_by_NIN(self):
         from eduid_lookup_mobile.tasks import logger
         mobile_verifier = MobileLookupClient(logger, self.lookup_mobile_settings)
 
-        self.assertEqual(mobile_verifier.find_mobiles_by_NIN('200202025678'), ['+46700011222'])
-        self.assertEqual(mobile_verifier.find_mobiles_by_NIN('197512125432'), ['+46700011333', '+46700011777'])
+        self.assertEqual(mobile_verifier.find_mobiles_by_NIN('200202027140'), ['+46700011222'])
+        self.assertEqual(mobile_verifier.find_mobiles_by_NIN('197512126371'), ['+46700011333', '+46700011777'])
         self.assertEqual(mobile_verifier.find_mobiles_by_NIN('197512125430'), [])
