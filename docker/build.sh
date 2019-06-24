fc6a7cce398f81c0983b98797eb863b416e73a9f
Johan Lundberg <lundberg@sunet.se>
2019-06-24 16:48:46 +0200

    Do not install deprecated am plugins
    Remove check for local pypi server

--- docker/build.sh
+++ docker/build.sh
@@ -6,19 +6,12 @@
 set -e
 set -x
 
-PYPI="https://pypi.sunet.se/simple/"
-ping -c 1 -q pypiserver.docker && PYPI="http://pypiserver.docker:8080/simple/"
+PYPI=${PYPI-'https://pypi.sunet.se/simple/'}
 
 echo "#############################################################"
 echo "$0: Using PyPi URL ${PYPI}"
 echo "#############################################################"
 
 /opt/eduid/bin/pip install --pre -i ${PYPI} /src/eduid-am
-# eduid-api currently doesn't do attribute updating
-#/opt/eduid/bin/pip install --pre -i ${PYPI} eduid-api-amp
-/opt/eduid/bin/pip install --pre -i ${PYPI} eduid-signup-amp
-/opt/eduid/bin/pip install --pre -i ${PYPI} eduid-dashboard-amp
-/opt/eduid/bin/pip install --pre -i ${PYPI} eduid-proofing-amp
-/opt/eduid/bin/pip install --pre -i ${PYPI} eduid_action
 
 /opt/eduid/bin/pip freeze
