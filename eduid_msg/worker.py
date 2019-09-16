9f5f6120ac9add849c8c0e798f68fcdd375eeda7
enriquepablo <enrique@cazalla.net>
2019-09-16 15:26:22 +0200

    using new config classes

--- eduid_msg/worker.py
+++ eduid_msg/worker.py
@@ -1,13 +1,15 @@
 from eduid_common.rpc.worker import get_worker_config
 from eduid_common.rpc.celery import init_celery
+from eduid_common.config.base import CommonConfig
+from eduid_common.config.workers import MsgConfig
 
 import eduid_msg.common as common
 
-worker_config: dict = {}
+worker_config: MsgConfig = MsgConfig()
 
 if common.celery is None:
-    worker_config = get_worker_config('msg')
-    celery = init_celery('eduid_msg', config=worker_config['CELERY'], include=['eduid_msg.tasks'])
+    worker_config = get_worker_config('msg', config_class=MsgConfig)
+    celery = init_celery('eduid_msg', config=worker_config.celery, include=['eduid_msg.tasks'])
 
     # When Celery starts the worker, it expects there to be a 'celery' in the module it loads,
     # but our tasks expect to find the Celery instance in eduid_am.common.celery - so copy it there
