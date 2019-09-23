812ae8e68b928955d16bec0bb97023e90d838501
Johan Lundberg <lundberg@sunet.se>
2019-09-23 15:26:37 +0200

    Revert "Eperez refactor testing"

--- src/eduid_userdb/tests/test_actions_db.py
+++ src/eduid_userdb/tests/test_actions_db.py
@@ -87,7 +87,7 @@ TOU_ACTION_USERID = {
 class TestActionsDB(MongoTestCase):
 
     def setUp(self):
-        super(TestActionsDB, self).setUp()
+        super(TestActionsDB, self).setUp(None, None)
         self.actionsdb = ActionDB(self.tmp_db.uri)
         self.actionsdb.add_action(data=TOU_ACTION)
         self.actionsdb.add_action(data=DUMMY_ACTION)
@@ -124,7 +124,7 @@ class TestActionsDB(MongoTestCase):
 class TestActionsDBUserid(MongoTestCase):
 
     def setUp(self):
-        super(TestActionsDBUserid, self).setUp()
+        super(TestActionsDBUserid, self).setUp(None, None)
         self.actionsdb = ActionDB(self.tmp_db.uri)
         self.actionsdb.add_action(data=TOU_ACTION_USERID)
         self.actionsdb.add_action(data=DUMMY_ACTION_USERID)
