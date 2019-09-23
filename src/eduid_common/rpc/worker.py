c7d98b42de333746cdcc94b22eae2ff913351a61
enriquepablo <enrique@cazalla.net>
2019-09-23 15:50:05 +0200

    fix configuration of workers

--- src/eduid_common/rpc/worker.py
+++ src/eduid_common/rpc/worker.py
@@ -8,6 +8,7 @@ from typing import Type, Dict, Any
 from eduid_common.config.exceptions import BadConfiguration
 from eduid_common.config.parsers.etcd import EtcdConfigParser
 from eduid_common.config.base import CommonConfig, BaseConfig
+from eduid_common.config.workers import AmConfig, MsgConfig, MobConfig
 
 
 def get_worker_config(name: str, config_class: Type[CommonConfig] = BaseConfig) -> CommonConfig:
@@ -28,7 +29,13 @@ def get_worker_config(name: str, config_class: Type[CommonConfig] = BaseConfig) 
     cfg.update(common_parser.read_configuration(silent=True))
     cfg.update(app_parser.read_configuration(silent=True))
     cfg = {key.lower(): value for key, value in cfg.items()}
+    if name == 'msg':
+        config_class = MsgConfig
+    elif name == 'am':
+        config_class = AmConfig
+    elif name == 'lookup_mobile':
+        config_class = MobConfig
     config = config_class(**cfg)
-    if not config.celery.broker_url:
+    if 'broker_url' not in config.celery:
         raise BadConfiguration('broker_url for celery is missing')
     return config
