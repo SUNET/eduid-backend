97ccac1c5916bbab268b95d26218620124b2ff43
enriquepablo <enrique@cazalla.net>
2019-09-24 15:32:20 +0200

    new config and tests

--- eduid_msg/testing.py
+++ eduid_msg/testing.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import pkg_resources
 
 from eduid_userdb.testing import MongoTestCase
+from eduid_common.config.workers import MsgConfig
 
 
 class MsgMongoTestCase(MongoTestCase):
@@ -10,24 +11,25 @@ class MsgMongoTestCase(MongoTestCase):
         super(MsgMongoTestCase, self).setUp()
         data_dir = pkg_resources.resource_filename(__name__, 'tests/data')
         if init_msg:
-            self.msg_settings = {
-                'CELERY': {'broker_transport': 'memory',
+            settings = {
+                'celery': {'broker_transport': 'memory',
                            'broker_url': 'memory://',
                            'task_eager_propagates': True,
                            'task_always_eager': True,
                            'result_backend': 'cache',
                            'cache_backend': 'memory',
                            },
-                'MONGO_URI': self.tmp_db.uri,
-                'MONGO_DBNAME': 'test',
-                'SMS_ACC': 'foo',
-                'SMS_KEY': 'bar',
-                'SMS_SENDER': 'Test sender',
-                'TEMPLATE_DIR': data_dir,
-                'MESSAGE_RATE_LIMIT': '2/m',
+                'mongo_uri': self.tmp_db.uri,
+                'mongo_dbname': 'test',
+                'sms_acc': 'foo',
+                'sms_key': 'bar',
+                'sms_sender': 'Test sender',
+                'template_dir': data_dir,
+                'message_rate_limit': '2/m',
             }
+            self.msg_settings = MsgConfig(**settings)
             # initialize eduid_msg without requiring config in etcd
             import eduid_msg
-            self.msg = eduid_msg.init_app(self.msg_settings['CELERY'])
+            self.msg = eduid_msg.init_app(self.msg_settings.celery)
             import eduid_msg.worker
             eduid_msg.worker.worker_config = self.msg_settings
