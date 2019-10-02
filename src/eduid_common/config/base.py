e0f218e43cdab5cec238bfdfb776db0a566a81ac
enriquepablo <enrique@cazalla.net>
2019-10-02 14:11:11 +0200

    wrong type for configuration key

--- src/eduid_common/config/base.py
+++ src/eduid_common/config/base.py
@@ -366,7 +366,7 @@ class FlaskConfig(BaseConfig):
     templates_auto_reload: Optional[bool] = None
     explain_template_loading: bool = False
     max_cookie_size: int = 4093
-    babel_translation_directories: List[str] = field(default_factory=list)
+    babel_translation_directories: str = 'translations'
     babel_default_locale: str = 'en'
     babel_default_timezone: str = ''
     babel_domain: str = ''
