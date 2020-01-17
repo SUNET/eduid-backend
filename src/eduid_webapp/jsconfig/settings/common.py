7199d3df4b81c2ead332beba148a1d33bf508285
Johan Lundberg <lundberg@sunet.se>
2020-01-17 16:16:30 +0100

    Serve bundles with relative path

--- src/eduid_webapp/jsconfig/settings/common.py
+++ src/eduid_webapp/jsconfig/settings/common.py
@@ -44,6 +44,8 @@ class JSConfigConfig(FlaskConfig):
     """
     Configuration for the jsconfig app
     """
+    static_path: str = '/static'
+
     dashboard_bundle_path: str = 'front-build/dashboard-bundle.dev.js'
     dashboard_bundle_version: str = 'dev'
     # Dashboard feature toggle settings
