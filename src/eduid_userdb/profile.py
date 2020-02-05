0465a9a4c4c1af8a86b043aa57ed940563bbcd4c
Johan Lundberg <lundberg@sunet.se>
2020-02-05 10:36:58 +0100

    Tidy up comments

--- src/eduid_userdb/profile.py
+++ src/eduid_userdb/profile.py
@@ -33,26 +33,18 @@ class Profile(Element):
     # -----------------------------------------------------------------
     @property
     def key(self) -> str:
-        """
-        Return the element that is used as key in a ElementList.
-        """
+        """ Return the element that is used as key in a ElementList """
         return self.owner
 
     # -----------------------------------------------------------------
     @property
     def owner(self) -> str:
-        """
-        This is the name of the profile owner.
-
-        :return: Name of owner owner
-        """
+        """ Name of the profile owner """
         return self._data['owner']
 
     @owner.setter
     def owner(self, value: str) -> None:
-        """
-        :param value: Name of owner owner
-        """
+        """ Name of profile owner """
         if not isinstance(value, str):
             raise UserDBValueError(f"Invalid 'owner': {repr(value)}")
         self._data['owner'] = value.lower()
@@ -60,18 +52,12 @@ class Profile(Element):
     # -----------------------------------------------------------------
     @property
     def schema(self) -> str:
-        """
-        This is the schema used for the external data
-
-        :return: Schema identifier
-        """
+        """ This is the schema identifier for schema used for the external data """
         return self._data['schema']
 
     @schema.setter
     def schema(self, value: str) -> None:
-        """
-        :param value: Schema identifier
-        """
+        """ Schema identifier for schema used for the external data"""
         if not isinstance(value, str):
             raise UserDBValueError(f"Invalid 'schema': {repr(value)}")
         self._data['schema'] = value.lower()
@@ -79,18 +65,12 @@ class Profile(Element):
     # -----------------------------------------------------------------
     @property
     def profile_data(self) -> Mapping[str, Any]:
-        """
-        This is the schema used for the external data
-
-        :return: Schema identifier
-        """
+        """ This is the schema identifier used for the external data """
         return self._data['profile_data']
 
     @profile_data.setter
     def profile_data(self, value: Mapping[str, Any]) -> None:
-        """
-        :param value: Opaque profile data
-        """
+        """ Opaque profile data """
         if not isinstance(value, Mapping):
             raise UserDBValueError(f"Invalid 'profile_data': {repr(value)}")
         self._data['profile_data'] = value
@@ -103,8 +83,6 @@ class ProfileList(ElementList):
     Provide methods to add, update and remove elements from the list while
     maintaining some governing principles, such as ensuring there is only one
     owner with the same name.
-
-    :param profiles: List of profiles
     """
     def __init__(self, profiles: List[Profile]):
         super().__init__(elements=list())
