e2a33932fed452dda935318cc54a7e3f36868153
enriquepablo <enrique@cazalla.net>
2020-02-19 11:25:48 +0100

    debug logging

--- src/eduid_webapp/reset_password/helpers.py
+++ src/eduid_webapp/reset_password/helpers.py
@@ -369,6 +369,7 @@ def send_verify_phone_code(state: ResetPasswordEmailState, phone_number: str):
     }
     send_sms(state.phone_number, template, context, state.reference)
     current_app.logger.info(f'Sent password reset sms to user with eppn: {state.eppn}')
+    current_app.logger.debug(f'Sent password reset sms with code: {state.phone_code.code}')
     current_app.logger.debug(f'Phone number: {state.phone_number}')
 
 
