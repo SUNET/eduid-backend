938180fe9147d059dd70711a99cda913cf29753c
Johan Lundberg <lundberg@sunet.se>
2020-02-26 13:30:24 +0100

    Import optimizations

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+
 from setuptools import setup, find_packages
 
 version = '0.0.1'
--- src/eduid_groupdb/__init__.py
+++ src/eduid_groupdb/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from eduid_groupdb.db import Neo4jDB, BaseGraphDB
-from eduid_groupdb.group import Group, User, Role
+from eduid_groupdb.group import Group, User
 from eduid_groupdb.groupdb import GroupDB
 
 __author__ = 'lundberg'
