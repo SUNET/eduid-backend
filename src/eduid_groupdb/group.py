fc6fe60f81a15e4414c35777c8d230273e5d0d57
Johan Lundberg <lundberg@sunet.se>
2020-02-27 18:31:25 +0100

    Easy comparision of User and Group
    Handle Group owners as Group members

--- src/eduid_groupdb/group.py
+++ src/eduid_groupdb/group.py
@@ -17,6 +17,11 @@ class User:
     created_ts: Optional[datetime] = None
     modified_ts: Optional[datetime] = None
 
+    def __eq__(self, other: Group):
+        if self.identifier == other.identifier:
+            return True
+        return False
+
     @classmethod
     def from_mapping(cls, data: Mapping) -> User:
         dt = neo4j_ts_to_dt(data)
@@ -35,16 +40,30 @@ class Group:
     owners: List[User] = field(default_factory=list)
     members: List[Union[User, Group]] = field(default_factory=list)
 
-    def _filter_members(self, member_type: Type[Union[User, Group]]):
-        return [member for member in self.members if isinstance(member, member_type)]
+    def __eq__(self, other: Group):
+        if (self.scope == other.scope) and (self.identifier == other.identifier):
+            return True
+        return False
+
+    @staticmethod
+    def _filter_type(it: List[Union[User, Group]], member_type: Type[Union[User, Group]]):
+        return [item for item in it if isinstance(item, member_type)]
 
     @property
     def user_members(self) -> List[User]:
-        return self._filter_members(member_type=User)
+        return self._filter_type(it=self.members, member_type=User)
 
     @property
     def group_members(self) -> List[Group]:
-        return self._filter_members(member_type=Group)
+        return self._filter_type(it=self.members, member_type=Group)
+
+    @property
+    def user_owners(self) -> List[User]:
+        return self._filter_type(it=self.owners, member_type=User)
+
+    @property
+    def group_owners(self) -> List[Group]:
+        return self._filter_type(it=self.owners, member_type=Group)
 
     @classmethod
     def from_mapping(cls, data: Mapping) -> Group:
