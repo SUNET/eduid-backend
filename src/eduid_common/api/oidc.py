9619898bddf3c285c12433f7d48f42478948518a
Johan Lundberg <lundberg@sunet.se>
2020-03-03 13:20:27 +0100

    Actually use initialized oidc client in retry

--- src/eduid_common/api/oidc.py
+++ src/eduid_common/api/oidc.py
@@ -14,19 +14,18 @@ def init_client(app):
     oidc_client = Client(client_authn_method=CLIENT_AUTHN_METHOD)
     oidc_client.store_registration_info(RegistrationRequest(**app.config['CLIENT_REGISTRATION_INFO']))
     provider = app.config['PROVIDER_CONFIGURATION_INFO']['issuer']
-
     try:
         oidc_client.provider_config(provider)
-        app.oidc_client = oidc_client
     except ConnectionError:
         app.logger.warning('No connection to provider {!s}. Can not start without provider configuration.'
                            ' Retrying...'.format(provider))
-        # Retry after 20 seconds
-        sleep(20)  # Hack until we use new frontends
+        # Retry after 20 seconds so we don't get an excessive exit-restart loop
+        sleep(20)
         try:
             oidc_client.provider_config(provider)
         except ConnectionError:
             app.logger.critical('No connection to provider {!s}. Can not start without provider configuration.'
                                 ' Exiting.'.format(provider))
             exit(1)
+    app.oidc_client = oidc_client
     return app
