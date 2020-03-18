bc3129095a56ee210e2771578b8f50b7fc1d83a6
Johan Lundberg <lundberg@sunet.se>
2020-03-18 15:55:28 +0100

    Use init_config for initializing configuration

--- src/eduid_scimapi/config.py
+++ src/eduid_scimapi/config.py
@@ -1,11 +1,7 @@
-import os
-from dataclasses import asdict, dataclass
+from dataclasses import dataclass, field
 from typing import Dict
 
-import yaml
-
 from eduid_common.config.base import BaseConfig
-from eduid_common.config.parsers.etcd import EtcdConfigParser
 
 
 @dataclass
@@ -17,32 +13,5 @@ class ScimApiConfig(BaseConfig):
     schema: str = 'http'
     server_name: str = 'localhost:8000'
     application_root: str = '/'
-
-
-def load_config(config: Dict, name: str = 'scimapi', testing: bool=False) -> ScimApiConfig:
-    try:
-        # Init etcd config parsers
-        common_parser = EtcdConfigParser('/eduid/api/common/')
-        app_etcd_namespace = os.environ.get('EDUID_CONFIG_NS', '/eduid/api/{!s}/'.format(name))
-        app_parser = EtcdConfigParser(app_etcd_namespace)
-        # Load optional project wide settings
-        common_config = common_parser.read_configuration(silent=False)
-        if common_config:
-            config.update(common_config)
-        # Load optional app specific settings
-        app_config = app_parser.read_configuration(silent=False)
-        if app_config:
-            config.update(app_config)
-    except:
-        if not testing:
-            raise
-
-    cfg = ScimApiConfig(**config)
-
-    # Save config to a file in /dev/shm for introspection
-    fd_int = os.open(f'/dev/shm/{name}_config.yaml', os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0o600)
-    with open(fd_int, 'w') as fd:
-        fd.write('---\n')
-        yaml.safe_dump(asdict(cfg), fd)
-
-    return cfg
+    neo4j_uri: str = ''
+    neo4j_config: Dict = field(default_factory=dict)
