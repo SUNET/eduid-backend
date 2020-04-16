031d459c33e6d67c8a3aa4d814d99e0326e6535a
Johan Lundberg <lundberg@sunet.se>
2020-04-16 18:33:19 +0200

    More tests

--- src/eduid_scimapi/tests/test_scimuser.py
+++ src/eduid_scimapi/tests/test_scimuser.py
@@ -1,11 +1,14 @@
 import unittest
 from dataclasses import asdict
 from datetime import datetime
+from uuid import uuid4
 
 import bson
 from bson import ObjectId
 
-from eduid_scimapi.scimbase import SCIMSchema
+from eduid_scimapi.profile import Profile
+from eduid_scimapi.scimbase import SCIMSchema, make_etag
+from eduid_scimapi.testing import ScimApiTestCase
 from eduid_scimapi.user import ScimApiUser
 
 
@@ -81,5 +84,113 @@ class TestScimUser(unittest.TestCase):
         self.assertTrue(x)
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestUserResource(ScimApiTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.test_profile = Profile()
+        self.test_profile.attributes['displayName'] = 'Test User 1'
+        self.test_profile.data = {'test_key': 'test_value'}
+
+    def test_get_user(self):
+        db_user = self.add_user(identifier=str(uuid4()), external_id='test-id-1', profiles={'test': self.test_profile})
+        response = self.client.simulate_get(path=f'/Users/{db_user.scim_id}', headers=self.headers)
+        self.assertEqual([SCIMSchema.CORE_20_USER.value, SCIMSchema.NUTID_V1.value], response.json.get('schemas'))
+        self.assertEqual(str(db_user.scim_id), response.json.get('id'))
+        self.assertEqual(f'http://localhost:8000/Users/{db_user.scim_id}', response.headers.get('location'))
+        self.assertEqual('test-id-1', response.json.get('externalId'))
+
+        nutid = response.json.get(SCIMSchema.NUTID_V1.value)
+        self.assertIsNotNone(nutid.get('profiles'))
+        test_profile = nutid.get('profiles').get('test')
+        self.assertEqual(self.test_profile.attributes, test_profile.get('attributes'))
+        self.assertEqual(self.test_profile.data, test_profile.get('data'))
+
+        meta = response.json.get('meta')
+        self.assertIsNotNone(meta)
+        self.assertIsNotNone(meta.get('created'))
+        self.assertIsNotNone(meta.get('lastModified'))
+        self.assertIsNotNone(meta.get('version'))
+        self.assertEqual(f'http://localhost:8000/Users/{response.json.get("id")}', meta.get('location'))
+        self.assertEqual(f'User', meta.get('resourceType'))
+
+    def test_create_user(self):
+        req = {
+            'schemas': [SCIMSchema.CORE_20_USER.value, SCIMSchema.NUTID_V1.value],
+            'externalId': 'test-id-1',
+            SCIMSchema.NUTID_V1.value: {
+                'profiles': {'test': {'attributes': {'displayName': 'Test User 1'}, 'data': {'test_key': 'test_value'}}}
+            },
+        }
+        response = self.client.simulate_post(path='/Users/', body=self.as_json(req), headers=self.headers)
+
+        # TODO: SCIMSchema.DEBUG_V1 should be returned if not asked for
+        self.assertEqual(
+            [SCIMSchema.CORE_20_USER.value, SCIMSchema.NUTID_V1.value, SCIMSchema.DEBUG_V1.value],
+            response.json.get('schemas'),
+        )
+        self.assertIsNotNone(response.json.get('id'))
+        self.assertEqual(f'http://localhost:8000/Users/{response.json.get("id")}', response.headers.get('location'))
+        self.assertEqual('test-id-1', response.json.get('externalId'))
+
+        nutid = response.json.get(SCIMSchema.NUTID_V1.value)
+        self.assertIsNotNone(nutid.get('profiles'))
+        test_profile = nutid.get('profiles').get('test')
+        self.assertEqual(self.test_profile.attributes, test_profile.get('attributes'))
+        self.assertEqual(self.test_profile.data, test_profile.get('data'))
+
+        meta = response.json.get('meta')
+        self.assertIsNotNone(meta)
+        self.assertIsNotNone(meta.get('created'))
+        self.assertIsNotNone(meta.get('lastModified'))
+        self.assertIsNotNone(meta.get('version'))
+        self.assertEqual(f'http://localhost:8000/Users/{response.json.get("id")}', meta.get('location'))
+        self.assertEqual(f'User', meta.get('resourceType'))
+
+    def test_update_user(self):
+        db_user = self.add_user(identifier=str(uuid4()), external_id='test-id-1', profiles={'test': self.test_profile})
+        req = {
+            'schemas': [SCIMSchema.CORE_20_USER.value, SCIMSchema.NUTID_V1.value],
+            'id': str(db_user.scim_id),
+            'externalId': 'test-id-1',
+            SCIMSchema.NUTID_V1.value: {
+                'profiles': {'test': {'attributes': {'displayName': 'Test User 1'}, 'data': {'test_key': 'test_value'}}}
+            },
+        }
+        self.headers['IF-MATCH'] = make_etag(db_user.version)
+        response = self.client.simulate_put(
+            path=f'/Users/{db_user.scim_id}', body=self.as_json(req), headers=self.headers
+        )
+
+        self.assertEqual([SCIMSchema.CORE_20_USER.value, SCIMSchema.NUTID_V1.value], response.json.get('schemas'))
+        self.assertIsNotNone(response.json.get('id'))
+        self.assertEqual(f'http://localhost:8000/Users/{response.json.get("id")}', response.headers.get('location'))
+        self.assertEqual('test-id-1', response.json.get('externalId'))
+
+        nutid = response.json.get(SCIMSchema.NUTID_V1.value)
+        self.assertIsNotNone(nutid.get('profiles'))
+        test_profile = nutid.get('profiles').get('test')
+        self.assertEqual(self.test_profile.attributes, test_profile.get('attributes'))
+        self.assertEqual(self.test_profile.data, test_profile.get('data'))
+
+        meta = response.json.get('meta')
+        self.assertIsNotNone(meta)
+        self.assertIsNotNone(meta.get('created'))
+        self.assertIsNotNone(meta.get('lastModified'))
+        self.assertIsNotNone(meta.get('version'))
+        self.assertEqual(f'http://localhost:8000/Users/{response.json.get("id")}', meta.get('location'))
+        self.assertEqual(f'User', meta.get('resourceType'))
+
+    def test_search_user_external_id(self):
+        db_user = self.add_user(identifier=str(uuid4()), external_id='test-id-1', profiles={'test': self.test_profile})
+        self.add_user(identifier=str(uuid4()), external_id='test-id-2', profiles={'test': self.test_profile})
+        req = {
+            'schemas': [SCIMSchema.API_MESSAGES_20_SEARCH_REQUEST.value],
+            'filter': f'externalid eq "{db_user.external_id}"',
+            'startIndex': 1,
+            'count': 10,
+        }
+        response = self.client.simulate_post(path='/Users/.search', body=self.as_json(req), headers=self.headers)
+        self.assertEqual([SCIMSchema.API_MESSAGES_20_LIST_RESPONSE.value], response.json.get('schemas'))
+        resources = response.json.get('Resources')
+        self.assertEqual(1, len(resources))
+        self.assertEqual(str(db_user.scim_id), resources[0].get('id'))
