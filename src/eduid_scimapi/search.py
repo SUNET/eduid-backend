0ea370341b860306ff30d6a04097d888671a91ea
Johan Lundberg <lundberg@sunet.se>
2020-05-06 11:25:13 +0200

    Added debug logging for search filter parsing

--- src/eduid_scimapi/search.py
+++ src/eduid_scimapi/search.py
@@ -1,8 +1,11 @@
+import logging
 import re
 from dataclasses import dataclass
 from typing import Union
 
 from eduid_scimapi.exceptions import BadRequest
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -15,6 +18,7 @@ class SearchFilter(object):
 def parse_search_filter(filter: str) -> SearchFilter:
     match = re.match('(.+?) (..) (.+)', filter)
     if not match:
+        logger.debug(f'Unrecognised filter: {filter}')
         raise BadRequest(scim_type='invalidFilter', detail='Unrecognised filter')
 
     val: Union[str, int]
@@ -22,12 +26,14 @@ def parse_search_filter(filter: str) -> SearchFilter:
 
     if len(val) and val[0] == '"' and val[-1] == '"':
         val = val[1:-1]
-        check = re.match('^[a-zA-Z0-9_ .,:;+-]*$', val)
+        check = re.match('^[a-zA-Z0-9_ .,:;+-@]*$', val)
         if not check:
+            logger.debug(f'Unrecognised string value in filter: {val}')
             raise BadRequest(scim_type='invalidFilter', detail='Unrecognised string value in filter')
     elif val.isdecimal():
         val = int(val)
     else:
+        logger.debug(f'Unrecognised type of value (not string or integer) in filter: {val}')
         raise BadRequest(
             scim_type='invalidFilter', detail='Unrecognised type of value (not string or integer) in filter'
         )
