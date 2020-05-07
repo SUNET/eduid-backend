e8ee5786f7af949d64679bdd18bc6638962bb789
Johan Lundberg <lundberg@sunet.se>
2020-05-07 13:24:11 +0200

    Replace regex with isprintable check

--- src/eduid_scimapi/search.py
+++ src/eduid_scimapi/search.py
@@ -26,9 +26,8 @@ def parse_search_filter(filter: str) -> SearchFilter:
 
     if len(val) and val[0] == '"' and val[-1] == '"':
         val = val[1:-1]
-        check = re.match('^[a-zA-Z0-9_ .,:;+-@]*$', val)
-        if not check:
-            logger.debug(f'Unrecognised string value in filter: {val}')
+        if not val.isprintable():
+            logger.debug(f'Unrecognised string value in filter: {repr(val)}')
             raise BadRequest(scim_type='invalidFilter', detail='Unrecognised string value in filter')
     elif val.isdecimal():
         val = int(val)
