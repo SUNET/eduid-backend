f027069375af0f7469db07ccf1814976329034ba
Johan Lundberg <lundberg@sunet.se>
2020-05-18 16:00:25 +0200

    Init group_management

---
+++ src/eduid_webapp/group_management/app.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2020 SUNET
+# All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or
+#   without modification, are permitted provided that the following
+#   conditions are met:
+#
+#     1. Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#     2. Redistributions in binary form must reproduce the above
+#        copyright notice, this list of conditions and the following
+#        disclaimer in the documentation and/or other materials provided
+#        with the distribution.
+#     3. Neither the name of the NORDUnet nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+from typing import cast
+
+from flask import current_app
+
+from eduid_common.api import mail_relay
+from eduid_common.authn.middleware import AuthnBaseApp
+
+from eduid_scimapi.groupdb import ScimApiGroupDB
+from eduid_scimapi.userdb import ScimApiUserDB
+
+# from eduid_userdb.group_management.db import GroupManagementStateDB
+from eduid_webapp.group_management.settings.common import GroupManagementConfig
+
+__author__ = 'lundberg'
+
+
+class GroupManagementApp(AuthnBaseApp):
+    def __init__(self, name: str, config: dict, **kwargs):
+        super(GroupManagementApp, self).__init__(name, GroupManagementConfig, config, **kwargs)
+        self.config: GroupManagementConfig = cast(GroupManagementConfig, self.config)
+
+        # Init dbs
+        # self.group_management_state_db = GroupManagementStateDB(self.config.mongo_uri)
+
+        _owner = self.config.scim_data_owner.replace('.', '_')  # replace dots with underscores
+        self.scimapi_userdb = ScimApiUserDB(db_uri=self.config.mongo_uri, collection=f'{_owner}__users')
+        self.scimapi_groupdb = ScimApiGroupDB(
+            db_uri=self.config.neo4j_uri,
+            config=self.config.neo4j_config,
+            scope=self.config.scim_data_owner,
+            mongo_uri=self.config.mongo_uri,
+            mongo_dbname='eduid_scimapi',
+            mongo_collection=f'{_owner}__groups',
+        )
+        # Init celery
+        mail_relay.init_relay(self)
+
+
+current_group_management_app = cast(GroupManagementApp, current_app)
+
+
+def init_group_management_app(name: str, config: dict) -> GroupManagementApp:
+    """
+    :param name: The name of the instance, it will affect the configuration loaded.
+    :param config: any additional configuration settings. Specially useful
+                   in test cases
+
+    :return: the flask app
+    """
+    app = GroupManagementApp(name, config)
+
+    # Register views
+    from eduid_webapp.group_management.views import group_management_views
+
+    app.register_blueprint(group_management_views)
+
+    app.logger.info('{!s} initialized'.format(name))
+    return app
