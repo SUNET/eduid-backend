5af82e47c9ab6f649fb8c0daafb3f25eccb3e478
enriquepablo <enrique@cazalla.net>
2020-05-19 08:06:42 +0200

    fix copyright

--- src/eduid_common/api/tests/test_backdoor.py
+++ src/eduid_common/api/tests/test_backdoor.py
@@ -1,6 +1,6 @@
 #  -*- encoding: utf-8 -*-
 #
-# Copyright (c) 2016 NORDUnet A/S
+# Copyright (c) 2020 SUNET
 # All rights reserved.
 #
 #   Redistribution and use in source and binary forms, with or
