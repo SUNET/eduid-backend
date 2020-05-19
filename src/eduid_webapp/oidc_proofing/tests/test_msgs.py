9b2d912b00d25caf551e3612b5472cf4469679b3
enriquepablo <enrique@cazalla.net>
2020-05-19 14:41:52 +0200

    test messages

---
+++ src/eduid_webapp/oidc_proofing/tests/test_msgs.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from eduid_webapp.oidc_proofing.helpers import OIDCMsg
+
+
+class MessagesTests(unittest.TestCase):
+
+    def test_messages(self):
+        """"""
+        self.assertEqual(str(OIDCMsg.temp_error.value), 'Temporary technical problems')
+        self.assertEqual(str(OIDCMsg.no_conn.value), 'No connection to authorization endpoint')
+        self.assertEqual(str(OIDCMsg.nin_invalid.value), 'nin needs to be formatted as 18|19|20yymmddxxxx')
