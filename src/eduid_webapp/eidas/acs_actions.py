415f53a9f2bc120416e9be9d6da89e3955044a97
enriquepablo <enrique@cazalla.net>
2020-05-28 14:26:20 +0200

    weirdly duplicated code

--- src/eduid_webapp/eidas/acs_actions.py
+++ src/eduid_webapp/eidas/acs_actions.py
@@ -245,19 +245,6 @@ def nin_verify_BACKDOOR(user: User) -> Response:
 
     return redirect_with_msg(redirect_url, 'eidas.nin_verify_success')
 
-    # Verify NIN for user
-    try:
-        nin_element = NinProofingElement(number=asserted_nin, application='eduid-eidas', verified=False)
-        proofing_state = NinProofingState(id=None, modified_ts=None, eppn=user.eppn, nin=nin_element)
-        verify_nin_for_user(user, proofing_state, proofing_log_entry)
-    except AmTaskFailed as e:
-        current_app.logger.error('Verifying NIN for user failed')
-        current_app.logger.error('{}'.format(e))
-        return redirect_with_msg(redirect_url, ':ERROR:Temporary technical problems')
-    current_app.stats.count(name='nin_verified')
-
-    return redirect_with_msg(redirect_url, 'eidas.nin_verify_success')
-
 
 @acs_action('mfa-authentication-action')
 @require_user
