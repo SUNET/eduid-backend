0151455be26d2148861b7e596599bf90ec9c7e85
Fredrik Thulin <fredrik@thulin.net>
2020-06-09 16:59:26 +0200

    bump version to 0.4.15

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = 'eduID User Database interface module'
 if os.path.exists(README_fn):
     README = open(README_fn).read()
 
-version = '0.4.14'
+version = '0.4.15'
 
 install_requires = [
     'pymongo >= 3.6',
