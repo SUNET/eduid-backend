c84774f965fa8d6001dbb6a850177edcbb24b6e4
Fredrik Thulin <fredrik@thulin.net>
2020-06-11 17:32:09 +0200

    add is_valid_password
    This is based on the exising
    eduid_common.api.schemas.password.PasswordSchema, and is intended to
    replace the former.

--- src/eduid_common/api/validation.py
+++ src/eduid_common/api/validation.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-
+import math
 import re
+from typing import Sequence
+
+from zxcvbn import zxcvbn
+
 
 nin_re = re.compile(r'^(18|19|20)\d{2}(0[1-9]|1[0-2])\d{2}\d{4}$')
 # RFC2822_email, http://www.regular-expressions.info/email.html
@@ -29,3 +33,30 @@ def is_valid_email(email, **kwargs):
     if email_re.match(email):
         return True
     raise ValueError('email needs to be formatted according to RFC2822')
+
+
+def is_valid_password(password, user_info: Sequence[str], min_entropy: int):
+    """
+    Checks the complexity of the password - NOT if the password is the right one for a user.
+
+    :param password: Password candidate
+    :param user_info: List of strings (name, surname etc.) that zxcvbn will reduce the score for
+    :param min_entropy: Minimum Shannon entropy (?) of password to allow
+
+    :return: True or raises ValueError
+    """
+    # Remove whitespace
+    password = ''.join(password.split())
+
+    # Reject blank passwords
+    if not password:
+        raise ValueError('The password complexity is too weak.')
+
+    # Check password complexity with zxcvbn
+    result = zxcvbn(password, user_inputs=user_info)
+    _guesses = result.get('guesses', 1)
+    _pw_entropy = math.log(_guesses, 2)
+    if _pw_entropy < min_entropy:
+        raise ValueError('The password complexity is too weak.')
+
+    return True
