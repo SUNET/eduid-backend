002950418bcefcd876194e518b13c97f29d24d04
enriquepablo <enrique@cazalla.net>
2020-07-29 14:21:44 +0200

    User as dataclass - fixoing tests in eduid-am

--- src/eduid_userdb/personal_data/user.py
+++ src/eduid_userdb/personal_data/user.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+from dataclasses import dataclass
+
 from eduid_userdb.user import User
 
 __author__ = 'lundberg'
 
 
+@dataclass
 class PersonalDataUser(User):
     pass
--- src/eduid_userdb/support/user.py
+++ src/eduid_userdb/support/user.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+from dataclasses import dataclass
+
 from eduid_userdb.signup import SignupUser
 from eduid_userdb.user import User
 
 __author__ = 'lundberg'
 
 
+@dataclass
 class SupportUser(User):
     pass
 
 
+@dataclass
 class SupportSignupUser(SignupUser):
     pass
