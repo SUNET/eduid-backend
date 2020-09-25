31e32e986f5d01f89d6e1d660a91aa9877293a20
Fredrik Thulin <fredrik@thulin.net>
2020-09-25 15:01:53 +0200

    use EduidTemporaryInstance from eduid_userdb.testing

--- src/eduid_graphdb/testing.py
+++ src/eduid_graphdb/testing.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-import atexit
 import logging
 import random
-import subprocess
-import time
 import unittest
 from os import environ
-from typing import Optional
+from typing import Optional, Sequence
 
 from neo4j.exceptions import ServiceUnavailable
 
 from eduid_graphdb.db import Neo4jDB
+from eduid_userdb.testing import EduidTemporaryInstance
 
 __author__ = 'lundberg'
+
 
 # Run tests with different Neo4j docker image versions using environment variables
 NEO4J_VERSION = environ.get('NEO4J_VERSION', 'enterprise')
@@ -23,7 +22,7 @@ logger = logging.getLogger(__name__)
 logger.info(f'NEO4J_VERSION={NEO4J_VERSION}')
 
 
-class Neo4jTemporaryInstance(object):
+class Neo4jTemporaryInstance(EduidTemporaryInstance):
     """
     Singleton to manage a temporary Neo4j instance
 
@@ -40,64 +39,53 @@ class Neo4jTemporaryInstance(object):
     DEFAULT_USERNAME = 'neo4j'
     DEFAULT_PASSWORD = 'testing'
 
-    @classmethod
-    def get_instance(cls):
-        if cls._instance is None:
-            cls._instance = cls()
-            atexit.register(cls._instance.shutdown)
-        return cls._instance
-
-    def __init__(self, neo4j_version=NEO4J_VERSION):
+    def __init__(self, max_retry_seconds: int = 60, neo4j_version=NEO4J_VERSION):
         self._http_port = random.randint(40000, 43000)
         self._https_port = random.randint(44000, 46000)
         self._bolt_port = random.randint(47000, 50000)
-        self._docker_exc = 'docker'
         self._docker_name = 'neo4j-{!s}'.format(self.bolt_port)
-        try:
-            self._process = subprocess.Popen(
-                [
-                    self._docker_exc,
-                    'run',
-                    '--rm',
-                    '--name',
-                    f'{self._docker_name}',
-                    '-p',
-                    f'{self.http_port}:7474',
-                    '-p',
-                    f'{self.https_port}:7473',
-                    '-p',
-                    f'{self.bolt_port}:7687',
-                    '-e',
-                    f'NEO4J_AUTH={self.DEFAULT_USERNAME}/{self.DEFAULT_PASSWORD}',
-                    '-e',
-                    'NEO4J_ACCEPT_LICENSE_AGREEMENT=yes',
-                    f'neo4j:{neo4j_version}',
-                ],
-                stdout=open('/tmp/neo4j-temp.log', 'wb'),
-                stderr=subprocess.STDOUT,
-            )
-        except OSError:
-            assert False, "No docker executable found"
-
+        self._neo4j_version = neo4j_version
         self._host = 'localhost'
 
         self._db = None
-        for i in range(300):
-            time.sleep(0.5)
-            try:
-                db_uri = f'bolt://{self.DEFAULT_USERNAME}:{self.DEFAULT_PASSWORD}@{self.host}:{self.bolt_port}'
-                self._db = Neo4jDB(db_uri=db_uri, config={'encrypted': False})
-            except (ServiceUnavailable, ConnectionError) as e:
-                logger.error(e)
-                logger.info(f'Retrying, attempt {i}')
-                continue
-            else:
-                logger.info(f'Successfully connected to neo4jdb on attempt {i} after {i*0.5}s')
-                break
 
-        if self._db is None:
-            self.shutdown()
-            assert False, 'Cannot connect to the neo4j test instance'
+        super().__init__(max_retry_seconds=max_retry_seconds)
+
+    @property
+    def command(self) -> Sequence[str]:
+        return [
+            'docker',
+            'run',
+            '--rm',
+            '--name',
+            f'{self._docker_name}',
+            '-p',
+            f'{self.http_port}:7474',
+            '-p',
+            f'{self.https_port}:7473',
+            '-p',
+            f'{self.bolt_port}:7687',
+            '-e',
+            f'NEO4J_AUTH={self.DEFAULT_USERNAME}/{self.DEFAULT_PASSWORD}',
+            '-e',
+            'NEO4J_ACCEPT_LICENSE_AGREEMENT=yes',
+            f'neo4j:{self._neo4j_version}',
+        ]
+
+    def setup_conn(self) -> bool:
+        try:
+            db_uri = f'bolt://{self.DEFAULT_USERNAME}:{self.DEFAULT_PASSWORD}@{self.host}:{self.bolt_port}'
+            self._db = Neo4jDB(db_uri=db_uri, config={'encrypted': False})
+        except (ServiceUnavailable, ConnectionError) as e:
+            logger.error(e)
+            return False
+        return True
+
+    @property
+    def conn(self) -> Neo4jDB:
+        if self._conn is None:
+            raise RuntimeError('Missing temporary Neo4jDB instance')
+        return self._conn
 
     @property
     def db(self):
@@ -131,12 +119,6 @@ class Neo4jTemporaryInstance(object):
                 s.run(f'DROP CONSTRAINT {constraint["name"]}')
             for index in s.run("CALL db.indexes"):
                 s.run(f'DROP INDEX {index["name"]}')
-
-    def shutdown(self):
-        if self._process:
-            self._process.terminate()
-            self._process.wait()
-            self._process = None
 
 
 class Neo4jTestCase(unittest.TestCase):
