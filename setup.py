cad746458d2f6629caa62c767fff451fa717505a
Johan Lundberg <lundberg@sunet.se>
2020-10-13 18:55:08 +0200

    Added package files

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+install_requires = [x for x in open(os.path.join(here, 'requirements.txt')).read().split('\n') if len(x) > 0]
+testing_extras = [
+    x
+    for x in open(os.path.join(here, 'test_requirements.txt')).read().split('\n')
+    if len(x) > 0 and not x.startswith('-')
+]
+
+version = '0.0.1'
+
+setup(
+    name='eduid-queue',
+    version=version,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/sunet/eduid-queue',
+    license='BSD-2-Clause',
+    keywords='eduid',
+    author='Johan Lundberg',
+    author_email='lundberg@sunet.se',
+    description='MongoDB based task queue',
+    install_requires=install_requires,
+    extras_require={
+        'testing': testing_extras,
+    },
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'run-mail-worker=eduid_queue.workers.mail:start_worker',
+        ],
+    },
+)
