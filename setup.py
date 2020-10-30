81c22f7ad0d3e1f687ee56b35e26f02db8737ca0
Johan Lundberg <lundberg@sunet.se>
2020-10-30 14:40:28 +0100

    Added lowercase email Marshmallow field

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-version = '0.13.1'
+version = '0.14.0'
 
 
 requires = [
@@ -51,7 +51,7 @@ worker_requires = [
     'python-etcd >= 0.4.5',
     'PyNaCl >= 1.0.1',
     'PyYAML >= 3.11',
-    ]
+]
 worker_extras = worker_requires + []
 api_extras = worker_extras
 
@@ -61,39 +61,35 @@ nodeps_requires = requires
 test_requires = [
     'mock == 1.0.1',
 ]
-testing_extras = test_requires + webapp_extras + [
-    'pytest>=5.2.0'
-    'pytest-cov>=2.7.1'
-]
+testing_extras = test_requires + webapp_extras + ['pytest>=5.2.0' 'pytest-cov>=2.7.1']
 
 long_description = open('README.txt').read()
 
-setup(name='eduid-common',
-      version=version,
-      description="Common code for eduID applications",
-      long_description=long_description,
-      classifiers=[
-          "Programming Language :: Python",
-      ],
-      keywords='',
-      author='SUNET',
-      author_email='',
-      url='https://github.com/SUNET/',
-      license='bsd',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      tests_require=test_requires,
-      extras_require={
-          'testing': testing_extras,
-          'webapp': webapp_extras,
-          'idp': idp_extras,
-          'worker': worker_extras,
-          'api': api_extras,
-          'nodeps': []
-      },
-      entry_points="""
+setup(
+    name='eduid-common',
+    version=version,
+    description="Common code for eduID applications",
+    long_description=long_description,
+    classifiers=["Programming Language :: Python",],
+    keywords='',
+    author='SUNET',
+    author_email='',
+    url='https://github.com/SUNET/',
+    license='bsd',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    tests_require=test_requires,
+    extras_require={
+        'testing': testing_extras,
+        'webapp': webapp_extras,
+        'idp': idp_extras,
+        'worker': worker_extras,
+        'api': api_extras,
+        'nodeps': [],
+    },
+    entry_points="""
       """,
-      )
+)
