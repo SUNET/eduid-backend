4909d0bf02affbda3ea3c8e522d5cda3c27380de
Fredrik Thulin <fredrik@thulin.net>
2020-11-03 15:49:31 +0100

    further tweak the log format for more fixed width

--- src/eduid_common/config/base.py
+++ src/eduid_common/config/base.py
@@ -223,7 +223,7 @@ class BaseConfig(CommonConfig):
     log_file: Optional[str] = None
     log_max_bytes: int = 1000000  # 1 MB
     log_backup_count: int = 10  # 10 x 1 MB
-    log_format: str = '{asctime} | {levelname:7} | {hostname} | {eppn} | {name:35} | {module} | {message}'
+    log_format: str = '{asctime} | {levelname:7} | {hostname} | {eppn:9} | {name:35} | {module:10} | {message}'
     log_type: List[str] = field(default_factory=lambda: ['stream'])
     logger: Optional[logging.Logger] = None
     redis_config: RedisConfig = field(default_factory=RedisConfig)
