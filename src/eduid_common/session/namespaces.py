e15af626494fed121f38cd2a3fda6f7bc3984657
Fredrik Thulin <fredrik@thulin.net>
2020-11-10 10:38:25 +0100

    fix, the UNIX timestamp was stored as a string in to_dict()

--- src/eduid_common/session/namespaces.py
+++ src/eduid_common/session/namespaces.py
@@ -72,7 +72,7 @@ class TimestampedNS(SessionNSBase):
         if _ts is not None:
             # Load timestamp from ISO format string, or fallback to old UNIX time.
             # When this code is deployed everywhere, we can change to ISO format in to_dict above.
-            if isinstance(_ts, str):
+            if isinstance(_ts, str) and not _ts.isdigit():
                 _data['ts'] = datetime.fromisoformat(_ts)
             else:
                 _data['ts'] = datetime.fromtimestamp(int(_ts))
