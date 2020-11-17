e157d3d5af262e3f63a4ea00b1f76489e1ba2877
Johan Lundberg <lundberg@sunet.se>
2020-11-17 10:57:13 +0100

    Fixes for package creation

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import os
 from setuptools import find_packages, setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-
 install_requires = [x for x in open(os.path.join(here, 'requirements.txt')).read().split('\n') if len(x) > 0]
 testing_extras = [
     x
     for x in open(os.path.join(here, 'test_requirements.txt')).read().split('\n')
     if len(x) > 0 and not x.startswith('-')
 ]
+client_extras = [x for x in open(os.path.join(here, 'requirements.txt')).read().split('\n') if len(x) > 0]
 
 version = '0.0.1'
 
@@ -27,6 +27,7 @@ setup(
     install_requires=install_requires,
     extras_require={
         'testing': testing_extras,
+        'client': client_extras,
     },
     include_package_data=True,
     entry_points={
