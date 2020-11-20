5afb99b8f04a0b05d6f008964a69873ef08c36d4
Johan Lundberg <lundberg@sunet.se>
2020-11-20 14:57:04 +0100

    Missing SenderInfo in db __init__

--- src/eduid_queue/db/__init__.py
+++ src/eduid_queue/db/__init__.py
@@ -2,6 +2,6 @@
 
 from eduid_queue.db.client import QueueDB
 from eduid_queue.db.payload import Payload, RawPayload, TestPayload
-from eduid_queue.db.queue_item import QueueItem
+from eduid_queue.db.queue_item import QueueItem, SenderInfo
 
 __author__ = 'lundberg'
