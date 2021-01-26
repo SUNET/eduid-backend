907c3002305b3d1f469cc23b42c92b3ed0811254
Fredrik Thulin <fredrik@thulin.net>
2021-01-26 12:21:12 +0100

    improve documentation

--- src/eduid_common/config/parsers/__init__.py
+++ src/eduid_common/config/parsers/__init__.py
@@ -29,13 +29,23 @@ def load_config(
 
 
 def _choose_parser(app_name: str, ns: str) -> Optional[BaseConfigParser]:
+    """
+    Choose a parser to use for this app.
+
+    Do local imports accordingly to not make etcd, yaml etc. mandatory requirements for
+    all users of eduid-common.
+
+    :param app_name: Name of the application
+    :param ns: Namespace for the application
+
+    :return: Config parser instance
+    """
     parser: Optional[BaseConfigParser] = None
     ns = os.environ.get('EDUID_CONFIG_NS', ns)
     app_name = os.environ.get('EDUID_CONFIG_APP_NAME', app_name)
     yaml_file = os.environ.get('EDUID_CONFIG_YAML')
     if yaml_file:
         try:
-            # Do not force applications that does not use EtcdConfigParser to have yaml and etcd installed
             from eduid_common.config.parsers.yaml import YamlConfigParser
 
             parser = YamlConfigParser(path=Path(yaml_file), ns=ns, app_name=app_name)
@@ -43,7 +53,6 @@ def _choose_parser(app_name: str, ns: str) -> Optional[BaseConfigParser]:
             raise ParserException('YamlConfigParser could not be imported')
     if not parser:
         try:
-            # Do not force applications that does not use EtcdConfigParser to have yaml and etcd installed
             from eduid_common.config.parsers.etcd import EtcdConfigParser
         except ImportError:
             raise ParserException('EtcdConfigParser could not be imported')
