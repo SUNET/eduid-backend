c0bca575f0fd2046b187c6b5ba4b8dad39ee8df9
Johan Lundberg <lundberg@sunet.se>
2021-02-11 17:09:50 +0100

    Use new config class

--- src/eduid_scimapi/config.py
+++ src/eduid_scimapi/config.py
@@ -1,26 +1,29 @@
-from dataclasses import dataclass, field
 from typing import Dict, List
 
-from eduid_common.config.base import BaseConfig
+from pydantic import Field
+
+from eduid_common.config.base import LoggingConfigMixin, RootConfig
 
 
-@dataclass
-class ScimApiConfig(BaseConfig):
+class ScimApiConfig(RootConfig, LoggingConfigMixin):
     """
     Configuration for the SCIM API app
     """
 
+    app_name: str = 'scimapi'
     testing: bool = False
-    schema: str = 'http'
+    protocol: str = 'http'
     server_name: str = 'localhost:8000'
     application_root: str = '/'
+    logging_config: dict = Field(default={})
     log_format: str = '{asctime} | {levelname:7} | {hostname} | {name:35} | {module:10} | {message}'
+    mongo_uri: str = ''
     neo4j_uri: str = ''
-    neo4j_config: Dict = field(default_factory=dict)
+    neo4j_config: Dict = Field(default_factory=dict)
     authorization_token_secret: str = 'secret'
     authorization_token_expire: int = 5 * 60
-    no_authn_urls: List[str] = field(default_factory=lambda: ['^/login$', '^/status/healthy$'])
-    data_owners: List[str] = field(default_factory=lambda: ['eduid.se'])
+    no_authn_urls: List[str] = Field(default=['^/login$', '^/status/healthy$'])
+    data_owners: List[str] = Field(default=['eduid.se'])
     # Invite config
     invite_url: str = ''
     invite_expire: int = 180 * 86400  # 180 days
