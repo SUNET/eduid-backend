964b7e1ed1217945fd9275a89922b09db0a25ce0
Johan Lundberg <lundberg@sunet.se>
2021-02-11 10:51:49 +0100

    merge fail

--- src/eduid_webapp/email/settings/common.py
+++ src/eduid_webapp/email/settings/common.py
@@ -36,10 +36,10 @@ Configuration (file) handling for the eduID email app.
 """
 
 
-from eduid_common.config.base import EduIDBaseAppConfig, MagicCookieMixin
+from eduid_common.config.base import EduIDBaseAppConfig, MagicCookieMixin, AmConfigMixin, MailConfigMixin
 
 
-class EmailConfig(EduIDBaseAppConfig, MagicCookieMixin):
+class EmailConfig(EduIDBaseAppConfig, MagicCookieMixin, AmConfigMixin, MailConfigMixin):
     """
     Configuration for the email app
     """
