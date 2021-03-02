06342dba1c4e51940ebd520b951aa5d9cb9c9847
Fredrik Thulin <fredrik@thulin.net>
2021-03-02 17:58:27 +0100

    use pip-tools to manage requirements

--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
-import os
+from pathlib import PurePath
+from typing import List
 
 from setuptools import find_packages, setup
 
-here = os.path.abspath(os.path.dirname(__file__))
-install_requires = [x for x in open(os.path.join(here, 'requirements.txt')).read().split('\n') if len(x) > 0]
-testing_extras = [
-    x
-    for x in open(os.path.join(here, 'test_requirements.txt')).read().split('\n')
-    if len(x) > 0 and not x.startswith('-')
-]
-client_extras = [x for x in open(os.path.join(here, 'requirements.txt')).read().split('\n') if len(x) > 0]
-
 version = '0.0.2'
+
+
+def load_requirements(path: PurePath) -> List[str]:
+    """ Load dependencies from a requirements.txt style file, ignoring comments etc. """
+    res = []
+    with open(path) as fd:
+        for line in fd.readlines():
+            while line.endswith('\n') or line.endswith('\\'):
+                line = line[:-1]
+            line = line.strip()
+            if not line or line.startswith('-') or line.startswith('#'):
+                continue
+            res += [line]
+    return res
+
+
+here = PurePath(__file__)
+README = open(here.with_name('README.md')).read()
+
+install_requires = load_requirements(here.with_name('requirements.txt'))
+test_requires = load_requirements(here.with_name('test_requirements.txt'))
 
 setup(
     name='eduid-queue',
@@ -25,7 +38,10 @@ setup(
     author_email='lundberg@sunet.se',
     description='MongoDB based task queue',
     install_requires=install_requires,
-    extras_require={'testing': testing_extras, 'client': client_extras},
+    test_requires=test_requires,
+    extras_require={'testing': [],
+                    'client': load_requirements(here.with_name('client_requirements.txt')),
+                    },
     include_package_data=True,
     entry_points={'console_scripts': ['run-mail-worker=eduid_queue.workers.mail:start_worker',],},
 )
