58a7bd32ad051938f0d7a63acb37081d55ffd919
Fredrik Thulin <fredrik@thulin.net>
2021-03-10 09:34:48 +0100

    fix task path

--- src/eduid/workers/lookup_mobile/worker.py
+++ src/eduid/workers/lookup_mobile/worker.py
@@ -1,14 +1,15 @@
+import eduid.workers.lookup_mobile.common as common
 from eduid.common.config.workers import MobConfig
 from eduid.common.rpc.celery import init_celery
 from eduid.common.rpc.worker import get_worker_config
-
-import eduid.workers.lookup_mobile.common as common
 
 worker_config: MobConfig = MobConfig(app_name='app_name_NOT_SET')
 
 if common.celery is None:
     worker_config = get_worker_config('lookup_mobile', config_class=MobConfig)
-    celery = init_celery('eduid_lookup_mobile', config=worker_config.celery, include=['eduid_lookup_mobile.tasks'])
+    celery = init_celery(
+        'eduid_lookup_mobile', config=worker_config.celery, include=['eduid.workers.lookup_mobile.tasks']
+    )
 
     # When Celery starts the worker, it expects there to be a 'celery' in the module it loads,
     # but our tasks expect to find the Celery instance in common.celery - so copy it there
--- src/eduid/workers/msg/worker.py
+++ src/eduid/workers/msg/worker.py
@@ -1,14 +1,13 @@
+import eduid.workers.msg.common as common
 from eduid.common.config.workers import MsgConfig
 from eduid.common.rpc.celery import init_celery
 from eduid.common.rpc.worker import get_worker_config
-
-import eduid.workers.msg.common as common
 
 worker_config: MsgConfig = MsgConfig(app_name='app_name_NOT_SET')
 
 if common.celery is None:
     worker_config = get_worker_config('msg', config_class=MsgConfig)
-    celery = init_celery('eduid_msg', config=worker_config.celery, include=['eduid_msg.tasks'])
+    celery = init_celery('eduid_msg', config=worker_config.celery, include=['eduid.workers.msg.tasks'])
 
     # When Celery starts the worker, it expects there to be a 'celery' in the module it loads,
     # but our tasks expect to find the Celery instance in eduid.workers.am.common.celery - so copy it there
