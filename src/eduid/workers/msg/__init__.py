42503923551ede26fd4d810262bc75aa393a9028
Fredrik Thulin <fredrik@thulin.net>
2021-03-12 09:42:53 +0100

    type fix - nothing optional here

--- src/eduid/workers/msg/__init__.py
+++ src/eduid/workers/msg/__init__.py
@@ -4,11 +4,10 @@ The eduID messaging package.
 Copyright (c) 2013, 2014, 2015, 2018 SUNET. All rights reserved.
 See the file LICENSE.txt for full license statement.
 """
-from typing import Optional
 
 from eduid.common.config.base import CeleryConfig
 from eduid.workers.msg.common import MsgCelerySingleton
 
 
-def init_app(config: Optional[CeleryConfig]) -> None:
+def init_app(config: CeleryConfig) -> None:
     MsgCelerySingleton.update_celery_config(config)
