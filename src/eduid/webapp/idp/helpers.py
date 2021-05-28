903764e39e1222fb51b5d8a269fc95e7a043ac34
Fredrik Thulin <fredrik@thulin.net>
2021-05-28 15:56:11 +0200

    change idp. to login.

--- src/eduid/webapp/idp/helpers.py
+++ src/eduid/webapp/idp/helpers.py
@@ -5,25 +5,25 @@ from eduid.webapp.common.api.messages import TranslatableMsg
 
 @unique
 class IdPMsg(str, TranslatableMsg):
-    action_required = 'idp.action_required'  # Shouldn't actually be returned to the frontend
-    assurance_failure = 'idp.assurance_failure'
-    assurance_not_possible = 'idp.assurance_not_possible'
-    bad_ref = 'idp.bad_ref'
-    credential_expired = 'idp.credential_expired'
-    general_failure = 'idp.general_failure'
-    mfa_required = 'idp.mfa_required'
-    mfa_auth_failed = 'idp.mfa_auth_failed'
-    must_authenticate = 'idp.must_authenticate'
-    not_available = 'idp.not_available'
-    not_implemented = 'idp.not_implemented'
-    proceed = 'idp.proceed'  # Shouldn't actually be returned to the frontend
-    swamid_mfa_required = 'idp.swamid_mfa_required'
-    tou_not_acceptable = 'idp.tou_not_acceptable'
-    tou_required = 'idp.tou_required'
-    user_temporary_locked = 'idp.user_temporary_locked'
-    user_terminated = 'idp.user_terminated'
-    wrong_credentials = 'idp.wrong_credentials'
-    wrong_user = 'idp.wrong_user'
+    action_required = 'login.action_required'  # Shouldn't actually be returned to the frontend
+    assurance_failure = 'login.assurance_failure'
+    assurance_not_possible = 'login.assurance_not_possible'
+    bad_ref = 'login.bad_ref'
+    credential_expired = 'login.credential_expired'
+    general_failure = 'login.general_failure'
+    mfa_required = 'login.mfa_required'
+    mfa_auth_failed = 'login.mfa_auth_failed'
+    must_authenticate = 'login.must_authenticate'
+    not_available = 'login.not_available'
+    not_implemented = 'login.not_implemented'
+    proceed = 'login.proceed'  # Shouldn't actually be returned to the frontend
+    swamid_mfa_required = 'login.swamid_mfa_required'
+    tou_not_acceptable = 'login.tou_not_acceptable'
+    tou_required = 'login.tou_required'
+    user_temporary_locked = 'login.user_temporary_locked'
+    user_terminated = 'login.user_terminated'
+    wrong_credentials = 'login.wrong_credentials'
+    wrong_user = 'login.wrong_user'
 
 
 @unique
