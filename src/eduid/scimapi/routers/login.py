88580456fe1155badbe58c2e1f32cdb21b6c3cef
Johan Lundberg <lundberg@sunet.se>
2021-06-17 11:02:18 +0200

    scimapi login not ported to fastapi
    added test for login

--- src/eduid/scimapi/routers/login.py
+++ src/eduid/scimapi/routers/login.py
@@ -3,6 +3,7 @@ import datetime
 from fastapi import APIRouter, Response
 from jose import jwt
 
+from eduid.scimapi.context_request import ContextRequest
 from eduid.scimapi.exceptions import Unauthorized
 from eduid.scimapi.models.login import TokenRequest
 
@@ -10,15 +11,15 @@ login_router = APIRouter(prefix='/login')
 
 
 @login_router.post('/')
-async def get_token(self, token_req: TokenRequest, response: Response) -> None:
-    self.context.logger.info(f'Logging in')
-    if token_req.data_owner not in self.context.config.data_owners:
+async def get_token(req: ContextRequest, resp: Response, token_req: TokenRequest) -> None:
+    req.app.context.logger.info(f'Logging in')
+    if token_req.data_owner not in req.app.context.config.data_owners:
         raise Unauthorized()
     now = datetime.datetime.now(tz=datetime.timezone.utc)
-    expire = now + datetime.timedelta(seconds=self.context.config.authorization_token_expire)
+    expire = now + datetime.timedelta(seconds=req.app.context.config.authorization_token_expire)
     claims = {
         'data_owner': token_req.data_owner,
         'exp': expire,
     }
-    token = jwt.encode(claims, self.context.config.authorization_token_secret, algorithm='HS256')
-    response.headers['Authorization'] = f'Bearer {token}'
+    token = jwt.encode(claims, req.app.context.config.authorization_token_secret, algorithm='HS256')
+    resp.headers['Authorization'] = f'Bearer {token}'
