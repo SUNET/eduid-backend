18d8aa35cd4d72e18b388c515ce9feaada59f5e9
Johan Lundberg <lundberg@sunet.se>
2021-06-24 17:34:11 +0200

    Added convenience functions that will be in python 3.9

--- src/eduid/common/utils.py
+++ src/eduid/common/utils.py
@@ -25,3 +25,19 @@ def urlappend(base: str, path: str) -> str:
     if not base.endswith('/'):
         base = '{!s}/'.format(base)
     return '{!s}{!s}'.format(base, path)
+
+
+# TODO: removeprefix and removesuffix be a part of str in python 3.9
+def removeprefix(s: str, prefix: str) -> str:
+    if s.startswith(prefix):
+        return s[len(prefix) :]
+    else:
+        return s[:]
+
+
+def removesuffix(s: str, suffix: str) -> str:
+    # suffix='' should not call self[:-0].
+    if suffix and s.endswith(suffix):
+        return s[: -len(suffix)]
+    else:
+        return s[:]
