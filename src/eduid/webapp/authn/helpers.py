8600d5711925c53ce828eabf53e6dd5ff9d4be6f
Fredrik Thulin <fredrik@thulin.net>
2021-07-02 15:25:05 +0200

    avoid misleading variable name

--- src/eduid/webapp/authn/helpers.py
+++ src/eduid/webapp/authn/helpers.py
@@ -9,10 +9,9 @@ def credential_used_to_log_in(credential: Credential, max_age: int = 60) -> bool
     Check if a particular credential was used to log in (using the eduID IdP and authn).
     """
     login_authn = session.authn.sp.get_authn_for_action(AuthnAcsAction.login)
-    credential_already_used = False
     if login_authn and credential.key in login_authn.credentials_used:
         if login_authn.authn_instant is not None:
             age = (utc_now() - login_authn.authn_instant).total_seconds()
             if 0 < age < max_age:
-                credential_already_used = True
-    return credential_already_used
+                return True
+    return False
