ffa45bef309be3b607c5a1a574905c853881be55
Fredrik Thulin <fredrik@thulin.net>
2021-08-20 13:52:02 +0200

    nin_to_age: refactor to solve off-by-one error on people's birthday

---
+++ src/eduid/webapp/lookup_mobile_proofing/tests/test_helpers.py
@@ -0,0 +1,16 @@
+import unittest
+from datetime import datetime
+
+from eduid.webapp.lookup_mobile_proofing.helpers import nin_to_age
+
+
+class HelperTests(unittest.TestCase):
+    def test_nin_to_age(self):
+        now = datetime.fromisoformat('2021-08-21T00:00:00')
+        assert nin_to_age('20210820abcd', now=now) == 0
+
+        assert nin_to_age('202008211234', now=now) == 1
+
+        assert nin_to_age('20200820abcd', now=now) == 1
+
+        assert nin_to_age('20000101abcd', now=now) == 21
