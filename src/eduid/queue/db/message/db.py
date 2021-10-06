c4e30636fa6f05bc31ae01f0d3bfc8d6db52319c
Fredrik Thulin <fredrik@thulin.net>
2021-10-06 17:50:39 +0200

    add a sink worker

--- src/eduid/queue/db/message/db.py
+++ src/eduid/queue/db/message/db.py
@@ -33,6 +33,11 @@ from eduid.queue.db import QueueDB
 __author__ = 'lundberg'
 
 
+class TestDB(QueueDB):
+    def __init__(self, db_uri: str, collection: str = 'test'):
+        super().__init__(db_uri, collection=collection)
+
+
 class MessageDB(QueueDB):
     def __init__(self, db_uri: str, collection: str = 'message'):
         super().__init__(db_uri, collection=collection)
---
+++ src/eduid/queue/workers/sink.py
@@ -0,0 +1,96 @@
+""" Test worker that just logs received items """
+import asyncio
+import logging
+import os
+from asyncio import Task
+from datetime import datetime, timedelta
+from typing import Any, List, Mapping, Optional
+
+from eduid.common.config.parsers import load_config
+from eduid.queue.config import QueueWorkerConfig
+from eduid.queue.db import QueueItem, SenderInfo
+from eduid.queue.db.message.payload import EduidTestPayload, EduidTestResultPayload
+from eduid.queue.workers.base import QueueWorker
+from eduid.userdb.util import utc_now
+
+logger = logging.getLogger(__name__)
+
+__author__ = 'lundberg'
+
+
+class SinkQueueWorker(QueueWorker):
+    def __init__(self, config: QueueWorkerConfig):
+        # Register which queue items this worker should try to grab
+        payloads = [EduidTestPayload]
+        super().__init__(config=config, handle_payloads=payloads)
+
+        self._receiving = False
+        self._counter = 0
+        self._first_ts: Optional[datetime] = None
+        self._last_ts: Optional[datetime] = None
+        self._sender_info = SenderInfo(hostname=os.environ.get('HOSTNAME'), node_id='sink_worker')
+
+    async def handle_new_item(self, queue_item: QueueItem) -> None:
+        if queue_item.payload_type == EduidTestPayload.get_type():
+            self._receiving = True
+            logger.info(f'Received queue item: {queue_item.item_id}')
+            logger.debug(queue_item)
+            now = utc_now()
+            if not self._first_ts:
+                self._first_ts = now
+            self._counter += 1
+            self._last_ts = now
+
+        await self.item_successfully_handled(queue_item)
+
+    async def handle_expired_item(self, queue_item: QueueItem) -> None:
+        logger.warning(f'Found expired item: {queue_item}')
+
+    async def collect_periodic_tasks(self) -> List[Task]:
+        tasks = await super().collect_periodic_tasks()
+        tasks += [asyncio.create_task(self.periodic_stats_publishing(), name='periodic_stats_publishing')]
+        return tasks
+
+    async def periodic_stats_publishing(self):
+        if not self._receiving and self._counter:
+            # publish statistics when no longer receiving new items for a whole period
+
+            delta = self._last_ts - self._first_ts
+            per_second = self._counter / delta.total_seconds()
+            payload = EduidTestResultPayload(
+                counter=self._counter,
+                first_ts=self._first_ts,
+                last_ts=self._last_ts,
+                delta=str(delta),
+                per_second=int(per_second),
+            )
+            self._last_ts = None
+            self._counter = 0
+
+            now = utc_now()
+            qitem = QueueItem(
+                version=1,
+                expires_at=now + timedelta(seconds=3),
+                discard_at=now + timedelta(seconds=6),
+                sender_info=self._sender_info,
+                payload_type=payload.get_type(),
+                payload=payload,
+            )
+            logger.info(f'Test results this period: {payload}')
+            self.db.save(qitem)
+
+        self._receiving = False
+
+
+def init_sink_worker(name: str = 'sink_worker', test_config: Optional[Mapping[str, Any]] = None) -> SinkQueueWorker:
+    config = load_config(typ=QueueWorkerConfig, app_name=name, ns='queue', test_config=test_config)
+    return SinkQueueWorker(config=config)
+
+
+def start_worker():
+    worker = init_sink_worker()
+    exit(asyncio.run(worker.run()))
+
+
+if __name__ == '__main__':
+    start_worker()
