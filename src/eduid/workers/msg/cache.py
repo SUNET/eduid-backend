0c441c4a9245036f60176980081c669a2004a841
Fredrik Thulin <fredrik@thulin.net>
2021-10-21 18:20:51 +0200

    modernise CacheDB to use auto-discard in mongodb

--- src/eduid/workers/msg/cache.py
+++ src/eduid/workers/msg/cache.py
@@ -1,64 +1,45 @@
-from datetime import datetime
-from time import time
-from typing import Optional
+from typing import Any, Dict, Optional, Set
 
-from eduid.userdb.db import MongoDB
+from eduid.userdb.db import BaseDB
+from eduid.userdb.util import utc_now
 
 
-# TODO: Update to use mongodb built-in expiry
-class CacheMDB(object):
+class CacheMDB(BaseDB):
 
-    _init_collections: dict = {}
+    _init_collections: Set[str] = set()
 
-    def __init__(
-        self, mongo_dburi: Optional[str], mongo_dbname: str, mongo_collection: str, ttl: int, expiration_freq: int = 60
-    ):
-        self.conn = MongoDB(mongo_dburi, db_name=mongo_dbname)
-        self.db = self.conn.get_database(mongo_dbname)
-        self.collection = self.db[mongo_collection]
-        self._expiration_freq = expiration_freq
-        self._last_expire_at: Optional[int] = None
-        self._ttl = ttl
-        self.ensure_indices(mongo_collection)
+    def __init__(self, db_uri: str, db_name: str, collection: str, ttl: int):
+        super().__init__(db_uri=db_uri, db_name=db_name, collection=collection)
+        indexes = {
+            # Remove cache entries after TTL expires
+            'auto-discard': {'key': [('created_at', 1)], 'expireAfterSeconds': ttl},
+            # Ensure unique cache item identifier
+            'unique-scimid': {'key': [('identifier', 1)], 'unique': True},
+        }
+        self.setup_indexes(indexes)
 
-    def add_cache_item(self, identifier, data):
-        date = datetime.fromtimestamp(time(), None)
+    def add_cache_item(self, identifier: str, data: Dict[str, Any]):
+        date = utc_now()
         doc = {'identifier': identifier, 'data': data, 'created_at': date}
-        self.collection.insert_one(doc)
-        self.expire_cache_items()
+        self._coll.insert_one(doc)
         return True
 
-    def get_cache_item(self, identifier):
+    def get_cache_item(self, identifier: str) -> Optional[Dict[str, Any]]:
         query = {'identifier': identifier}
-        result = self.collection.find_one(query)
+        result = self._coll.find_one(query)
         if result is not None:
             return result['data']
         return result
 
     def get_cached_items_by_query(self, query):
-        result = self.collection.find(query)
+        result = self._coll.find(query)
         return result
 
-    def update_cache_item(self, identifier, data):
-        date = datetime.fromtimestamp(time(), None)
-        return self.collection.update(
+    def update_cache_item(self, identifier: str, data: Dict[str, Any]):
+        date = utc_now()
+        return self._coll.update(
             {'identifier': identifier}, {'$set': {'data': data, 'updated_at': date}}, w=1, getLastError=True
         )
 
-    def remove_cache_item(self, identifier):
-        return self.collection.delete_one({'identifier': identifier})
-
-    def expire_cache_items(self, force=False):
-        ts = int(time()) - self._ttl
-        if not force and (self._last_expire_at is not None) and (self._last_expire_at > ts - self._expiration_freq):
-            return False
-        self._last_expire_at = ts
-        date = datetime.fromtimestamp(ts, None)
-        self.collection.delete_many({'created_at': {'$lt': date}})
-        return True
-
-    def ensure_indices(self, collection):
-        if collection not in self._init_collections:  # Only ensure indices once
-            self._init_collections[collection] = 1
-            self.db[collection].create_index('created_at', name='created_at_idx', unique=False)
-            self.db[collection].create_index('identifier', name='identifier_idx', unique=True)
+    def remove_cache_item(self, identifier: str):
+        return self._coll.delete_one({'identifier': identifier})
