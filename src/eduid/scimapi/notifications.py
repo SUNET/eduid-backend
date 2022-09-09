b5350f65f3998b4e809357ae878f69b7e19cdbb9
Fredrik Thulin <fredrik@thulin.net>
2022-09-09 17:29:06 +0200

    send notifications of SCIM events using an in-house queue worker instead of Amazon SNS

--- src/eduid/scimapi/notifications.py
+++ src/eduid/scimapi/notifications.py
@@ -3,42 +3,59 @@ __author__ = 'lundberg'
 
 import json
 import logging
-from typing import Any, Dict, List
+from datetime import datetime, timedelta
+from os import environ
+from typing import Any, Dict, List, NewType, TYPE_CHECKING
 
-import boto3
-
+from eduid.queue.db import QueueItem, SenderInfo
+from eduid.queue.db.message.payload import EduidSCIMAPINotification
 from eduid.scimapi.config import DataOwnerName, ScimApiConfig
 
+if TYPE_CHECKING:
+    from eduid.scimapi.context import Context
+
 logger = logging.getLogger(__name__)
+
+TFormattedMessage = NewType('TFormattedMessage', str)
 
 
 class NotificationRelay:
     def __init__(self, config: ScimApiConfig):
         self.config = config
-        self.sns_client = None
-        if config.aws_access_key_id and config.aws_secret_access_key and config.aws_region:
-            boto3.setup_default_session(
-                aws_access_key_id=config.aws_access_key_id,
-                aws_secret_access_key=config.aws_secret_access_key,
-                region_name=config.aws_region,
-            )
-            self.sns_client = boto3.client('sns')
+        app_name = config.app_name
+        system_hostname = environ.get('SYSTEM_HOSTNAME', '')  # Underlying hosts name for containers
+        hostname = environ.get('HOSTNAME', '')  # Actual hostname or container id
+        self.sender_info = SenderInfo(hostname=hostname, node_id=f'{app_name}@{system_hostname}')
 
-    def _topics_for(self, data_owner: DataOwnerName) -> List[str]:
+    def _urls_for(self, data_owner: DataOwnerName) -> List[str]:
         if data_owner not in self.config.data_owners:
             return []
         return self.config.data_owners[data_owner].notify
 
-    def format_message(self, version: int, data: Dict[str, Any]):
+    def format_message(self, version: int, data: Dict[str, Any]) -> TFormattedMessage:
         if version != 1:
             raise NotImplementedError(f'version {version} not implemented')
-        return json.dumps({'v': version, 'location': data['location']})
+        return TFormattedMessage(json.dumps({'v': version, 'location': data['location']}))
 
-    def notify(self, data_owner: DataOwnerName, message: str) -> None:
-        if self.sns_client is not None:
-            logger.info(f'Notifying {data_owner}')
-            logger.debug(f'message: {message}')
-            for topic_arn in self._topics_for(data_owner):
-                logger.debug(f'TopicArn: {topic_arn}')
-                res = self.sns_client.publish(TopicArn=topic_arn, Message=message)
-                logger.debug(f'Publish result: {res}')
+    def notify(self, data_owner: DataOwnerName, message: TFormattedMessage, context: 'Context') -> None:
+        """
+        Send a request for 'someone else' to POST information about this event to a URL.
+        """
+        expires_at = datetime.utcnow() + timedelta(seconds=self.config.invite_expire)
+        discard_at = expires_at + timedelta(days=7)
+        _urls = self._urls_for(data_owner)
+        if not _urls:
+            context.logger.debug(f'No notification urls for data owner {data_owner}')
+            return None
+        for post_url in _urls:
+            payload = EduidSCIMAPINotification(data_owner=data_owner, message=message, post_url=post_url)
+            item = QueueItem(
+                version=1,
+                expires_at=expires_at,
+                discard_at=discard_at,
+                sender_info=self.sender_info,
+                payload_type=payload.get_type(),
+                payload=payload,
+            )
+            context.messagedb.save(item)
+            context.logger.info(f'Saved notification {item.item_id} in message queue')
