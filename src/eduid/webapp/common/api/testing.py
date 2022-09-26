fd2d499bf779cb965f02ddfa97e394441fc6882a
Johan Lundberg <lundberg@sunet.se>
2022-09-26 14:32:44 +0200

    merge and move normalised_data from webapp.common and userdb to proper common
    implement replace_datetime to replace any datetime with an arbitrary
    value

--- src/eduid/webapp/common/api/testing.py
+++ src/eduid/webapp/common/api/testing.py
@@ -30,15 +30,13 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 #
-import json
 import logging.config
 import pprint
 import sys
 import traceback
 from contextlib import contextmanager
 from copy import deepcopy
-from datetime import datetime
-from typing import Any, Dict, List, Mapping, Optional, Sequence, Union
+from typing import Any, Dict, List, Mapping, Optional
 
 from flask import Response
 from flask.testing import FlaskClient
@@ -403,42 +401,3 @@ class CSRFTestClient(FlaskClient):
         """
         with super().session_transaction(*args, **kwargs) as sess:  # type: ignore
             yield sess
-
-
-def normalised_data(
-    data: Union[Mapping[str, Any], Sequence[Mapping[str, Any]]]
-) -> Union[Dict[str, Any], List[Dict[str, Any]]]:
-    """Utility function for normalising dicts (or list of dicts) before comparisons in test cases."""
-    if isinstance(data, list):
-        # Recurse into lists of dicts. mypy (correctly) says this recursion can in fact happen
-        # more than once, so the result can be a list of list of dicts or whatever, but the return
-        # type becomes too bloated with that in mind and the code becomes too inelegant when unrolling
-        # this list comprehension into a for-loop checking types for something only intended to be used in test cases.
-        # Hence the type: ignore.
-        return sorted([_normalise_value(x) for x in data], key=_any_key)  # type: ignore
-    elif isinstance(data, dict):
-        # normalise all values found in the dict, returning a new dict (to not modify callers data)
-        return {k: _normalise_value(v) for k, v in data.items()}
-    raise TypeError('normalised_data not called on dict (or list of dicts)')
-
-
-class SortEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime):
-            return str(_normalise_value(obj))
-        return json.JSONEncoder.default(self, obj)
-
-
-def _any_key(value: Any):
-    """Helper function to be able to use sorted with key argument for everything"""
-    if isinstance(value, dict):
-        return json.dumps(value, sort_keys=True, cls=SortEncoder)  # Turn dict in to a string for sorting
-    return value
-
-
-def _normalise_value(data: Any) -> Any:
-    if isinstance(data, dict) or isinstance(data, list):
-        return normalised_data(data)
-    elif isinstance(data, datetime):
-        return data.replace(microsecond=0, tzinfo=None)
-    return data
