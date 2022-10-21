3b0d0ef54eed91517b1356229471fc012b607ead
Johan Lundberg <lundberg@sunet.se>
2022-10-21 15:06:00 +0200

    please mypy

--- src/eduid/webapp/eidas/views.py
+++ src/eduid/webapp/eidas/views.py
@@ -185,12 +185,13 @@ def _authn(
     if check_magic_cookie(current_app.conf):
         # set a test IdP with minimal interaction for the integration tests
         if current_app.conf.magic_cookie_idp:
-            if proofing_method.method == "freja":
+            if proofing_method.method == "freja" and current_app.conf.magic_cookie_idp:
                 idp = current_app.conf.magic_cookie_idp
-            elif proofing_method.method == "eidas":
+            elif proofing_method.method == "eidas" and current_app.conf.magic_cookie_foreign_id_idp:
                 idp = current_app.conf.magic_cookie_foreign_id_idp
             else:
                 current_app.logger.error(f"Magic cookie is not supported for method {method}")
+                return AuthnResult(error=EidasMsg.method_not_available)
             current_app.logger.debug(f"Changed requested IdP due to magic cookie: {idp}")
         else:
             current_app.logger.warning(f"Missing configuration magic_cookie_idp")
