2c2b0a81538da0e92e729fc463ca50c93da51c59
masv3971 <masv@sunet.se>
2022-11-15 12:49:10 +0100

    Remove nomalised_data, wrong in merge.

--- src/eduid/userdb/tests/test_userdb.py
+++ src/eduid/userdb/tests/test_userdb.py
@@ -32,7 +32,6 @@
 from datetime import timedelta
 from typing import Any, Dict
 
-
 import bson
 
 from eduid.common.testing_base import normalised_data
@@ -40,8 +39,9 @@ from eduid.userdb import User
 from eduid.userdb.exceptions import UserOutOfSync
 from eduid.userdb.fixtures.passwords import signup_password
 from eduid.userdb.fixtures.users import mocked_user_standard, mocked_user_standard_2
-from eduid.userdb.testing import MongoTestCase, normalised_data, MongoTestCaseRaw
+from eduid.userdb.testing import MongoTestCase, MongoTestCaseRaw
 from eduid.userdb.util import utc_now
+
 
 class TestUserDB(MongoTestCase):
     def setUp(self, *args, **kwargs):
