e33009ceb3304dd71bf06e1d294b522fffcfc32c
Johan Lundberg <lundberg@sunet.se>
2023-01-30 18:21:40 +0100

    bump proofing user as the last iteration of the method was done in 2023

--- src/eduid/common/config/base.py
+++ src/eduid/common/config/base.py
@@ -367,7 +367,7 @@ class ProofingConfigMixin(BaseModel):
     # identity proofing
     freja_proofing_version: str = Field(default="2018v1")
     foreign_eid_proofing_version: str = Field(default="2022v1")
-    svipe_id_proofing_version: str = Field(default="2022v1")
+    svipe_id_proofing_version: str = Field(default="2023v1")
 
     # security key proofing
     security_key_proofing_method: CredentialProofingMethod = Field(default=CredentialProofingMethod.SWAMID_AL3_MFA)
