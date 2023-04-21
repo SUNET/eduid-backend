dccd51d9b2dae07a9006eee9dd8032ecc9c3a5d1
masv3971 <masv@sunet.se>
2023-04-21 16:47:17 +0200

    Add debug logging and print to stderr.

--- src/eduid/vccs/server/run.py
+++ src/eduid/vccs/server/run.py
@@ -1,4 +1,5 @@
 from asyncio import Lock
+import sys
 from binascii import unhexlify
 from typing import Any, Mapping, Optional
 
@@ -21,6 +22,7 @@ from eduid.vccs.server.log import InterceptHandler, init_logging
 
 class VCCS_API(FastAPI):
     def __init__(self, test_config: Optional[Mapping[str, Any]] = None):
+        print("vccs_api is starting", file=sys.stderr)
         super().__init__()
 
         self.state.config = init_config(ns="api", app_name="vccs", test_config=test_config)
