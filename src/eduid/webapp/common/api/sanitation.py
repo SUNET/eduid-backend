e153160a6e158cdbe40af7799b6768ed8fd65bdc
Johan Lundberg <lundberg@sunet.se>
2023-09-21 13:25:26 +0200

    remove logger arguement from request sanitation
    flask do no longer allow accessing request attributes outside request
    context

--- src/eduid/webapp/common/api/sanitation.py
+++ src/eduid/webapp/common/api/sanitation.py
@@ -34,9 +34,8 @@ from typing import AnyStr, Optional
 from urllib.parse import quote, unquote
 
 from bleach import clean
-from flask import request
 
-module_logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 class SanitationProblem(Exception):
@@ -53,7 +52,6 @@ class Sanitizer:
         untrusted_text: AnyStr,
         content_type: Optional[str] = None,
         strip_characters: bool = False,
-        logger: Optional[logging.Logger] = None,
     ) -> str:
         """
         Sanitize user input by escaping or removing potentially
@@ -61,14 +59,11 @@ class Sanitizer:
         bleach as recommended by OWASP.
 
         :param untrusted_text: User input to sanitize
-        :param logger: logging facility
         :param content_type: Content type of the input to sanitize
         :param strip_characters: Set to True to remove instead of escaping potentially harmful input.
 
         :return: Sanitized user input
         """
-        if logger is None:
-            logger = module_logger
         try:
             # Test if the untrusted text is percent encoded before running bleach.
             _untrusted_text: str
@@ -81,7 +76,6 @@ class Sanitizer:
 
             return self._sanitize_input(
                 _untrusted_text,
-                logger,
                 strip_characters=strip_characters,
                 content_type=content_type,
                 percent_encoded=use_percent_encoding,
@@ -96,7 +90,6 @@ class Sanitizer:
     def _sanitize_input(
         self,
         untrusted_text: str,
-        logger: logging.Logger,
         strip_characters: bool = False,
         content_type: Optional[str] = None,
         percent_encoded: bool = False,
@@ -116,16 +109,13 @@ class Sanitizer:
         :return: Sanitized user input
         """
 
-        # Decide on whether or not to use percent encoding:
+        # Decide on whether to use percent encoding:
         # 1. Check if the content type has been explicitly set
         # 2. If set, use percent encoding if requested by the client
         # 3. If the content type has not been explicitly set,
         # 3.1 use percent encoding according to the calling
         #    functions preference or,
         # 3.2 use the default value as set in the function definition.
-        if content_type is None and hasattr(request, "mimetype"):
-            content_type = request.mimetype
-
         if isinstance(content_type, str) and content_type:
             if content_type == "application/x-www-form-urlencoded":
                 use_percent_encoding = True
@@ -142,7 +132,7 @@ class Sanitizer:
             # 3. Percent encode, if needed, and returning it back.
 
             decoded_text = unquote(untrusted_text)
-            cleaned_text = self._safe_clean(decoded_text, logger, strip_characters)
+            cleaned_text = self._safe_clean(decoded_text, strip_characters)
 
             if decoded_text != cleaned_text:
                 logger.warning("Some potential harmful characters were removed from untrusted user input.")
@@ -156,14 +146,14 @@ class Sanitizer:
         # If the untrusted_text is not percent encoded we only have to:
         # 1. Clean it to remove dangerous characters.
 
-        cleaned_text = self._safe_clean(untrusted_text, logger, strip_characters)
+        cleaned_text = self._safe_clean(untrusted_text, strip_characters)
 
         if untrusted_text != cleaned_text:
             logger.warning("Some potential harmful characters were removed from untrusted user input.")
 
         return cleaned_text
 
-    def _safe_clean(self, untrusted_text: str, logger: logging.Logger, strip_characters: bool = False) -> str:
+    def _safe_clean(self, untrusted_text: str, strip_characters: bool = False) -> str:
         """
         Wrapper for the clean function of bleach to be able
         to catch when illegal UTF-8 is processed.
