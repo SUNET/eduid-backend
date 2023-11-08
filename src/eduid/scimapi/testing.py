a8b7ae87d391789109f89774d638600209a6c378
Johan Lundberg <lundberg@sunet.se>
2023-11-08 09:47:20 +0100

    properly tear down tests with groupdb

--- src/eduid/scimapi/testing.py
+++ src/eduid/scimapi/testing.py
@@ -83,10 +83,6 @@ class MongoNeoTestCase(BaseDBTestCase):
         )
         super().setUpClass()
 
-    def tearDown(self):
-        super().tearDown()
-        self.neo4j_instance.purge_db()
-
 
 class ScimApiTestCase(MongoNeoTestCase):
     """Base test case providing the real API"""
@@ -107,6 +103,7 @@ class ScimApiTestCase(MongoNeoTestCase):
         # TODO: more tests for scoped groups when that is implemented
         self.data_owner = DataOwnerName("eduid.se")
         self.userdb = self.context.get_userdb(self.data_owner)
+        self.groupdb = self.context.get_groupdb(self.data_owner)
         self.invitedb = self.context.get_invitedb(self.data_owner)
         self.signup_invitedb = SignupInviteDB(db_uri=config.mongo_uri)
         self.messagedb = MessageDB(db_uri=config.mongo_uri)
@@ -155,6 +152,9 @@ class ScimApiTestCase(MongoNeoTestCase):
             self.signup_invitedb._drop_whole_collection()
         if self.messagedb:
             self.messagedb._drop_whole_collection()
+        if self.groupdb:
+            self.groupdb._drop_whole_collection()
+            self.neo4j_instance.purge_db()
 
     def _assertScimError(
         self,
