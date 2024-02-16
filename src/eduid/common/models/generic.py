6d4f93618b29a3246532c8a97bbd44521dd9b4af
Johan Lundberg <lundberg@sunet.se>
2024-02-16 15:19:07 +0100

    added a place for generic models

---
+++ src/eduid/common/models/generic.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+from typing import Any
+
+from bson import ObjectId
+from pydantic import GetCoreSchemaHandler, GetJsonSchemaHandler
+from pydantic.json_schema import JsonSchemaValue
+from pydantic_core import core_schema
+
+__author__ = "lundberg"
+
+
+# https://docs.pydantic.dev/2.6/concepts/types/#handling-third-party-types
+class ObjectIdPydanticAnnotation:
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls,
+        _source_type: Any,
+        _handler: GetCoreSchemaHandler,
+    ) -> core_schema.CoreSchema:
+        """
+        We return a pydantic_core.CoreSchema that behaves in the following ways:
+
+        * strs will be parsed as `ObjectId` instances
+        * `ObjectId` instances will be parsed as `ObjectId` instances without any changes
+        * Nothing else will pass validation
+        * Serialization will always return just a str
+        """
+
+        def validate_from_str(value: str) -> ObjectId:
+            return ObjectId(value)
+
+        from_str_schema = core_schema.chain_schema(
+            [
+                core_schema.str_schema(),
+                core_schema.no_info_plain_validator_function(validate_from_str),
+            ]
+        )
+
+        return core_schema.json_or_python_schema(
+            json_schema=from_str_schema,
+            python_schema=core_schema.union_schema(
+                [
+                    # check if it's an instance first before doing any further work
+                    core_schema.is_instance_schema(ObjectId),
+                    from_str_schema,
+                ]
+            ),
+            serialization=core_schema.plain_serializer_function_ser_schema(lambda instance: str(instance)),
+        )
+
+    @classmethod
+    def __get_pydantic_json_schema__(
+        cls, _core_schema: core_schema.CoreSchema, handler: GetJsonSchemaHandler
+    ) -> JsonSchemaValue:
+        # Use the same schema that would be used for `str`
+        return handler(core_schema.str_schema())
