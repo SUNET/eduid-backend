c600762a4327a4a0fc5f57b743221160c39e28da
Johan Lundberg <lundberg@sunet.se>
2024-02-27 08:47:25 +0100

    Optional now needs explicit default None

--- src/eduid/webapp/jsconfig/settings/jsapps.py
+++ src/eduid/webapp/jsconfig/settings/jsapps.py
@@ -16,55 +16,55 @@ class JsAppsConfig(PasswordConfigMixin):
     available_languages: dict[str, str] = Field(default={"en": "English", "sv": "Svenska"})
     csrf_token: Optional[str] = None
     dashboard_link: HttpUrlStr
-    dashboard_url: Optional[str]  # deprecated
+    dashboard_url: Optional[str] = None  # deprecated
     debug: bool = False
     eduid_site_link: HttpUrlStr = Field(default=HttpUrlStr("https://eduid.se"))
     eduid_site_name: str = "eduID"
     eduid_site_url: Optional[str] = "https://eduid.se"  # deprecated
     environment: EduidEnvironment = EduidEnvironment.production
     faq_link: HttpUrlStr
-    reset_password_link: HttpUrlStr  # used for directing a user to the reset password app
+    # reset_password_link is used for directing a user to the reset password app
+    reset_password_link: HttpUrlStr
     sentry_dsn: Optional[str] = None
     signup_link: HttpUrlStr
-    signup_url: Optional[str]  # deprecated
-    static_faq_url: Optional[str]  # deprecated
+    signup_url: Optional[str] = None  # deprecated
+    static_faq_url: Optional[str] = None  # deprecated
     # backend endpoint urls
     authn_service_url: HttpUrlStr
-    authn_url: Optional[str]  # deprecated
+    authn_url: Optional[str] = None  # deprecated
     bankid_service_url: HttpUrlStr
     eidas_service_url: HttpUrlStr
-    eidas_url: Optional[str]  # deprecated
+    eidas_url: Optional[str] = None  # deprecated
     emails_service_url: HttpUrlStr
-    emails_url: Optional[str]  # deprecated
-    error_info_url: Optional[HttpUrlStr] = (
-        None  # Needs to be a full URL since the backend is on the idp, not on https://eduid.se
-    )
+    emails_url: Optional[str] = None  # deprecated
+    # error_info_url needs to be a full URL since the backend is on the idp, not on https://eduid.se
+    error_info_url: Optional[HttpUrlStr] = None
     group_mgmt_service_url: HttpUrlStr
-    group_mgmt_url: Optional[str]  # deprecated
+    group_mgmt_url: Optional[str] = None  # deprecated
     ladok_service_url: HttpUrlStr
-    ladok_url: Optional[str]  # deprecated
+    ladok_url: Optional[str] = None  # deprecated
     letter_proofing_service_url: HttpUrlStr
-    letter_proofing_url: Optional[str]  # deprecated
-    login_base_url: Optional[AnyUrlStr]  # deprecated
-    login_next_url: HttpUrlStr  # Needs to be a full URL since the backend is on the idp, not on https://eduid.se
-    login_request_other_url: Optional[HttpUrlStr] = (
-        None  # Needs to be a full URL since the backend is on the idp, not on https://eduid.se
-    )
+    letter_proofing_url: Optional[str] = None  # deprecated
+    login_base_url: Optional[AnyUrlStr] = None  # deprecated
+    # login_next_url needs to be a full URL since the backend is on the idp, not on https://eduid.se
+    login_next_url: HttpUrlStr = None
+    # login_request_other_url needs to be a full URL since the backend is on the idp, not on https://eduid.se
+    login_request_other_url: Optional[HttpUrlStr] = None
     login_service_url: HttpUrlStr
     lookup_mobile_proofing_service_url: HttpUrlStr
-    lookup_mobile_proofing_url: Optional[str]  # deprecated
+    lookup_mobile_proofing_url: Optional[str] = None  # deprecated
     orcid_service_url: HttpUrlStr
-    orcid_url: Optional[str]  # deprecated
+    orcid_url: Optional[str] = None  # deprecated
     personal_data_service_url: HttpUrlStr
-    personal_data_url: Optional[str]  # deprecated
+    personal_data_url: Optional[str] = None  # deprecated
     phone_service_url: HttpUrlStr
-    phone_url: Optional[str]  # deprecated
+    phone_url: Optional[str] = None  # deprecated
     reset_password_service_url: HttpUrlStr
-    reset_password_url: Optional[str]  # deprecated
+    reset_password_url: Optional[str] = None  # deprecated
     security_service_url: HttpUrlStr
-    security_url: Optional[str]  # deprecated
-    svipe_service_url: Optional[HttpUrlStr]  # if not set the frontend component will not show
-    svipe_url: Optional[str]  # deprecated
+    security_url: Optional[str] = None  # deprecated
+    svipe_service_url: Optional[HttpUrlStr] = None  # if not set the frontend component will not show
+    svipe_url: Optional[str] = None  # deprecated
     # Dashboard config
     default_country_code: int = 46
     proofing_methods: list = Field(default=["letter", "lookup_mobile", "oidc", "eidas"])
