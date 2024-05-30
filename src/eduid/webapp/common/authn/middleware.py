bf67267cff49de94762067155d52c1d04e828895
Johan Lundberg <lundberg@sunet.se>
2024-05-30 10:59:47 +0200

    add CORS headers to authn middleware

--- src/eduid/webapp/common/authn/middleware.py
+++ src/eduid/webapp/common/authn/middleware.py
@@ -2,12 +2,11 @@ import json
 import logging
 import re
 from abc import ABCMeta
-from typing import TYPE_CHECKING, Iterable, cast, Mapping, Any
+from typing import TYPE_CHECKING, Any, Iterable, Mapping, Optional, cast
 from urllib.parse import parse_qs, urlencode, urlparse, urlunparse
 
-from flask import Request, current_app, jsonify, make_response
+from flask import Request, current_app
 from flask_cors.core import get_cors_headers, get_cors_options
-from werkzeug.wrappers import Response
 from werkzeug.wsgi import get_current_url
 
 from eduid.common.config.base import EduIDBaseAppConfig
@@ -65,37 +64,46 @@ class AuthnBaseApp(EduIDBaseApp, metaclass=ABCMeta):
                 # If HTTP_COOKIE is not removed self.request_context(environ) below
                 # will try to look up the Session data in the backend
 
-            ts_url = urlappend(conf.token_service_url, "login")
+        # User has to log in to continue
+        login_url = urlappend(conf.authn_service_url, "login")
 
-            params = {"next": next_url}
-
-            if conf.enable_authn_json_response:
-                # NEW way, respond with a 401 with a JSON payload
-                params["login_url"] = ts_url
-                res = error_response(message="Authentication required", payload=params)
-                _encoded = cast(Mapping[str, Any], FluxStandardAction().dump(res.to_dict()))
-                body = json.dumps(_encoded).encode("utf-8")
-                headers = [
-                    ("Content-Type", "application/json"),
-                    ("Content-Length", str(len(body))),
-                    ("WWW-Authenticate", "eduID"),
-                ]
-                start_response("401 Unauthorized", headers)
-                # return make_response(jsonify(_encoded), 401)
-                return [body]
+        if conf.enable_authn_json_response:
+            # NEW way, respond with a 401 with a JSON payload
+            params = {"location": login_url}
+            res = error_response(message="Authentication required", payload=params)
+            _encoded = cast(Mapping[str, Any], FluxStandardAction().dump(res.to_dict()))
+            body = json.dumps(_encoded).encode("utf-8")
+            headers = [
+                ("Content-Type", "application/json"),
+                ("Content-Length", str(len(body))),
+                ("WWW-Authenticate", "eduID"),
+            ]
+            headers = self._add_cors_headers(environ=environ, headers=headers)
+            start_response("401 Unauthorized", headers)
+            return [body]
 
         # OLD way, respond with a 301 redirect
-
-        url_parts = list(urlparse(ts_url))
+        params = {"next": next_url}
+        url_parts = list(urlparse(login_url))
         query = parse_qs(url_parts[4])
         # Set the 'next' query parameter. parse_qs says it returns a list of values for each key,
         # but if we add [next_url], the result will be actually "...&next=%5B...%5D" which is not what we want."
         # Don't know why that is, but url_parts[4] is probably empty (unless there are query parameters in
-        # conf.token_service_url) so let's just ignore the type error for now.
+        # conf.authn_service_url) so let's just ignore the type error for now.
         query.update(params)  # type: ignore
         url_parts[4] = urlencode(query)
         location = urlunparse(url_parts)
+        headers = self._add_cors_headers(environ=environ)
+        # Add redirect location to header
+        headers.append(("Location", location))
+        start_response("302 Found", headers)
+        return []
 
+    def _add_cors_headers(
+        self, environ: "WSGIEnvironment", headers: Optional[list[tuple[str, str]]] = None
+    ) -> list[tuple[str, str]]:
+        if headers is None:
+            headers = []
         # add cors headers to authentication redirect response
         req = Request(environ)
         cors_options = get_cors_options(self)
@@ -105,11 +113,6 @@ class AuthnBaseApp(EduIDBaseApp, metaclass=ABCMeta):
             request_method=req.method,
         )
         # cors_headers is a MultiDict, start_response wants a list of tuples
-        headers = []
         for key, value in cors_headers.items():
             headers.append((key, value))
-
-        # Add redirect location to header
-        headers.append(("Location", location))
-        start_response("302 Found", headers)
-        return []
+        return headers
