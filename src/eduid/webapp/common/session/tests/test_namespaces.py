f924c0d0d875b5b1c0da71949ca68e3661cab59b
Johan Lundberg <lundberg@sunet.se>
2024-05-30 11:24:56 +0200

    add test for session AuthnNamespace

--- src/eduid/webapp/common/session/tests/test_namespaces.py
+++ src/eduid/webapp/common/session/tests/test_namespaces.py
@@ -2,12 +2,14 @@ import logging
 from datetime import datetime
 from typing import Any, Mapping
 
+from eduid.common.config.base import FrontendAction
 from eduid.common.config.parsers import load_config
 from eduid.common.testing_base import normalised_data
 from eduid.webapp.common.api.testing import EduidAPITestCase
 from eduid.webapp.common.session import EduidSession
 from eduid.webapp.common.session.eduid_session import SessionFactory
 from eduid.webapp.common.session.meta import SessionMeta
+from eduid.webapp.common.session.namespaces import AuthnRequestRef, SP_AuthnRequest
 from eduid.webapp.common.session.tests.test_eduid_session import SessionTestApp, SessionTestConfig
 
 logger = logging.getLogger(__name__)
@@ -98,3 +100,34 @@ class TestIdPNamespace(EduidAPITestCase):
 
         assert second.idp.sso_cookie_val == first.idp.sso_cookie_val
         assert second.idp.ts == first.idp.ts
+
+
+class TestAuthnNamespace(EduidAPITestCase):
+    app: SessionTestApp
+
+    def load_app(self, test_config: Mapping[str, Any]) -> SessionTestApp:
+        """
+        Called from the parent class, so we can provide the appropriate flask
+        app for this test case.
+        """
+        logger.debug("Starting SessionTestApp")
+        config = load_config(typ=SessionTestConfig, app_name="testing", ns="webapp", test_config=test_config)
+        app = SessionTestApp(config)
+        logger.debug("Started SessionTestApp")
+        app.session_interface = SessionFactory(app.conf)
+        return app
+
+    def test_sp_authns_cleanup(self):
+        _meta = SessionMeta.new(app_secret="secret")
+        base_session = self.app.session_interface.manager.get_session(meta=_meta, new=True)
+        sess = EduidSession(app=self.app, meta=_meta, base_session=base_session, new=True)
+        for i in range(15):
+            sess.authn.sp.authns[AuthnRequestRef(str(i))] = SP_AuthnRequest(
+                frontend_action=FrontendAction.LOGIN, finish_url="some_url"
+            )
+        assert len(sess.authn.sp.authns) == 15, f"Expected 15 authns got {len(sess.authn.sp.authns)}"
+        sess.persist()
+        # load the session again
+        base_session = self.app.session_interface.manager.get_session(meta=_meta, new=False)
+        sess = EduidSession(self.app, _meta, base_session, new=False)
+        assert len(sess.authn.sp.authns) == 10, f"Expected 10 authns got {len(sess.authn.sp.authns)}"
