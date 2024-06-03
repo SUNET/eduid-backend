9391b1c185029362d5d8283bb3f67781eeb9d1e2
Johan Lundberg <lundberg@sunet.se>
2024-06-03 10:56:52 +0200

    add common message to signal that the user should not be logged in

--- src/eduid/webapp/common/api/messages.py
+++ src/eduid/webapp/common/api/messages.py
@@ -41,6 +41,8 @@ class CommonMsg(TranslatableMsg):
     nin_invalid = "nin needs to be formatted as 18|19|20yymmddxxxx"
     # Email address validation error
     email_invalid = "email needs to be formatted according to RFC2822"
+    # user must log out
+    logout_required = "logout_required"
     # TODO: These _should_ be unused now - check and remove
     csrf_try_again = "csrf.try_again"
     csrf_missing = "csrf.missing"
