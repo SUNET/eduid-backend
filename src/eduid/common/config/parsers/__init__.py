1b65f4c40006215f8a3e53b8e1cf57f450ce0d80
Lasse Yledahl <ylle@sunet.se>
2024-09-04 08:26:12 +0000

    follow pycodestyle readability, E713

--- src/eduid/common/config/parsers/__init__.py
+++ src/eduid/common/config/parsers/__init__.py
@@ -45,7 +45,7 @@ def load_config(
         # Looks like there could be a FlaskConfig mixed into the config
         config["flask"] = FlaskConfig(**config)
 
-    if "celery_config" in config and not "celery" in config:
+    if "celery_config" in config and "celery" not in config:
         config["celery"] = config["celery_config"]
 
     if "app_name" not in config:
