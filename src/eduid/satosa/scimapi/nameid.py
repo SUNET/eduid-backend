7ec84deaaaf6f6efaa18f0234e4a5746392f72ac
Johan Wassberg <jocar@sunet.se>
2025-05-09 12:47:46 +0200

    Comment in wrong place

--- src/eduid/satosa/scimapi/nameid.py
+++ src/eduid/satosa/scimapi/nameid.py
@@ -35,6 +35,8 @@ class request(RequestMicroService):
         super().__init__(*args, **kwargs)
 
     def process(self, context: satosa.context.Context, data: satosa.internal.InternalData) -> ProcessReturnType:
+        # Pysaml or satosa makes sure that we always have a subject_typeeven
+        # even if not requested by the SP or specified in the metadata.
         if data.subject_type:
             if data.subject_type not in ALLOWED_NAMEIDS:
                 raise SATOSAAuthenticationError(context.state, "Unsupported NameID")
@@ -58,8 +60,6 @@ class response(ResponseMicroService):
         super().__init__(*args, **kwargs)
 
     def process(self, context: satosa.context.Context, data: satosa.internal.InternalData) -> ProcessReturnType:
-        # Pysaml or satosa makes sure that we always have a subject_type
-        # even if not requested by the SP or specified in the metadata.
         subject_type = context.state.get("subject_type")
         if not subject_type:
             raise SATOSAAuthenticationError(context.state, "No NameID from saved state")
