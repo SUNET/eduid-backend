4362494d02176c0867c244f9492999cc484c6414
Lasse Yledahl <ylle@sunet.se>
2025-11-10 09:36:18 +0000

    fix type conversion from environment variables (PLW1508)

--- src/eduid/workers/job_runner/run.py
+++ src/eduid/workers/job_runner/run.py
@@ -3,7 +3,7 @@ from sys import stderr
 
 from eduid.workers.job_runner.app import init_app
 
-DEBUG = os.environ.get("EDUID_APP_DEBUG", False)
+DEBUG = os.environ.get("EDUID_APP_DEBUG", "False").lower() != "false"
 if DEBUG:
     stderr.writelines("----- WARNING! EDUID_APP_DEBUG is enabled -----\n")
 
