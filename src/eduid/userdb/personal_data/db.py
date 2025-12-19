61f2a645d3d6f4082bb3afce81d93f7421ba202b
Johan Lundberg <lundberg@sunet.se>
2025-12-19 15:03:26 +0100

    Have the other private userdb databases inherit from AutoExpiringUserDB

--- src/eduid/userdb/personal_data/db.py
+++ src/eduid/userdb/personal_data/db.py
@@ -1,17 +1,24 @@
 import logging
+from datetime import timedelta
 
 from eduid.userdb.db import TUserDbDocument
 from eduid.userdb.personal_data.user import PersonalDataUser
-from eduid.userdb.userdb import UserDB
+from eduid.userdb.userdb import AutoExpiringUserDB
 
 logger = logging.getLogger(__name__)
 
 __author__ = "lundberg"
 
 
-class PersonalDataUserDB(UserDB[PersonalDataUser]):
-    def __init__(self, db_uri: str, db_name: str = "eduid_personal_data", collection: str = "profiles") -> None:
-        super().__init__(db_uri, db_name, collection=collection)
+class PersonalDataUserDB(AutoExpiringUserDB[PersonalDataUser]):
+    def __init__(
+        self,
+        db_uri: str,
+        db_name: str = "eduid_personal_data",
+        collection: str = "profiles",
+        auto_expire: timedelta | None = None,
+    ) -> None:
+        super().__init__(db_uri, db_name, collection=collection, auto_expire=auto_expire)
 
     @classmethod
     def user_from_dict(cls, data: TUserDbDocument) -> PersonalDataUser:
--- src/eduid/userdb/signup/userdb.py
+++ src/eduid/userdb/signup/userdb.py
@@ -2,12 +2,12 @@ from datetime import timedelta
 
 from eduid.userdb.db import TUserDbDocument
 from eduid.userdb.signup import SignupUser
-from eduid.userdb.userdb import UserDB
+from eduid.userdb.userdb import AutoExpiringUserDB
 
 __author__ = "ft"
 
 
-class SignupUserDB(UserDB[SignupUser]):
+class SignupUserDB(AutoExpiringUserDB[SignupUser]):
     def __init__(
         self,
         db_uri: str,
@@ -15,8 +15,7 @@ class SignupUserDB(UserDB[SignupUser]):
         collection: str = "registered",
         auto_expire: timedelta | None = None,
     ) -> None:
-        super().__init__(db_uri, db_name, collection=collection)
-
+        super().__init__(db_uri, db_name, collection=collection, auto_expire=auto_expire)
 
     @classmethod
     def user_from_dict(cls, data: TUserDbDocument) -> SignupUser:
