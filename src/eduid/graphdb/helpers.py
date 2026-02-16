548f96599b82f4fa71ec166176a1e4f58e47c95d
Lasse Yledahl <ylle@sunet.se>
2026-02-16 14:39:48 +0000

    fix: ensure UTC timezone is applied when converting timestamps

--- src/eduid/graphdb/helpers.py
+++ src/eduid/graphdb/helpers.py
@@ -7,10 +7,8 @@ __author__ = "lundberg"
 def neo4j_ts_to_dt(data: Mapping) -> Mapping[str, datetime | None]:
     created_ts = data.get("created_ts")
     if isinstance(created_ts, int):
-        created_ts = datetime.fromtimestamp(created_ts / 1000)  # Milliseconds since 1970
-        created_ts.replace(tzinfo=UTC)
+        created_ts = datetime.fromtimestamp(created_ts / 1000, tz=UTC)  # Milliseconds since 1970
     modified_ts = data.get("modified_ts")
     if isinstance(modified_ts, int):
-        modified_ts = datetime.fromtimestamp(modified_ts / 1000)  # Milliseconds since 1970
-        modified_ts.replace(tzinfo=UTC)
+        modified_ts = datetime.fromtimestamp(modified_ts / 1000, tz=UTC)  # Milliseconds since 1970
     return {"created_ts": created_ts, "modified_ts": modified_ts}
