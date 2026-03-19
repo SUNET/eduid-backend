0307302790050e5b072ffdd9568fc947db75be99
Johan Lundberg <lundberg@sunet.se>
2026-03-19 15:22:45 +0100

    feat(amapi): migrate to shared status router factory, add /status/ping, remove duplicate utils

--- src/eduid/workers/amapi/tests/test_status.py
+++ src/eduid/workers/amapi/tests/test_status.py
@@ -11,3 +11,8 @@ class TestStatus(TestAMBase):
     def test_status_healthy_ok(self) -> None:
         response = self.client.get(url="/status/healthy")
         assert response.status_code == HTTPStatus.OK
+
+    def test_status_ping(self) -> None:
+        response = self.client.get(url="/status/ping")
+        assert response.status_code == HTTPStatus.OK
+        assert response.content == b""
