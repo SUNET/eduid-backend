2440a7952b7976f827bc0653a43fd3bda198a53d
Lasse Yledahl <ylle@sunet.se>
2026-08-21 13:46:38 +0000

    refactor: raise TypeError for isinstance narrowing guards

--- src/eduid/webapp/idp/logout.py
+++ src/eduid/webapp/idp/logout.py
@@ -137,7 +137,7 @@ class SLO(Service):
         try:
             req_info = current_app.IDP.parse_logout_request(request, binding)
             if not isinstance(req_info, LogoutRequest):
-                raise RuntimeError(f"unexpected parse_logout_request type: {type(req_info).__name__}")
+                raise TypeError(f"unexpected parse_logout_request type: {type(req_info).__name__}")
             current_app.logger.debug(f"Parsed Logout request ({binding}):\n{req_info.message}")
         except Exception as e:
             current_app.logger.exception("Failed parsing logout request")
